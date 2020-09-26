e773f936918bb20af4c08c01dd6ca6a7219cc1bf
Jomesh <jomesh18@gmail.com>
2020-09-26 18:44:50 +0530

    modifications

--- Arrays_easy/sample.cpp
+++ Arrays_easy/sample.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <typeinfo>
+#include <string>
+#include <algorithm>
 
-using namespace std;
+using std::cout;
+using std::string;
+using std::endl;
 
-auto test(string s){
-    return s.size();
+void test(string &s){
+    reverse(s.begin(), s.end());
 }
 
 int main(){	
-    // string s = "the  sky is  blue";
-    string s = "";
-    // string s = " ";
-	cout<<test(s);
+    // int a[5] = {1, 2, 3, 4, 5};
+    // cout<<a<<endl;
+    // cout<<*a<<endl;
+    int p[3][4] = {{4, 8, 13, 9}, {1, 6, 9, 3}, {5, 2, 7, 10}};
+    // cout<<p[2][3]<<endl;
+    cout<<((p+2))<<endl;
+    cout<<*(*(p+2))<<endl;
 	return 0;
 }
