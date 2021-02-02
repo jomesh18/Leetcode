aaa5e7c2f50c062909461e4fd4923f12647683b5
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-02-02 18:56:48 +0530

    added group odd even

--- Linked_list/remove_elements_with_val.py
+++ Linked_list/remove_elements_with_val.py
@@ -10,44 +10,44 @@ Output: 1->2->3->4->5
 
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
 
 class LL:
-    def __init__(self):
-        self.head = None
-    def addAtTail(self, val):
-        node = ListNode(val)
-        if self.head:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = node
-        else:
-            self.head = node
-    def __str__(self):
-        l = []
-        current =self. head
-        while current:
-            l.append(str(current.val))
-            current = current.next
-        return '==>'.join(l)+'==>'
+	def __init__(self):
+		self.head = None
+	def addAtTail(self, val):
+		node = ListNode(val)
+		if self.head:
+			current = self.head
+			while current.next:
+				current = current.next
+			current.next = node
+		else:
+			self.head = node
+	def __str__(self):
+		l = []
+		current =self. head
+		while current:
+			l.append(str(current.val))
+			current = current.next
+		return '==>'.join(l)+'==>'
 
 class Solution:
-    def removeElements(self, head: ListNode, val: int) -> ListNode:
-        current = new_head = head 
-        while current and current.val == val:
-            new_head = current = current.next
-        prev = new_head
-        while current:
-            if current.val == val:
-                prev.next = current.next
-                current = current.next
-            else:
-                prev = current
-                current = current.next
-        return new_head
+	def removeElements(self, head: ListNode, val: int) -> ListNode:
+		current = new_head = head 
+		while current and current.val == val:
+			new_head = current = current.next
+		prev = new_head
+		while current:
+			if current.val == val:
+				prev.next = current.next
+				current = current.next
+			else:
+				prev = current
+				current = current.next
+		return new_head
 
 ll = LL()
 # for i in range(1, 3):
