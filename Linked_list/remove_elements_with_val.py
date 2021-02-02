d9af3f231788e7583e69e3bc1bba427cd3d0a712
jomesh18 <jomesh18@users.noreply.github.com>
2021-02-02 09:40:09 +0530

    Update remove_elements_with_val.py

--- Linked_list/remove_elements_with_val.py
+++ Linked_list/remove_elements_with_val.py
@@ -14,44 +14,50 @@ class ListNode:
         self.val = val
         self.next = next
 
- class LL:
- 	def __init__(self):
- 		self.head = None
- 	def addAtTail(self, val):
- 		node = ListNode(val)
- 		if self.head:
- 			current = self.head
- 			while current.next:
- 				current = current.next
- 			current.next = node
- 		else:
- 			self.head = node
- 	def __str__(self):
- 		l = []
- 		current = head
- 		while current:
- 			l.append(str(current.val))
- 		return '==>'.join(l)+'==>'
+class LL:
+    def __init__(self):
+        self.head = None
+    def addAtTail(self, val):
+        node = ListNode(val)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
+    def __str__(self):
+        l = []
+        current =self. head
+        while current:
+            l.append(str(current.val))
+            current = current.next
+        return '==>'.join(l)+'==>'
 
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-    	current = head
-    	while current.val == 6:
-    		new_head = current.next
-    		current = current.next
-    	prev = new_head
-    	while current:
-    		if current.val == 6:
-    			prev.next = current.next
-    			current = current.next
-    		else:
-    			prev = current
-    			current = current.next
+        current = new_head = head 
+        while current and current.val == val:
+            new_head = current = current.next
+        prev = new_head
+        while current:
+            if current.val == val:
+                prev.next = current.next
+                current = current.next
+            else:
+                prev = current
+                current = current.next
+        return new_head
 
 ll = LL()
-for i in range(1, 3):
-	ll.addAtTail(i)
-ll.addAtTail(6)
-for i in range(3, 7):
-	ll.addAtTail(i)
-s = Solution(ll.head)
+# for i in range(1, 3):
+#     ll.addAtTail(i)
+ll.addAtTail(1)
+# for i in range(3, 7):
+#     ll.addAtTail(i)
+print(ll)
+s = Solution()
+new_ll = LL()
+val = 1
+new_ll.head = s.removeElements(ll.head, val)
+print(new_ll)
