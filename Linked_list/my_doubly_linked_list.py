68db1f251d6c85c4f28fbaac15eae2dffb2ef7b6
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-02-03 19:00:32 +0530

    added doubly ll

---
+++ Linked_list/my_doubly_linked_list.py
@@ -0,0 +1,187 @@
+'''
+Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement the MyLinkedList class:
+
+    MyLinkedList() Initializes the MyLinkedList object.
+    int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+    void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+    void addAtTail(int val) Append a node of value val as the last element of the linked list.
+    void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+    void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+
+ 
+
+Example 1:
+
+Input
+["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+[[], [1], [3], [1, 2], [1], [1], [1]]
+Output
+[null, null, null, null, 2, null, 3]
+
+Explanation
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.get(1);              // return 2
+myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.get(1);              // return 3
+
+ 
+
+Constraints:
+
+    0 <= index, val <= 1000
+    Please do not use the built-in LinkedList library.
+    At most 2000 calls will be made to get, addAtHead, addAtTail,  addAtIndex and deleteAtIndex.
+
+
+'''
+
+class MyLinkedList:
+    class Node:
+        def __init__(self, val=None, next=None, prev=None):
+            self.val = val
+            self.next = next
+            self.prev = prev
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+        
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        current = self.head
+        count = 0
+        while current and count < index:
+            current = current.next
+            count += 1
+        if index<0 or not current:
+            return -1   
+        return current.val
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        node = self.Node(val)
+        if self.head:
+            current = self.head
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        else:
+            self.head = node
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        node = self.Node(val)
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = node
+        node.prev = current
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        if index == 0:
+            self.addAtHead(val)
+        elif index == len(self):
+            self.addAtTail(val)
+        else:
+            node = self.Node(val)
+            current = self.head.next
+            i = 1
+            while current and i<index:
+                current = current.next
+                i += 1
+            node.next = current
+            node.prev = current.prev
+            current.prev.next = node
+            current.prev = node
+
+    def __len__(self):
+        l = 0
+        current = self.head
+        while current:
+            current = current.next
+            l += 1
+        return l
+
+    def __str__(self):
+        l = []
+        current = self.head
+        i = 0
+        while current and i<10:
+            l.append(str(current.val))
+            current = current.next
+            i += 1 
+        return '==>'.join(l)+'==>'
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        current = self.head
+        if index == 0:
+            self.head = current.next
+            current.next.prev = None
+        elif index == len(self)-1:
+            while current.next:
+                current = current.next
+            current.prev.next = None
+        else:
+            i = 0
+            while current and i<index:
+                current = current.next
+                i += 1
+            current.prev.next = current.next
+            current.next.prev = current.prev
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+
+# ["MyLinkedList","addAtHead","addAtHead","addAtHead","addAtIndex","deleteAtIndex","addAtHead","addAtTail","get","addAtHead","addAtIndex","addAtHead"]
+# [[],[7],[2],[1],[3,0],[2],[6],[4],[4],[4],[5,0],[6]]
+
+obj = MyLinkedList()
+print(obj)
+obj.addAtHead(7)
+print(obj)
+obj.addAtHead(2)
+print(obj)
+obj.addAtHead(1)
+print(obj)
+obj.addAtIndex(3, 0)
+print(obj)
+obj.deleteAtIndex(2)
+print(obj)
+obj.addAtHead(6)
+print(obj)
+obj.addAtTail(4)
+print(obj)
+obj.get(4)
+print(obj)
+obj.addAtHead(4)
+print(obj)
+obj.addAtIndex(5, 0)
+print(obj)
+obj.addAtHead(6)
+print(obj)
--- Linked_list/palindrome_ll.py
+++ Linked_list/palindrome_ll.py
@@ -1,3 +1,4 @@
+'''
 Given a singly linked list, determine if it is a palindrome.
 
 Example 1:
@@ -12,11 +13,75 @@ Output: true
 
 Follow up:
 Could you do it in O(n) time and O(1) space?
+'''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LL:
+    def __init__(self):
+        self.head = None
+    def addAtTail(self, val):
+        node = ListNode(val)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
+    def __str__(self):
+        l = []
+        current = self.head
+        i = 0
+        while current and i<10:
+            l.append(str(current.val))
+            current = current.next
+            i += 1 
+        return '==>'.join(l)+'==>'
+
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
+        current = head
+        count = 0
+        odd = False
+        while current:
+            count += 1
+            current = current.next
+        if count < 2:
+            return True
+        if count % 2 != 0:
+            odd = True
+        i = 1
+        current = head.next
+        prev = head
+        while i <= count//2 - 1:
+            prev.next = current.next
+            current.next = head
+            head = current
+            i += 1
+            current = prev.next
+        if odd:
+            current = current.next
+        while current:
+            if current.val != head.val:
+                return False
+            current = current.next
+            head = head.next
+        return True
+
+ll = LL()
+# Input: 1->2->3->4->5->NULL
+# Output: 1->3->5->2->4->NULL
+# for i in range(1, 3):
+#   ll.addAtTail(i)
+# for i in range(2, 1, -1):
+#   ll.addAtTail(i)
+ll.addAtTail(1)
+ll.addAtTail(2)
+print(ll)
+s = Solution()
+print(s.isPalindrome(ll.head))
