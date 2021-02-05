c7208d152220c72a4a8b8a45df3b8ce1cc4edef7
jomesh18 <jomesh18@users.noreply.github.com>
2021-02-05 09:40:17 +0530

    Update merge_sorted_lists.py

--- Linked_list/merge_sorted_lists.py
+++ Linked_list/merge_sorted_lists.py
@@ -30,10 +30,205 @@ Constraints:
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LL:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+        self.size = 0
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index < 0 or index >= self.size:
+            return -1
+
+        if self.head is None:
+            return -1
+
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        node = ListNode(val)
+        node.next = self.head
+        self.head = node
+
+        self.size += 1
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        curr = self.head
+        if curr is None:
+            self.head = ListNode(val)
+        else:
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = ListNode(val)
+
+        self.size += 1
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list.
+        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        if index < 0 or index > self.size:
+            return
+
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            curr = self.head
+            for i in range(index - 1):
+                curr = curr.next
+            node = ListNode(val)
+            node.next = curr.next
+            curr.next = node
+
+            self.size += 1
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        if index < 0 or index >= self.size:
+            return
+
+        curr = self.head
+        if index == 0:
+            self.head = curr.next
+        else:
+            for i in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+
+        self.size -= 1
+    def __str__(self):
+        l = []
+        current = self.head
+        i = 0
+        while current and i<10:
+            l.append(str(current.val))
+            current = current.next
+            i += 1 
+        return '==>'.join(l)+'==>'
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        outNode = ListNode()
+        current = outNode
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+
+        # if l1:
+        #     current.next = l1
+        # if l2:
+        #     current.next = l2
+        current.next = l1 or l2
+
+        return outNode.next
+
+
+l1 = [1,2,4]
+l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# l1 = []
+# l2 = []
+# Output: []
+# l1 = []
+# l2 = [0]
+# Output: [0]
+ll1 = LL()
+ll2 = LL()
+for i in l1:
+    ll1.addAtTail(i)
+for i in l2:
+    ll2.addAtTail(i)
+print(ll1)
+print(ll2)
+s = Solution()
+result = LL()
+result.head = s.mergeTwoLists(ll1.head, ll2.head)
+print(result)
+print(ll1)
+print(ll2)
+
+#Python solutions (iteratively, recursively, iteratively in-place).
+# iteratively
+def mergeTwoLists1(self, l1, l2):
+    dummy = cur = ListNode(0)
+    while l1 and l2:
+        if l1.val < l2.val:
+            cur.next = l1
+            l1 = l1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+    cur.next = l1 or l2
+    return dummy.next
+    
+# recursively    
+def mergeTwoLists2(self, l1, l2):
+    if not l1 or not l2:
+        return l1 or l2
+    if l1.val < l2.val:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = self.mergeTwoLists(l1, l2.next)
+        return l2
         
+# in-place, iteratively        
+def mergeTwoLists(self, l1, l2):
+    if None in (l1, l2):
+        return l1 or l2
+    dummy = cur = ListNode(0)
+    dummy.next = l1
+    while l1 and l2:
+        if l1.val < l2.val:
+            l1 = l1.next
+        else:
+            nxt = cur.next
+            cur.next = l2
+            tmp = l2.next
+            l2.next = nxt
+            l2 = tmp
+        cur = cur.next
+    cur.next = l1 or l2
+    return dummy.next
