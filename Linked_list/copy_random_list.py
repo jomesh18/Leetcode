e5d994a0a06fa0eb4ef77d6af24191fa57079f85
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-02-13 18:54:18 +0530

    Update copy_random_list.py

--- Linked_list/copy_random_list.py
+++ Linked_list/copy_random_list.py
@@ -50,15 +50,90 @@ Constraints:
 
 '''
 
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
+class LL:
+    def __init__(self):
+        self.head = None
+
+    def addAtTail(self, val):
+        node = Node(val)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
+    def __str__(self):
+        l = []
+        current = self.head
+        i = 0
+        while current and i<10:
+            l.append(str(current.val))
+            current = current.next
+            i += 1 
+        return '==>'.join(l)+'==>'
+
+
 
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        
+        if not head: return head
+        current = head
+        while current:
+        	temp = current.next
+        	current.next = Node(current.val)
+        	current.next.next = temp
+        	current = current.next.next
+        current = head
+        while current:
+        	current.next.random = current.random if not current.random else current.random.next
+        	current = current.next.next
+        current = head.next
+        while current.next:
+        	current.next = current.next.next
+        	current = current.next
+        return head.next
+
+ll = LL()
+head = [[7,None],[13,0],[11,4],[10,2],[1,0]]
+# Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+for i in head:
+	ll.addAtTail(i[0])
+print(ll)
+main_loop = ll.head
+for i in head:
+	current = ll.head
+	pos = i[1]
+	if pos is None:
+		main_loop.random = pos
+	else:
+		while pos>0:
+			pos -= 1
+			current = current.next
+		main_loop.random = current
+	main_loop = main_loop.next
+l = []
+head = ll.head
+while head:
+	l.append((head.val, head.random.val if head.random else head.random))
+	head = head.next
+print(l)
+copy_ll = LL()
+s = Solution()
+copy_ll.head = s.copyRandomList(ll.head)
+print(copy_ll)
+head = copy_ll.head
+l = []
+while head:
+	l.append((head.val, head.random.val if head.random else head.random))
+	head = head.next
+print(l)
+print('finished')
