7a1d8dff03766815111f7a44ca0d639c96787591
jomesh18 <jomesh18@users.noreply.github.com>
2021-02-18 06:45:48 +0530

    Update circular_queue.py

--- Queue & Stack/circular_queue.py
+++ Queue & Stack/circular_queue.py
@@ -52,64 +52,57 @@ Follow up: Could you solve the problem without using the built-in queue?
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.l = [None] * k
+        self.data = [None] * k
         self.head = self.tail = None
+        self.size = k
 
     def enQueue(self, value: int) -> bool:
-        k = len(self.l)
         if self.isEmpty():
             self.head = self.tail = 0
-            self.l[self.tail] = value
-            print(self.head, self.tail, self.l)
+            self.data[self.tail] = value
         elif self.isFull():
             return False
-        elif self.tail < k-1:
+        elif self.tail < self.size-1: #or combine both cases with if tail <= k-1: self.tail=(self.tail+1)%k, self.data[self.tail] = value
             self.tail += 1
-            self.l[self.tail] = value
-            print(self.head, self.tail, self.l)
-        elif self.tail == k-1:
+            self.data[self.tail] = value
+            # print(self.head, self.tail, self.data)
+        elif self.tail == self.size-1:
             self.tail = 0
-            self.l[self.tail] = value
+            self.data[self.tail] = value
         return True
 
     def deQueue(self) -> bool:
-        k = len(self.l)
         if self.isEmpty():
             return False
         elif self.head == self.tail:
             self.head = self.tail = None
-            return False
-        elif self.head<k-1:
+        elif self.head<self.size-1: #or combine both as above
             self.head += 1
-            self.l[self.head] = value
-        elif self.head == k-1:
+        elif self.head == self.size-1:
             self.head = 0
-            self.l[self.head] = value
         return True
 
     def Front(self) -> int:
-        return self.l[self.head] if self.head is not None else -1
+        return self.data[self.head] if self.head is not None else -1
 
     def Rear(self) -> int:
-        return self.l[self.tail] if self.tail is not None else -1
+        return self.data[self.tail] if self.tail is not None else -1
 
     def isEmpty(self) -> bool:
-        a = True if self.head is not None else False
-        print(a)
-        return True if self.head is not None else False
+        return True if self.head is None else False
 
     def isFull(self) -> bool:
         if self.head is not None:
-            if (self.tail - self.head == k-1) or (self.head - self.tail == 1):
+            if (self.tail - self.head == self.size-1) or (self.head - self.tail == 1): #or return (self.tail+1)%self.size==self.head
                 return True
         return False
 
     def __str__(self):
         if self.head is not None:
             if self.head<=self.tail:
-                return str(l[self.head:self.tail+1])
+                return str(self.data[self.head:self.tail+1])
             else:
-                return str(l[:self.tail+1]+l[self.head:])
+                return str(self.data[:self.tail+1]+self.data[self.head:])
         return 'None'
 
 
@@ -127,7 +120,16 @@ class MyCircularQueue:
 # [[3], [1], [2], [3], [4], [], [], [], [4], []]
 # Output
 # [null, true, true, true, false, 3, true, true, true, 4]
-q = MyCircularQueue(3)
+q = MyCircularQueue(6)
+print(q.enQueue(6))
+# print(q.enQueue(2))
+# print(q)
+print(q.deQueue())
+print(q)
+print(q.deQueue())
+print(q)
+print(q.deQueue())
+print(q)
 # print(q)
 # print(q.l)
 # print(q.head)
@@ -137,11 +139,20 @@ q = MyCircularQueue(3)
 #     i+=1
 #     print(q.enQueue(i))
 # print(q)
-print(q.enQueue(1))
+# # print(q.enQueue(1))
 # # print(q.enQueue(2))
 # # print(q.enQueue(3))
 # # print(q.enQueue(4))
 # print(q.Rear())
 # print(q.isFull())
 # print(q.deQueue())
+# print(q)
 # print(q.Rear())
+# print(q.l, q.head, q.tail)
+
+'''
+i/p: ["MyCircularQueue","enQueue","Rear","Rear","deQueue","enQueue","Rear","deQueue","Front","deQueue","deQueue","deQueue"]
+    [[6],[6],[],[],[],[5],[],[],[],[],[],[]]
+My o/p: [null,true,6,6,false,true,5,false,-1,false,false,false]
+Real o/p: [null,true,6,6,true,true,5,true,-1,false,false,false]
+'''
