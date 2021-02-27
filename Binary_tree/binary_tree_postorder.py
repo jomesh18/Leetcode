4d03ebabc041325781d4add55eb03594a678319e
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-02-27 19:08:54 +0530

    Update binary_tree_postorder.py

--- Binary_tree/binary_tree_postorder.py
+++ Binary_tree/binary_tree_postorder.py
@@ -71,21 +71,45 @@ class TreeNode:
 # 					stack.append((curr.right, False))
 # 					stack.append((curr.left, False))
 # 		return res
-# iteratively using stack
+
+# iteratively using stack, not correct
 class Solution:
 	def postorderTraversal(self, root: TreeNode) -> []:
-		res = []
-		stack = []
-		curr = root
-		while curr or stack:
-			while curr:
-				stack.append(curr)
-				if curr.right:
-					stack.append(curr.right)
-				curr = curr.left
-			curr = stack.pop()
-			res.append(curr.val)
-		return res
+		nodes = []
+		todo = []
+		last = None
+		while root or todo:
+			if root:
+				todo.append(root)
+				root = root.left
+			else:
+				node = todo.pop()
+				if node.right and last != node.right:
+					root = node.right
+				else:
+					nodes.append(node.val)
+					last = node
+					todo.pop()
+		return nodes
+
+#from leetcode, modified pre order
+# modified preorder (right subtree first). Then reverse the result.
+
+# class Solution:
+#     # @param {TreeNode} root
+#     # @return {integer[]}
+#     def postorderTraversal(self, root):
+#         traversal, stack = [], [root]
+#         while stack:
+#             node = stack.pop()
+#             if node:
+#                 # pre-order, right first
+#                 traversal.append(node.val)
+#                 stack.append(node.left)
+#                 stack.append(node.right)
+
+#         # reverse result
+#         return traversal[::-1]
 
 tn = TreeNode('f', TreeNode('b', TreeNode('a', None, None), TreeNode('d', TreeNode('c', None, None), TreeNode('e', None, None))), TreeNode('g', None, TreeNode('i', TreeNode('h', None, None), None)))
 s = Solution()
