275c54fc2623da280ca7f94a17d618cc354747ce
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-03-18 18:56:45 +0530

    added has_path_sum.py

--- Binary_tree/symmetric_tree.py
+++ Binary_tree/symmetric_tree.py
@@ -35,21 +35,68 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# iterative
+# class Solution:
+#     def isSymmetric(self, root: TreeNode) -> bool:
+#         lef = deque([root.left])
+#         rig = deque([root.right])
+        
+#         while lef or rig:
+#             l = lef.popleft() if lef else None
+#             r = rig.popleft() if rig else None
+#             if l and r:
+#                 if l.val != r.val:
+#                     return False
+#                 lef.extend([l.left, l.right])
+#                 rig.extend([r.right, r.left])
+#             elif not l and not r:
+#                 pass
+#             elif not l or not r:
+#                 return False
+#         return True
+
+#from leetcode, using recursion
+# class Solution:
+#     def isSymmetric(self, root: TreeNode) -> bool:
+#         return self.isMirror(root, root)
+
+#     def isMirror(self, n1, n2):
+#         if not n1 and not n2: return True
+#         if not n1 or not n2: return False
+#         return n1.val == n2.val and self.isMirror(n1.left, n2.right) and self.isMirror(n1.right, n2.left)
+
+#from leetcode, using one queue
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        left = deque[root.left]
-        right = deque[root.right]
-        
-        while left or right:
-            l = left.popleft()
-            r = right.popleft()
-            if l and r:
-                if l.val != r.val:
-                    return False
-                l.extend([l.left, l.right])
-                r.extend([r.right, r.left])
-                
-        
+        q = deque([root, root])
+        while q:
+            l = q.popleft()
+            r = q.popleft()
+            if not l and not r: continue
+            if not l or not r: return False
+            if l.val != r.val: return False
+            q.extend([l.left, r.right, l.right, r.left])
+        return True
+
+#from leetcode
+class Solution:
+    def isSymmetric(self, root):
+        def isSym(L,R):
+            if L and R and L.val == R.val: 
+                return isSym(L.left, R.right) and isSym(L.right, R.left)
+            return L == R
+        return not root or isSym(root.left, root.right)
+
+#from leetcode, iterative
+class Solution:
+    def isSymmetric(self, root):
+        queue = [root]
+        while queue:
+            values = [i.val if i else None for i in queue]
+            if values != values[::-1]: return False
+            queue = [child for i in queue if i for child in (i.left, i.right)]
+        return True
+
 def construct_tree(root):
     start = TreeNode(root[0])
     q = deque([start])
@@ -75,7 +122,7 @@ def print_level_order(root):
     return ans
 
 root = [1,2,2,3,4,4,3]
-root = [1,2,2,3,None,3,None]
+# root = [1,2,2,3,None,3,None]
 # Output: true
 start = construct_tree(root)
 print(print_level_order(start))
