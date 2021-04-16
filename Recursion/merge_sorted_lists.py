9a63173b6a7a16ec72dd2945b53ecbf3e29dbd7c
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-04-16 18:51:37 +0530

    added kth symbol

--- Recursion/merge_sorted_lists.py
+++ Recursion/merge_sorted_lists.py
@@ -38,43 +38,85 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-    	dummy = ListNode()
-    	prev = dummy
-    	def helper(l1, l2):
-	    	if not l1:
-	    		prev.next = l2
-	    		return
-	    	if not l2:
-	    		prev.next = l1
-	    		return
-	    	if l1.val <= l2.val:
-	    		prev.next = l1
-	    		helper(l1.next, l2)
-	    	else:
-	    		prev.next = l2
-	    		helper(l1, l2.next)
-    	helper(l1, l2)
-    	return dummy.next
+        dummy = ListNode()
+        def helper(l1, l2, prev):
+            if not l1:
+                prev.next = l2
+                return
+            if not l2:
+                prev.next = l1
+                return
+            if l1.val <= l2.val:
+                prev.next = l1
+                helper(l1.next, l2, prev.next)
+            else:
+                prev.next = l2
+                helper(l1, l2.next, prev.next)
+        helper(l1, l2, dummy)
+        return dummy.next
+
+#from leetcode
+class Solution:        
+    def mergeTwoLists2(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+#from leetcode
+# Solution 1
+
+# If both lists are non-empty, I first make sure a starts smaller, use its head as result, and merge the remainders behind it. Otherwise, i.e., if one or both are empty, I just return what's there.
+
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if a and b:
+            if a.val > b.val:
+                a, b = b, a
+            a.next = self.mergeTwoLists(a.next, b)
+        return a or b
+
+# Solution 2
+
+# First make sure that a is the "better" one (meaning b is None or has larger/equal value). Then merge the remainders behind a.
+
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if not a or b and a.val > b.val:
+            a, b = b, a
+        if a:
+            a.next = self.mergeTwoLists(a.next, b)
+        return a
 
 def build_list(l):
-	if not l: return l
-	start = ListNode(l[0])
-	prev = start
-	for i in l[1:]:
-		prev.next = ListNode(i)
-		prev = prev.next
-	return start
+    if not l: return l
+    start = ListNode(l[0])
+    prev = start
+    for i in l[1:]:
+        prev.next = ListNode(i)
+        prev = prev.next
+    return start
 
 def print_list(l):
-	curr = l
-	while curr:
-		print(curr.val, end='==>')
-		curr = curr.next
-	print()
+    curr = l
+    while curr:
+        print(curr.val, end='==>')
+        curr = curr.next
+    print()
 
 l1 = [1,2,4]
 l2 = [1,3,4]
 # Output: [1,1,2,3,4,4]
+# l1 = []
+# l2 = []
+# # Output: []
+# l1 = []
+# l2 = [0]
+# Output: [0]
 start1 = build_list(l1)
 start2= build_list(l2)
 print_list(start1)
@@ -82,3 +124,5 @@ print_list(start2)
 s = Solution()
 start = s.mergeTwoLists(start1, start2)
 print_list(start)
+# print_list(start1)
+# print_list(start2)
