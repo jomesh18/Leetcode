841d6a5106d65342579cbebd1870902f2de8b8e5
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-06-25 09:37:33 +0530

    Update 24.out_of_boundary_paths.py

--- Leetcode_challenge/June 2021/24.out_of_boundary_paths.py
+++ Leetcode_challenge/June 2021/24.out_of_boundary_paths.py
@@ -27,26 +27,56 @@ Constraints:
     0 <= startColumn <= n
 
 '''
-from collections import deque
+# from collections import deque
+# class Solution:
+#     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+#         def bfs(c, d):
+#             neighbours = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+#             count = 0
+#             level = 0
+#             q = deque([(c, d)])
+#             while level < maxMove:
+#                 l = len(q)
+#                 for _ in range(l):
+#                     i, j = q.popleft()
+#                     for a, b in neighbours:
+#                         if not 0<=i+a<m or not 0<=j+b<n:
+#                             count += 1
+#                         if 0<=i+a<m and 0<=j+b<n:
+#                             q.append((i+a, j+b))
+#                 level += 1
+#             return count%(10**9+7)
+#         return bfs(startRow, startColumn)
+
+#from leetcode
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        def bfs(c, d):
-            neighbours = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-            count = 0
-            level = 0
-            q = deque([(c, d)])
-            while level < maxMove:
-                l = len(q)
-                for _ in range(l):
-                    i, j = q.popleft()
-                    for a, b in neighbours:
-                        if not 0<=i+a<m or not 0<=j+b<n:
-                            count += 1
-                        if 0<=i+a<m and 0<=j+b<n:
-                            q.append((i+a, j+b))
-                level += 1
-            return count%(10**9+7)
-        return bfs(startRow, startColumn)
+        
+        # define the dp array
+        dp = [[[-1]*(maxMove+1) for _ in range(n+1)] for _ in range(m+1)]
+        
+        def solve(i, j, maxMove):
+            if maxMove < 0:
+                return 0
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return 1
+            
+            # if the dp array at this position contains some value(not -1) then it means it has been computed earlier
+            # so we don't need to compute again, hence return whatever value is present in dp.
+            if dp[i][j][maxMove] != -1:
+                return dp[i][j][maxMove]
+            
+            # otherwise compute the value
+            a = solve(i-1, j, maxMove - 1)
+            b = solve(i+1, j, maxMove - 1)
+            c = solve(i, j-1, maxMove - 1)
+            d = solve(i, j+1, maxMove - 1)
+            
+            # store the computed value in dp array so that we do not need to compute it again when the same input occurs again.
+            dp[i][j][maxMove] = a + b + c + d
+            return dp[i][j][maxMove]
+        
+        return solve(startRow, startColumn, maxMove) % 1000000007
 
 m = 2
 n = 2
