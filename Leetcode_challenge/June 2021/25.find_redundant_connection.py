1a2b9695168be2ea0ffef8a378b8b3269f328d31
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-06-28 20:30:38 +0530

    added hard problems

--- Leetcode_challenge/June 2021/25.find_redundant_connection.py
+++ Leetcode_challenge/June 2021/25.find_redundant_connection.py
@@ -32,17 +32,16 @@ Constraints:
     The given graph is connected.
 
 '''
+#from leetcode
 from collections import defaultdict
 class Solution:
     def findRedundantConnection(self, edges: [[int]]) -> [int]:
         graph = defaultdict(set)
-
         def dfs(source, target):
             if source not in seen:
                 seen.add(source)
                 if source == target: return True
                 return any(dfs(nei, target) for nei in graph[source])
-
         for u, v in edges:
             seen = set()
             if u in graph and v in graph and dfs(u, v):
---
+++ Leetcode_challenge/June 2021/26.count_smaller_after_self.py
@@ -0,0 +1,66 @@
+'''
+Count of Smaller Numbers After Self
+You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,6,1]
+Output: [2,1,1,0]
+Explanation:
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
+Example 2:
+
+Input: nums = [-1]
+Output: [0]
+Example 3:
+
+Input: nums = [-1,-1]
+Output: [0,0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+
+'''
+
+class Solution:
+    def countSmaller(self, nums: [int]) -> [int]:
+        def sort(enum):
+            half = len(enum) // 2
+            if half:
+                left, right = sort(enum[:half]), sort(enum[half:])
+                for i in range(len(enum))[::-1]:
+                    if not right or left and left[-1][1] > right[-1][1]:
+                        smaller[left[-1][0]] += len(right)
+                        enum[i] = left.pop()
+                    else:
+                        enum[i] = right.pop()
+            return enum
+        smaller = [0] * len(nums)
+        sort(list(enumerate(nums)))
+        return smaller
+
+nums = [5,2,6,1]
+# Output: [2,1,1,0]
+
+nums = [-1]
+# Output: [0]
+
+nums = [-1,-1]
+# Output: [0,0]
+
+nums = [2,0,1]
+# Output: [2,0,0]
+
+nums = [2,0,1, 3, 2, 1, 0]
+# Output: [4,0,1,3,2,1,0]
+
+s = Solution()
+print(s.countSmaller(nums))
