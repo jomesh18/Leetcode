9fb479abfb6d8f5874ba7f84a6b7790fb488aca1
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-07-09 19:26:07 +0530

    added 9.lengthOfLIS.py

--- Binary_search_tree/deleteNode.py
+++ Binary_search_tree/deleteNode.py
@@ -52,71 +52,94 @@ class TreeNode:
         self.right = right
 
 #my try
+# class Solution:
+#     def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+#         dummy = TreeNode(float("-inf"), None, root)
+#         def find_pos(node, key):
+#             curr = node
+#             pos = (node, 1)
+#             while curr:
+#                 if key > curr.val:
+#                     pos = (curr, 1)
+#                     curr = curr.right
+#                 elif key < curr.val:
+#                     pos = (curr, 0)
+#                     curr = curr.left
+#                 else:
+#                     break
+#             return (curr, pos)
+
+#         def inorderSuccessor(curr):
+#             node = root
+#             succ = None
+#             while node:
+#                 if node.val <= curr.val:
+#                     node = node.right
+#                 else:
+#                     succ = node
+#                     node = node.left
+#             return succ
+
+#         key_node, p = find_pos(dummy, key)
+#         node_above_key_node, key_node_is_right_child = p[0], p[1]
+#         if not key_node: return dummy.right
+#         if key_node.left is None and key_node.right is None: #delete a leaf node
+#             if key_node_is_right_child:
+#                 node_above_key_node.right = None
+#             else:
+#                 node_above_key_node.left = None
+#             return dummy.right
+#         elif key_node.right is None: # delete node with no right subtree
+#             if key_node_is_right_child:
+#                 node_above_key_node.right = key_node.left
+#             else:
+#                 node_above_key_node.left = key_node.left
+#             return dummy.right
+
+#         succ = inorderSuccessor(key_node)
+#         parent_successor = None
+#         t1, t2 = find_pos(dummy, succ.val)
+#         parent_successor, succ_right_child_of_parent = t2
+#         if succ_right_child_of_parent:
+#             parent_successor.right = None
+#         else:
+#             parent_successor.left = None
+#         succ.left = key_node.left
+#         curr = succ
+#         prev = succ
+#         while curr:
+#             prev = curr
+#             curr = curr.right
+#         prev.right = key_node.right
+#         if key_node_is_right_child:
+#             node_above_key_node.right = succ
+#         else:
+#             node_above_key_node.left = succ
+
+#         return dummy.right
+        
 class Solution:
-    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
-        dummy = TreeNode(float("-inf"), None, root)
-        def find_pos(node, key):
-            curr = node
-            pos = (node, 1)
-            while curr:
-                if key > curr.val:
-                    pos = (curr, 1)
-                    curr = curr.right
-                elif key < curr.val:
-                    pos = (curr, 0)
-                    curr = curr.left
-                else:
-                    break
-            return (curr, pos)
-
-        def inorderSuccessor(curr):
-            node = root
-            succ = None
-            while node:
-                if node.val <= curr.val:
-                    node = node.right
-                else:
-                    succ = node
-                    node = node.left
-            return succ
-
-        key_node, p = find_pos(dummy, key)
-        node_above_key_node, key_node_is_right_child = p[0], p[1]
-        if not key_node: return dummy.right
-        if key_node.left is None and key_node.right is None: #delete a leaf node
-            if key_node_is_right_child:
-                node_above_key_node.right = None
-            else:
-                node_above_key_node.left = None
-            return dummy.right
-        elif key_node.right is None: # delete node with no right subtree
-            if key_node_is_right_child:
-                node_above_key_node.right = key_node.left
-            else:
-                node_above_key_node.left = key_node.left
-            return dummy.right
-
-        succ = inorderSuccessor(key_node)
-        parent_successor = None
-        t1, t2 = find_pos(dummy, succ.val)
-        parent_successor, succ_right_child_of_parent = t2
-        if succ_right_child_of_parent:
-            parent_successor.right = None
-        else:
-            parent_successor.left = None
-        succ.left = key_node.left
-        curr = succ
-        prev = succ
-        while curr:
-            prev = curr
-            curr = curr.right
-        prev.right = key_node.right
-        if key_node_is_right_child:
-            node_above_key_node.right = succ
-        else:
-            node_above_key_node.left = succ
-
-        return dummy.right
+   def deleteNode(root, key):
+    if not root: # if root doesn't exist, just return it
+        return root
+    if root.val > key: # if key value is less than root value, find the node in the left subtree
+        root.left = deleteNode(root.left, key)
+    elif root.val < key: # if key value is greater than root value, find the node in right subtree
+        root.right= deleteNode(root.right, key)
+    else: #if we found the node (root.value == key), start to delete it
+        if not root.right: # if it doesn't have right children, we delete the node then new root would be root.left
+            return root.left
+        if not root.left: # if it has no left children, we delete the node then new root would be root.right
+            return root.right
+               # if the node have both left and right children,  we replace its value with the minmimum value in the right subtree and then delete that minimum node in the right subtree
+        temp = root.right
+        mini = temp.val
+        while temp.left:
+            temp = temp.left
+            mini = temp.val
+        root.val = mini # replace value
+        root.right = deleteNode(root.right,root.val) # delete the minimum node in right subtree
+    return root
 
 def build_tree(root):
     if not root: return root
---
+++ Leetcode_challenge/July 2021/9.lengthOfLIS.py
@@ -0,0 +1,64 @@
+'''
+Longest Increasing Subsequence
+
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 2500
+    -104 <= nums[i] <= 104
+
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+'''
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums: [int]) -> int:
+        sub = []
+        for num in nums:
+            i = bisect_left(sub, num)
+
+            # If num is greater than any element in sub
+            if i == len(sub):
+                sub.append(num)
+            
+            # Otherwise, replace the first element in sub greater than or equal to num
+            else:
+                sub[i] = num
+        
+        return len(sub)
+
+nums = [10,9,2,5,3,7,101,18]
+# Output: 4
+
+# nums = [0,1,0,3,2,3]
+# # Output: 4
+
+# nums = [7,7,7,7,7,7,7]
+# # Output: 1
+
+s = Solution()
+print(s.lengthOfLIS(nums))
