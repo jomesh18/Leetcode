d3488a519a5897674413e135d1dc128d5539accc
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-07-12 08:56:54 +0530

    added 11.medianfinder.py

--- Binary_search_tree/deleteNode.py
+++ Binary_search_tree/deleteNode.py
@@ -117,29 +117,49 @@ class TreeNode:
 #             node_above_key_node.left = succ
 
 #         return dummy.right
-        
+
+#from leetcode    
+# class Solution:
+#    def deleteNode(self, root, key):
+#     if not root: # if root doesn't exist, just return it
+#         return root
+#     if root.val > key: # if key value is less than root value, find the node in the left subtree
+#         root.left = self.deleteNode(root.left, key)
+#     elif root.val < key: # if key value is greater than root value, find the node in right subtree
+#         root.right= self.deleteNode(root.right, key)
+#     else: #if we found the node (root.value == key), start to delete it
+#         if not root.right: # if it doesn't have right children, we delete the node then new root would be root.left
+#             return root.left
+#         if not root.left: # if it has no left children, we delete the node then new root would be root.right
+#             return root.right
+#                # if the node have both left and right children,  we replace its value with the minmimum value in the right subtree and then delete that minimum node in the right subtree
+#         temp = root.right
+#         mini = temp.val
+#         while temp.left:
+#             temp = temp.left
+#             mini = temp.val
+#         root.val = mini # replace value
+#         root.right = self.deleteNode(root.right,root.val) # delete the minimum node in right subtree
+#     return root
+
+#practising
 class Solution:
-   def deleteNode(root, key):
-    if not root: # if root doesn't exist, just return it
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root: return root
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.right: return root.left
+            if not root.left: return root.right
+            temp = root.right
+            while temp.left:
+                temp = temp.left
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, temp.val)
         return root
-    if root.val > key: # if key value is less than root value, find the node in the left subtree
-        root.left = deleteNode(root.left, key)
-    elif root.val < key: # if key value is greater than root value, find the node in right subtree
-        root.right= deleteNode(root.right, key)
-    else: #if we found the node (root.value == key), start to delete it
-        if not root.right: # if it doesn't have right children, we delete the node then new root would be root.left
-            return root.left
-        if not root.left: # if it has no left children, we delete the node then new root would be root.right
-            return root.right
-               # if the node have both left and right children,  we replace its value with the minmimum value in the right subtree and then delete that minimum node in the right subtree
-        temp = root.right
-        mini = temp.val
-        while temp.left:
-            temp = temp.left
-            mini = temp.val
-        root.val = mini # replace value
-        root.right = deleteNode(root.right,root.val) # delete the minimum node in right subtree
-    return root
+
 
 def build_tree(root):
     if not root: return root
