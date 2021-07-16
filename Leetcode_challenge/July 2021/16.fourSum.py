bf50b8aac24d2cac016bfaeae0c197caa58bfb61
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-07-16 19:43:55 +0530

    added foursum.py

---
+++ Leetcode_challenge/July 2021/16.fourSum.py
@@ -0,0 +1,70 @@
+'''
+4Sum
+
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+    0 <= a, b, c, d < n
+    a, b, c, and d are distinct.
+    nums[a] + nums[b] + nums[c] + nums[d] == target
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 200
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+
+'''
+
+class Solution:
+    def fourSum(self, nums: [int], target: int) -> [[int]]:
+        def kSum(nums: [int], target: int, k: int) -> [[int]]:
+            if len(nums) == 0 or nums[0] * k > target or target > nums[-1] * k:
+                return []
+            if k == 2:
+                return twoSum(nums, target)
+            res = []
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for _, set in enumerate(kSum(nums[i + 1:], target - nums[i], k - 1)):
+                        res.append([nums[i]] + set)
+            return res
+
+        def twoSum(nums: [int], target: int) -> [[int]]:
+            res = []
+            s = set()
+            for i in range(len(nums)):
+                if len(res) == 0 or res[-1][1] != nums[i]:
+                    if target - nums[i] in s:
+                        res.append([target - nums[i], nums[i]])
+                s.add(nums[i])
+            return res
+
+        nums.sort()
+        return kSum(nums, target, 4)
+
+nums = [1,0,-1,0,-2,2]
+target = 0
+# Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+# nums = [2,2,2,2,2]
+# target = 8
+# # Output: [[2,2,2,2]]
+
+s = Solution()
+print(s.fourSum(nums, target))
