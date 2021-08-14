eb5d18a289160cc72dc209c3dda7a549757df82b
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-08-14 10:03:01 +0530

    Create 13.setZeroes.py

---
+++ Leetcode_challenge/8.August 2021/13.setZeroes.py
@@ -0,0 +1,72 @@
+'''
+Set Matrix Zeroes
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+
+   Hide Hint #1  
+If any cell of the matrix has a zero we can record its row and column number using additional memory. But if you don't want to use extra memory then you can manipulate the array instead. i.e. simulating exactly what the question says.
+   Hide Hint #2  
+Setting cell values to zero on the fly while iterating might lead to discrepancies. What if you use some other integer value as your marker? There is still a better approach for this problem with 0(1) space.
+   Hide Hint #3  
+We could have used 2 sets to keep a record of rows/columns which need to be set to zero. But for an O(1) space solution, you can use one of the rows and and one of the columns to keep track of this information.
+   Hide Hint #4  
+We can use the first cell of every row and column as a flag. This flag would determine whether a row or column has been set to zero.
+'''
+
+class Solution:
+    def setZeroes(self, matrix: [[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        R = len(matrix)
+        C = len(matrix[0])
+        rows, cols = set(), set()
+
+        # Essentially, we mark the rows and columns that are to be made zero
+        for i in range(R):
+            for j in range(C):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        # Iterate over the array once again and using the rows and cols sets, update the elements
+        for i in range(R):
+            for j in range(C):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
+
+matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output = [[1,0,1],[0,0,0],[1,0,1]]
+
+sol = Solution()
+print(sol.setZeroes(matrix))
+print(matrix)
