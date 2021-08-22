0cc204d21c9432bd95d40f2fc94145c5c2041265
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-08-22 23:35:33 +0530

    Update 21.solveSudoku.py

--- Leetcode_challenge/8.August 2021/21.solveSudoku.py
+++ Leetcode_challenge/8.August 2021/21.solveSudoku.py
@@ -30,107 +30,115 @@ Constraints:
     It is guaranteed that the input board has only one solution.
 
 '''
+from collections import deque
 import pprint
+#my try, not working on non prohibitive puzzles
 class Solution:
-    # def solveSudoku(self, board: [[str]]) -> None:
-        # """
-        # Do not return anything, modify board in-place instead.
-        # """
-        # not_solved = set()
-        # row = len(board)
-        # column = len(board[0])
-        # for i in range(row):
-        #     for j in range(column):
-        #         if board[i][j] == ".":
-        #             not_solved.add((i, j))
+    def solveSudoku(self, board: [[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        not_solved = deque([])
+        row = len(board)
+        column = len(board[0])
+        count = 0
+        for i in range(row):
+            for j in range(column):
+                if board[i][j] == ".":
+                    not_solved.append((i, j))
 
-        # def find_possible_values(i, j):
-        #     # print('i = {}, j = {}'.format(i, j))
-        #     sub_box_row, sub_box_column = (i//3)*3, (j//3)*3
-        #     possible = set(range(1, 10))
-        #     for k in range(sub_box_row, sub_box_row+3):
-        #         for l in range(sub_box_column, sub_box_column+3):
-        #             current_val = board[k][l]
-        #             # print(k, l, current_val)
-        #             if current_val != '.' and (k, l) != (i, j):
-        #                 # print("current_val = {}, possible = {}".format(current_val, possible))
-        #                 possible.remove(int(current_val))
-        #     for k in range(row):
-        #         if board[k][j] != "." and k != i:
-        #             if int(board[k][j]) in possible:
-        #                 possible.remove(int(board[k][j]))
-        #     for k in range(column):
-        #         if board[i][k] != "." and k != j:
-        #             if int(board[i][k]) in possible:
-        #                 possible.remove(int(board[i][k]))
-        #     if len(possible) == 1:
-        #         board[i][j] = str(possible.pop())
-        #     else:
-        #         not_solved.add((i, j))
+        def find_possible_values(i, j):
+            # print('i = {}, j = {}'.format(i, j))
+            nonlocal count
+            sub_box_row, sub_box_column = (i//3)*3, (j//3)*3
+            possible = set(range(1, 10))
+            for k in range(sub_box_row, sub_box_row+3):
+                for l in range(sub_box_column, sub_box_column+3):
+                    current_val = board[k][l]
+                    # print(k, l, current_val)
+                    if current_val != '.' and (k, l) != (i, j):
+                        # print("current_val = {}, possible = {}".format(current_val, possible))
+                        possible.remove(int(current_val))
+            for k in range(row):
+                if board[k][j] != "." and k != i:
+                    if int(board[k][j]) in possible:
+                        possible.remove(int(board[k][j]))
+            for k in range(column):
+                if board[i][k] != "." and k != j:
+                    if int(board[i][k]) in possible:
+                        possible.remove(int(board[i][k]))
+            if len(possible) == 1:
+                board[i][j] = str(possible.pop())
+                pprint.pprint(board)
+                count += 1
+                print(count)
+            else:
+                not_solved.append((i, j))
 
-        # while not_solved:
-        #     i, j = not_solved.pop()
-        #     find_possible_values(i, j)
+        while not_solved:
+            i, j = not_solved.popleft()
+            find_possible_values(i, j)
         
-    def solveSudoku(self, board):
-        self.board = board
-        self.solve()
+#from leetcode, accepted
+    # def solveSudoku(self, board):
+    #     self.board = board
+    #     self.solve()
     
-    def findUnassigned(self):
-        for row in range(9):
-            for col in range(9):
-                if self.board[row][col] == ".":
-                    return row, col
-        return -1, -1
+    # def findUnassigned(self):
+    #     for row in range(9):
+    #         for col in range(9):
+    #             if self.board[row][col] == ".":
+    #                 return row, col
+    #     return -1, -1
     
-    def solve(self):
-        row, col = self.findUnassigned()
-        #no unassigned position is found, puzzle solved
-        if row == -1 and col == -1:
-            return True
-        for num in ["1","2","3","4","5","6","7","8","9"]:
-            if self.isSafe(row, col, num):
-                self.board[row][col] = num
-                if self.solve():
-                    return True
-                self.board[row][col] = "."
-        return False
+    # def solve(self):
+    #     row, col = self.findUnassigned()
+    #     #no unassigned position is found, puzzle solved
+    #     if row == -1 and col == -1:
+    #         return True
+    #     for num in ["1","2","3","4","5","6","7","8","9"]:
+    #         if self.isSafe(row, col, num):
+    #             self.board[row][col] = num
+    #             if self.solve():
+    #                 return True
+    #             self.board[row][col] = "."
+    #     return False
             
-    def isSafe(self, row, col, ch):
-        boxrow = row - row%3
-        boxcol = col - col%3
-        if self.checkrow(row,ch) and self.checkcol(col,ch) and self.checksquare(boxrow, boxcol, ch):
-            return True
-        return False
+    # def isSafe(self, row, col, ch):
+    #     boxrow = row - row%3
+    #     boxcol = col - col%3
+    #     if self.checkrow(row,ch) and self.checkcol(col,ch) and self.checksquare(boxrow, boxcol, ch):
+    #         return True
+    #     return False
     
-    def checkrow(self, row, ch):
-        for col in range(9):
-            if self.board[row][col] == ch:
-                return False
-        return True
+    # def checkrow(self, row, ch):
+    #     for col in range(9):
+    #         if self.board[row][col] == ch:
+    #             return False
+    #     return True
     
-    def checkcol(self, col, ch):
-        for row in range(9):
-            if self.board[row][col] == ch:
-                return False
-        return True
+    # def checkcol(self, col, ch):
+    #     for row in range(9):
+    #         if self.board[row][col] == ch:
+    #             return False
+    #     return True
        
-    def checksquare(self, row, col, ch):
-        for r in range(row, row+3):
-            for c in range(col, col+3):
-                if self.board[r][c] == ch:
-                    return False
-        return True
+    # def checksquare(self, row, col, ch):
+    #     for r in range(row, row+3):
+    #         for c in range(col, col+3):
+    #             if self.board[r][c] == ch:
+    #                 return False
+    #     return True
 
-board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
-Output = [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+# board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# Output = [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
 
 board = [[".",".","9","7","4","8",".",".","."],["7",".",".",".",".",".",".",".","."],[".","2",".","1",".","9",".",".","."],[".",".","7",".",".",".","2","4","."],[".","6","4",".","1",".","5","9","."],[".","9","8",".",".",".","3",".","."],[".",".",".","8",".","3",".","2","."],[".",".",".",".",".",".",".",".","6"],[".",".",".","2","7","5","9",".","."]]
-Output = None
+Output = [['5', '1', '9', '7', '4', '8', '6', '3', '2'], ['7', '8', '3', '6', '5', '2', '4', '1', '9'], ['4', '2', '6', '1', '3', '9', '8', '7', '5'], ['3', '5', '7', '9', '8', '6', '2', '4', '1'], ['2', '6', '4', '3', '1', '7', '5', '9', '8'], ['1', '9', '8', '5', '2', '4', '3', '6', '7'], ['9', '7', '5', '8', '6', '3', '1', '2', '4'], ['8', '3', '2', '4', '9', '1', '7', '5', '6'], ['6', '4', '1', '2', '7', '5', '9', '8', '3']]
 
 pprint.pprint(board)
 
 sol = Solution()
 sol.solveSudoku(board)
-print(board)
+pprint.pprint(board)
 print(board == Output)
