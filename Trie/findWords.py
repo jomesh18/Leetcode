313a331c673cc260318417a774f6869cecb41124
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-09-25 19:00:53 +0530

    added 25.shortestPath.py

--- Trie/findWords.py
+++ Trie/findWords.py
@@ -31,51 +31,126 @@ Constraints:
     All the strings of words are unique.
 
 '''
-class TrieNode:
-    def __init__(self):
-        self.neighbors = {}
-        self.isWord = False
+#taking too much time
+# class TrieNode:
+#     def __init__(self):
+#         self.neighbors = {}
+#         self.isWord = False
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
+# class Trie:
+#     def __init__(self):
+#         self.root = TrieNode()
 
-    def addWord(self, word):
-        current = self.root
-        for w in word:
-            current = current.neighbors.setdefault(w, TrieNode())
-        current.isWord = True
+#     def addWord(self, word):
+#         current = self.root
+#         for w in word:
+#             current = current.neighbors.setdefault(w, TrieNode())
+#         current.isWord = True
+
+# class Solution:
+#     def findWords(self, board: [[str]], words: [str]) -> [str]:
+#         trie = Trie()
+#         res = []
+#         node = trie.root
+#         for word in words:
+#             trie.addWord(word)
+#         # print(print_trie(trie))
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 self.dfs(node, i, j, board, res, "")
+#         return res
+
+#     def dfs(self, node, i, j, board, res, path):
+#         if node.isWord:
+#             res.append(path)
+#             node.isWord = False
+#         if i<0 or i>=len(board) or j<0 or j>=len(board[0]): return
+#         temp = board[i][j]
+#         if temp == "#": return
+#         if temp not in node.neighbors: return
+#         node = node.neighbors[temp]
+#         board[i][j] = "#"
+#         for k, l in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+#             self.dfs(node, i+k, j+l, board, res, path+temp)
+#         board[i][j] = temp
+#         return res
 
 class Solution:
     def findWords(self, board: [[str]], words: [str]) -> [str]:
-        trie = Trie()
+        # parse character and build a set 
+        charSet = set()
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                charSet.add(board[i][j])
+        # build a trie
+        root = {}
+        stop = '$'
         for word in words:
-            trie.addWord(word)
+            skip = False
+            for ch in word:
+                if ch not in charSet:
+                    skip = True
+                    break
+            if skip: # next word
+                continue
+            
+            node = root
+            for ch in word:
+                node = node.setdefault(ch, {}) 
+            node[stop] = stop
+                
         res = []
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                node = trie.root
-                res.append(self.dfs(node, i, j, board, ""))
+        searched = set()
+        
+        def dfs(i, j, node, string):
+            if stop in node: # find a match word
+                res.append(string)
+                node.pop(stop, None) # avoid re-compute
+            if (i, j, string) in searched:
+                return
+            searched.add((i, j, string))
+            
+            temp , board[i][j] = board[i][j], '#' # prevent for revisiting
+            
+            for nx, ny in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] in node:
+                    dfs(nx, ny, node[board[nx][ny]], string + board[nx][ny])
+                    if not node[board[nx][ny]]:
+                        node.pop(board[nx][ny], None)
+            board[i][j] = temp
+            return
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in root:
+                    node = root
+                    dfs(i, j, node[board[i][j]], board[i][j])
         return res
 
-    def dfs(self, node, i, j, board, path):
-        if node.isWord: return path
-        if i<0 or i>=len(board) or j<0 or j>=len(board[0]): return
-        temp = board[i][j]
-        if temp not in node.neighbors: return
-        node = node.neighbors[temp]
-        board[i][j] = "#"
-        for k, l in ((-1, 0), (0, -1), (0, 1), (1, 0)):
-            self.dfs(node, i+k, j+l, board, path+temp)
-        board[i][j] = temp
-            
+def print_trie(trie):
+    node = trie.root
+    res = []
+    def dfs(node, res, temp):
+        if node.isWord:
+            res.append(temp)
+        for n in node.neighbors:
+            dfs(node.neighbors[n], res, temp+n)
+
+    dfs(node, res, "")
+    return res
+
 board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
 words = ["oath","pea","eat","rain"]
 # # Output: ["eat","oath"]
 
-# board = [["a","b"],["c","d"]]
-# words = ["abcb"]
-# # Output: []
+board = [["a","b"],["c","d"]]
+words = ["abcb"]
+# # # # Output: []
+
+board = [["a","b"],["c","d"]]
+words = ["abd", "acd", "abcb"]
+# # # # Output: ["abd", "acd"]
 
 sol = Solution()
 print(sol.findWords(board, words))
