54c83501323e3757947b6c93d76a008e6a6cab9a
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-10-09 19:36:48 +0530

    added findwords.py

---
+++ Leetcode_challenge/10.October 2021/9.findWords.py
@@ -0,0 +1,101 @@
+'''
+212. Word Search II
+Hard
+
+Given an m x n board of characters and a list of strings words, return all words on the board.
+
+Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+ 
+
+Example 1:
+
+Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
+Output: ["eat","oath"]
+
+Example 2:
+
+Input: board = [["a","b"],["c","d"]], words = ["abcb"]
+Output: []
+
+ 
+
+Constraints:
+
+    m == board.length
+    n == board[i].length
+    1 <= m, n <= 12
+    board[i][j] is a lowercase English letter.
+    1 <= words.length <= 3 * 104
+    1 <= words[i].length <= 10
+    words[i] consists of lowercase English letters.
+    All the strings of words are unique.
+
+'''
+from collections import Counter
+class Trie:
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            node = node.setdefault(c, {})
+        node["#"] = "#"
+
+class Solution:
+    def findWords(self, board: [[str]], words: [str]) -> [str]:
+        def dfs(node, board, i, j, temp):
+            if node.get("#"):
+                node.pop("#")
+                res.append(temp)
+            if (i, j, temp) in searched:
+                return
+            searched.add((i, j, temp))
+            t, board[i][j] = board[i][j], "$"
+            for k, l in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+                if 0<=k<m and 0<=l<n and board[k][l] in node:
+                    dfs(node[board[k][l]], board, k, l, temp+board[k][l])
+                    if not node[board[k][l]]:
+                        node.pop(board[k][l])
+            board[i][j] = t
+        searched = set()
+        trie = Trie()
+        count = Counter()
+        for row in board:
+            count += Counter(row)
+        for word in words:
+            coun = Counter(word)
+            skip = False
+            for key in coun:
+                if coun[key] > count[key]:
+                    skip = True
+            if not skip:
+                trie.insert(word)
+
+        node = trie.root
+        res = []
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                char = board[i][j]
+                if char in node:
+                    dfs(node[char], board, i, j, char)
+        return res
+
+board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+words = ["oath","pea","eat","rain"]
+# Output: ["eat","oath"]
+
+# board = [["a","b"],["c","d"]]
+# words = ["abcb"]
+# # # Output: []
+
+board = [["o","a","a","o"],["e","t","a","a"],["i","a","k","t"],["i","f","l","h"]]
+words = ["oath","pea","eat","rain"]
+
+board = [["a","b","c","e"],["z","z","d","z"],["z","z","c","z"],["z","a","b","z"]]
+words = ["abcdce"]
+
+sol = Solution()
+print(sol.findWords(board, words))
--- Trie/findWords.py
+++ Trie/findWords.py
@@ -78,61 +78,61 @@ Constraints:
 
 #from leetcode fastest, <100ms
 # import pprint
-# class Solution:
-#     def findWords(self, board: [[str]], words: [str]) -> [str]:
-#         # parse character and build a set 
-#         charSet = set()
-#         m, n = len(board), len(board[0])
-#         for i in range(m):
-#             for j in range(n):
-#                 charSet.add(board[i][j])
-#         # build a trie
-#         root = {}
-#         stop = '$'
-#         for word in words:
-#             skip = False
-#             for ch in word:   
-#                 if ch not in charSet:
-#                     skip = True
-#                     break
-#             if skip: # next word
-#                 continue
+class Solution:
+    def findWords(self, board: [[str]], words: [str]) -> [str]:
+        # parse character and build a set 
+        charSet = set()
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                charSet.add(board[i][j])
+        # build a trie
+        root = {}
+        stop = '$'
+        for word in words:
+            skip = False
+            for ch in word:   
+                if ch not in charSet:
+                    skip = True
+                    break
+            if skip: # next word
+                continue
             
-#             node = root
-#             for ch in word:
-#                 node = node.setdefault(ch, {}) 
-#             node[stop] = stop
-#         pprint.pprint(root)
-#         res = []
-#         searched = set()
-#         # searched = {}
+            node = root
+            for ch in word:
+                node = node.setdefault(ch, {}) 
+            node[stop] = stop
+        # pprint.pprint(root)
+        res = []
+        searched = set()
+        # searched = {}
         
-#         def dfs(i, j, node, string):
-#             if stop in node: # find a match word
-#                 res.append(string)
-#                 node.pop(stop, None) # avoid re-compute
-#             if (i, j, string) in searched:
-#                 return
-#             # searched[(i, j, string)] = searched.setdefault((i, j, string), 0) + 1
+        def dfs(i, j, node, string):
+            if stop in node: # find a match word
+                res.append(string)
+                node.pop(stop, None) # avoid re-compute
+            if (i, j, string) in searched:
+                return
+            searched[(i, j, string)] = searched.setdefault((i, j, string), 0) + 1
             
-#             temp , board[i][j] = board[i][j], '#' # prevent for revisiting
+            temp , board[i][j] = board[i][j], '#' # prevent for revisiting
             
-#             for nx, ny in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-#                 if 0 <= nx < m and 0 <= ny < n and board[nx][ny] in node:
-#                     dfs(nx, ny, node[board[nx][ny]], string + board[nx][ny])
-#                     if not node[board[nx][ny]]:
-#                         print("before {}".format(root))
-#                         node.pop(board[nx][ny], None)
-#                         print("after {}".format(root))
-#             board[i][j] = temp
-#             return
+            for nx, ny in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] in node:
+                    dfs(nx, ny, node[board[nx][ny]], string + board[nx][ny])
+                    if not node[board[nx][ny]]:
+                        print("before {}".format(root))
+                        node.pop(board[nx][ny], None)
+                        print("after {}".format(root))
+            board[i][j] = temp
+            return
         
-#         for i in range(m):
-#             for j in range(n):
-#                 if board[i][j] in root:
-#                     node = root
-#                     dfs(i, j, node[board[i][j]], board[i][j])
-#         return res
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in root:
+                    node = root
+                    dfs(i, j, node[board[i][j]], board[i][j])
+        return res
 
 
 
@@ -221,52 +221,52 @@ Constraints:
 
 
 #leetcode, fastest my try
-import pprint
-from collections import Counter
-class Solution:
-    def findWords(self, board, words):
-        root = {}
-        count = Counter()
-        for c in board:
-            count += Counter(c)
-        for word in words:
-            wc = Counter(word)
-            #inserting words to trie
-            for c in word:
-                skip = False
-                if wc[c] > count[c]:
-                    skip = True
-                    break
-            if skip:
-                    continue # skip word
-            node = root
-            for c in word:
-                node = node.setdefault(c, {})
-            node[None] = True
-        # pprint.pprint(root)
-        # print(print_trie(root))
-        def dfs(board, node, i, j, temp_r):
-            # print(board, node, i, j, temp_r)
-            if node.pop(None, None):
-                res.append("".join(temp_r))
-            temp = board[i][j]
-            board[i][j] = "#"
-            for ix, iy in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
-                if 0<=ix<len(board) and 0<=iy<len(board[0]) and board[ix][iy] in node:
-                    temp_r.append(board[ix][iy])
-                    dfs(board, node[board[ix][iy]], ix, iy, temp_r)
-                    if not node[board[ix][iy]]:
-                        node.pop(board[ix][iy], None)
-                    temp_r.pop()
-            board[i][j] = temp
+# import pprint
+# from collections import Counter
+# class Solution:
+#     def findWords(self, board, words):
+#         root = {}
+#         count = Counter()
+#         for c in board:
+#             count += Counter(c)
+#         for word in words:
+#             wc = Counter(word)
+#             #inserting words to trie
+#             for c in word:
+#                 skip = False
+#                 if wc[c] > count[c]:
+#                     skip = True
+#                     break
+#             if skip:
+#                     continue # skip word
+#             node = root
+#             for c in word:
+#                 node = node.setdefault(c, {})
+#             node[None] = True
+#         # pprint.pprint(root)
+#         # print(print_trie(root))
+#         def dfs(board, node, i, j, temp_r):
+#             # print(board, node, i, j, temp_r)
+#             if node.pop(None, None):
+#                 res.append("".join(temp_r))
+#             temp = board[i][j]
+#             board[i][j] = "#"
+#             for ix, iy in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+#                 if 0<=ix<len(board) and 0<=iy<len(board[0]) and board[ix][iy] in node:
+#                     temp_r.append(board[ix][iy])
+#                     dfs(board, node[board[ix][iy]], ix, iy, temp_r)
+#                     if not node[board[ix][iy]]:
+#                         node.pop(board[ix][iy], None)
+#                     temp_r.pop()
+#             board[i][j] = temp
 
 
-        res = []
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] in root:
-                    dfs(board, root[board[i][j]], i, j, [board[i][j]])
-        return res
+#         res = []
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 if board[i][j] in root:
+#                     dfs(board, root[board[i][j]], i, j, [board[i][j]])
+#         return res
 
 
 # def print_trie(trie):
