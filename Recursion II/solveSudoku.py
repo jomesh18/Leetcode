b2f61a4d8bf809ef32e82428c31cbae90716d691
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-10-15 09:47:13 +0530

    updated solveSudoku

--- Recursion II/solveSudoku.py
+++ Recursion II/solveSudoku.py
@@ -47,7 +47,7 @@ class Solution:
             for candidate in candidates:
                 place_candidate(i, j, candidate)
                 backtrack()
-                if self.finished: break
+                if self.finished: return
                 remove_candidate(i, j, candidate)
             to_fill.append((i, j))
 
@@ -67,11 +67,48 @@ class Solution:
 
         backtrack()
 
+#from leetcode
+# from collections import defaultdict
+# class Solution:
+#     def solveSudoku(self, board: [[str]]) -> None:
+#         rows, cols, triples, visit = defaultdict(set), defaultdict(set), defaultdict(set), []
+#         for r in range(9):
+#             for c in range(9):
+#                 if board[r][c] != ".":
+#                     rows[r].add(board[r][c])
+#                     cols[c].add(board[r][c])
+#                     triples[(r // 3, c // 3)].add(board[r][c])
+#                 else:
+#                     visit.append((r, c))
+#         def dfs():
+#             if not visit:
+#                 return True
+#             r, c = visit.pop()
+#             t = (r // 3, c // 3)
+#             for dig in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
+#                 if dig not in rows[r] and dig not in cols[c] and dig not in triples[t]:
+#                     board[r][c] = dig
+#                     rows[r].add(dig)
+#                     cols[c].add(dig)
+#                     triples[t].add(dig)
+#                     # visit.pop()
+#                     if dfs():
+#                         return True
+#                     else:
+#                         board[r][c] = "."
+#                         rows[r].discard(dig)
+#                         cols[c].discard(dig)
+#                         triples[t].discard(dig)
+#                         # visit.append((r, c))
+#             visit.append((r, c))
+#             return False
+#         dfs()
+
 board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 Output = [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
 
 pprint.pprint(board)
-# sol = Solution()
+sol = Solution()
 sol.solveSudoku(board)
-# pprint.pprint(board)
+pprint.pprint(board)
 print(board == Output)
