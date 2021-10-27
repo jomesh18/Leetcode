ad56374ac838bbd2039cf50eded42e38a8504286
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-10-27 14:01:15 +0530

    updated acc.py

--- acc.py
+++ acc.py
@@ -1,4 +1,3 @@
-import pyinputplus as pyip
 d = {'a21s': 1, 
     'a71': 1, 
     'poco x3': 1, 
@@ -19,5 +18,53 @@ d = {'a21s': 1,
     'oppo a9 2020': 3
     }
 
-inp = input().lower()
-print(d.get(inp))
+from tkinter import *
+from tkinter import ttk
+
+class FindAccBox:
+
+    def __init__(self, root):
+        root.title("Sahayee Acc")
+        root.geometry("500x200")
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        mainframe = ttk.Frame(root, padding="3 3 12 12", width = 500, height = 200)
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        self.item = StringVar()
+        item_entry = ttk.Entry(mainframe, width=7, textvariable=self.item)
+        item_entry.grid(column=3, row=1, sticky=(W, E))
+
+        self.box = StringVar()
+        ttk.Label(mainframe, textvariable=self.box).grid(column=2, row=2, sticky=(W, E))
+
+        ttk.Button(mainframe, text="Search", command=self.get_box).grid(column=3, row=3, sticky=W)
+
+        # ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+        ttk.Label(mainframe, text="is in box").grid(column=1, row=2, sticky=E)
+        # ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+
+        item_entry.focus()
+        root.bind("<Return>", self.get_box)
+
+
+    def get_box(self, *args):
+        key = self.item.get().lower()
+        if key in d:
+            return self.box.set(d[key])
+
+    def add_to_dictionary(self):
+        key, box = input()
+        d[key] = box
+
+    def delete_from_dictionary(self):
+        key = input()
+        if key in d:
+            del d[key]
+
+root = Tk()
+FindAccBox(root)
+root.mainloop()
