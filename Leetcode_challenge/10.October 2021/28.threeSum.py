4358d883f9971fd30c380b0b23426440eacc11ca
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-10-29 17:03:26 +0530

    updated threeSum.py

---
+++ Leetcode_challenge/10.October 2021/28.threeSum.py
@@ -0,0 +1,77 @@
+'''
+15. 3Sum
+Medium
+
+13912
+
+1333
+
+Add to List
+
+Share
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+ 
+
+Constraints:
+
+0 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+Accepted
+1,563,538
+Submissions
+5,254,597
+'''
+
+class Solution:
+    def threeSum(self, nums: [int]) -> [[int]]:
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        N, result = len(nums), []
+        for i in range(N):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            target = nums[i]*-1
+            s,e = i+1, N-1
+            while s<e:
+                if nums[s]+nums[e] == target:
+                    result.append([nums[i], nums[s], nums[e]])
+                    s = s+1
+                    while s<e and nums[s] == nums[s-1]:
+                        s = s+1
+                elif nums[s] + nums[e] < target:
+                    s = s+1
+                else:
+                    e = e-1
+        return result
+
+nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+# nums = []
+# Output: []
+
+# nums = [0]
+# Output: []
+
+sol = Solution()
+print(sol.threeSum(nums))
