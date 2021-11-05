c3d8c6d90e84ab25c2a0b9d65128fe46c3260f1d
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-11-05 09:41:35 +0530

    updated largestRectangleArea.py

--- Recursion II/largestRectangleArea.py
+++ Recursion II/largestRectangleArea.py
@@ -27,4 +27,34 @@ Constraints:
 '''
 class Solution:
     def largestRectangleArea(self, heights: [int]) -> int:
-        
+        stack, area = [], 0
+        i = 0
+        while i < len(heights):
+            while i<len(heights) and (not stack or stack[-1][0] <= heights[i]):
+                stack.append((heights[i], i))
+                i += 1
+                # print(i)
+            while stack and i < len(heights) and stack[-1][0] >= heights[i]:
+                h, ind = stack.pop()
+                area = max(area, h*(i-ind))
+
+        while stack:
+            h, ind = stack.pop()
+            area = max(area, h*(i-ind))
+
+        return area
+
+heights = [2,1,5,6,2,3]
+# Output: 10
+
+heights = [2,4]
+Output: 4
+
+# heights = [0,0]
+# # Output: 0
+
+heights = [2,1,2]
+#Output: 3
+
+sol = Solution()
+print(sol.largestRectangleArea(heights))
