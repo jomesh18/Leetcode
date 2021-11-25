f8badb60f7d17fb56fcc39e93ed0792f60d8b091
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-11-25 19:22:25 +0530

    updated topKFrequent.py

--- Hash Table/topKFrequent.py
+++ Hash Table/topKFrequent.py
@@ -27,46 +27,75 @@ Constraints:
 
 Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 '''
-class Solution:
-    def topKFrequent(self, nums: [int], k: int) -> [int]:
-        freq = {}
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-        res = [(freq[key], key) for key in freq]
-        res.sort(reverse=True)
-        res = res[:k]
-        return [r[1] for r in res]
+# class Solution:
+#     def topKFrequent(self, nums: [int], k: int) -> [int]:
+#         freq = {}
+#         for num in nums:
+#             freq[num] = freq.get(num, 0) + 1
+#         res = [(freq[key], key) for key in freq]
+#         res.sort(reverse=True)
+#         res = res[:k]
+#         return [r[1] for r in res]
 
 
-#leetcode, fastest
-import heapq
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = collections.defaultdict(int)
-        for num in nums:
-            d[num] += 1
+# #leetcode, fastest
+# from collections import defaultdict
+# import heapq
+# class Solution:
+#     def topKFrequent(self, nums: [int], k: int) -> [int]:
+#         d = defaultdict(int)
+#         for num in nums:
+#             d[num] += 1
         
-        h = []
-        # Key = number, val is the count
-        for key, val in d.items():
-            if not h or len(h) < k: 
-                heapq.heappush(h, (val, key))
-            else:
-                if h[0][0] < val:
-                    heapq.heappop(h)
-                    heapq.heappush(h, (val, key))
+#         h = []
+#         # Key = number, val is the count
+#         for key, val in d.items():
+#             if not h or len(h) < k: 
+#                 heapq.heappush(h, (val, key))
+#             else:
+#                 if h[0][0] < val:
+#                     heapq.heappop(h)
+#                     heapq.heappush(h, (val, key))
                     
         
-        out = []
-        for _ in range(k):
-            curr = heapq.heappop(h)
-            out.append(curr[1])
-        return out
+#         out = []
+#         for _ in range(k):
+#             curr = heapq.heappop(h)
+#             out.append(curr[1])
+#         return out
 
+
+#leetcode fastest my try
+import heapq
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: [int], k: int) -> [int]:
+        count = Counter(nums)
+        l = []
+        heapq.heapify(l)
+        i = 0
+        for num, count_ in count.items():
+            if i < k:
+                i += 1
+                heapq.heappush(l, (count_,num))
+            else:
+                if l[0][0] < count_:
+                    heapq.heappop(l)
+                    heapq.heappush(l, (count_, num))
+        # print(l)
+        return [elem[1] for elem in l]
 
 
 nums = [3,0,1,0]
 k = 1
+
+nums = [1,1,1,2,2,3]
+k = 2
+# Output: [1,2]
+
+# nums = [1]
+# k = 1
+# Output: [1]
 
 sol = Solution()
 print(sol.topKFrequent(nums, k))
--- Leetcode_challenge/11.November 2021/24.intervalIntersection.py
+++ Leetcode_challenge/11.November 2021/24.intervalIntersection.py
@@ -51,17 +51,56 @@ Accepted
 Submissions
 348,409
 '''
+#from leetcode solution
+# class Solution:
+#     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+#         ans = []
+#         i = j = 0
+#         while i < len(firstList) and j < len(secondList):
+#             lo = max(firstList[i][0], secondList[j][0])
+#             hi = min(firstList[i][1],secondList[j][1])
+#             if lo <= hi:
+#                 ans.append([lo, hi])
+#             if firstList[i][1] <secondList[j][1]:
+#                 i += 1
+#             else:
+#                 j += 1
+#         return ans
+
+
+
+#my try of leetcode solution
 class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        ans = []
-        i = j = 0
+    def intervalIntersection(self, firstList: [[int]], secondList: [[int]]) -> [[int]]:
+        i, j = 0, 0
+        res = []
         while i < len(firstList) and j < len(secondList):
             lo = max(firstList[i][0], secondList[j][0])
-            hi = min(firstList[i][1],secondList[j][1])
+            hi = min(firstList[i][1], secondList[j][1])
             if lo <= hi:
-                ans.append([lo, hi])
-            if firstList[i][1] <secondList[j][1]:
+                res.append([lo, hi])
+            if firstList[i][1] < secondList[j][1]:
                 i += 1
             else:
                 j += 1
-        return ans
+        return res
+
+
+firstList = [[0,2],[5,10],[13,23],[24,25]]
+secondList = [[1,5],[8,12],[15,24],[25,26]]
+# Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+# firstList = [[1,3],[5,9]]
+# secondList = []
+# Output: []
+
+# firstList = []
+# secondList = [[4,8],[10,12]]
+# Output: []
+
+# firstList = [[1,7]]
+# secondList = [[3,10]]
+# Output: [[3,7]]
+
+sol = Solution()
+print(sol.intervalIntersection(firstList, secondList))
