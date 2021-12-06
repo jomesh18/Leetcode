addaa00f86a5d03d45b4c7a69d9501713b0663fb
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-12-06 20:01:07 +0530

    added singleNumber.py

--- Leetcode_challenge/12. December 2021/5.rob.py
+++ Leetcode_challenge/12. December 2021/5.rob.py
@@ -43,15 +43,6 @@ The number of nodes in the tree is in the range [1, 104].
 #         self.left = left
 #         self.right = right
 
-#from leetcode
-class Solution:
-    def rob(self, root) -> int:
-        def dfs(root):
-            if not root: return (0, 0)
-            L, R = dfs(root.left), dfs(root.right)
-            return (max(L) + max(R), root.val + L[0] + R[0])
-        return max(dfs(root))
-
 
 #recursive, brute force
 class Solution:
@@ -85,6 +76,7 @@ class Solution:
             return d[(root, canRob)]
         return helper(root, True)
 
+#second with memo
 class Solution:
     def __init__(self):
         self.d = {}
@@ -96,6 +88,30 @@ class Solution:
         if root.right: robbing_current += self.rob(root.right.left) + self.rob(root.right.right)
         self.d[root] = max(robbing_current, not_robbing_current)
         return self.d[root]
+
+
+#from leetcode, space optimized dp
+'''
+Step III -- Think one step back
+
+In step I, we defined our problem as rob(root), which will yield the maximum amount of money that can be robbed of the binary tree rooted at root. This leads to the DP problem summarized in step II.
+
+Now let's take one step back and ask why we have overlapping subproblems. If you trace all the way back to the beginning, you'll find the answer lies in the way how we have defined rob(root). As I mentioned, for each tree root, there are two scenarios: it is robbed or is not. rob(root) does not distinguish between these two cases, so "information is lost as the recursion goes deeper and deeper", which results in repeated subproblems.
+
+If we were able to maintain the information about the two scenarios for each tree root, let's see how it plays out. Redefine rob(root) as a new function which will return an array of two elements, the first element of which denotes the maximum amount of money that can be robbed if root is not robbed, while the second element signifies the maximum amount of money robbed if it is robbed.
+
+Let's relate rob(root) to rob(root.left) and rob(root.right)..., etc. For the 1st element of rob(root), we only need to sum up the larger elements of rob(root.left) and rob(root.right), respectively, since root is not robbed and we are free to rob its left and right subtrees. For the 2nd element of rob(root), however, we only need to add up the 1st elements of rob(root.left) and rob(root.right), respectively, plus the value robbed from root itself, since in this case it's guaranteed that we cannot rob the nodes of root.left and root.right.
+
+As you can see, by keeping track of the information of both scenarios, we decoupled the subproblems and the solution essentially boiled down to a greedy one. Here is the program:
+'''
+class Solution:
+    def rob(self, root) -> int:
+        def dfs(root):
+            if not root: return (0, 0)
+            L, R = dfs(root.left), dfs(root.right)
+            return (max(L) + max(R), root.val + L[0] + R[0])
+        return max(dfs(root))
+
 
 null = None
 
