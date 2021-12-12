a117792436fa07de4f7e5e47b6e4c88c7469238c
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-12-12 21:10:37 +0530

    Create 12.canPartition.py

---
+++ Leetcode_challenge/12. December 2021/12.canPartition.py
@@ -0,0 +1,46 @@
+'''
+416. Partition Equal Subset Sum
+Medium
+
+6243
+
+101
+
+Add to List
+
+Share
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+Accepted
+370,280
+Submissions
+805,945
+'''
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        if total_sum & 1: return False
+        half_sum = total_sum // 2
+        dp = [True] + [False]*half_sum
+        for num in nums:
+            for j in range(half_sum, num-1, -1):
+                dp[j] |= dp[j-num]
+        return dp[half_sum]
