214f60c7d618edb76fb3d686c68799c15e8c25b4
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-12-13 18:55:56 +0530

    added 13.maxPower.py

--- Leetcode_challenge/12. December 2021/12.canPartition.py
+++ Leetcode_challenge/12. December 2021/12.canPartition.py
@@ -34,13 +34,47 @@ Accepted
 Submissions
 805,945
 '''
-class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        total_sum = sum(nums)
-        if total_sum & 1: return False
-        half_sum = total_sum // 2
-        dp = [True] + [False]*half_sum
-        for num in nums:
-            for j in range(half_sum, num-1, -1):
-                dp[j] |= dp[j-num]
-        return dp[half_sum]
+#brute force, O(2**n)
+# class Solution:
+#     def canPartition(self, nums: [int]) -> bool:
+#         def helper(i, sum1, sum2):
+#             # print(sum1, sum2)
+#             if i >= len(nums): return sum1 == sum2
+#             return helper(i+1, sum1+nums[i], sum2) or helper(i+1, sum1, sum2+nums[i])
+#         return helper(0, sum1=0, sum2=0)
+
+#optimized brute force (return if sum in odd)
+# class Solution:
+#     def canPartition(self, nums: [int]) -> bool:
+#         def subsetSum(i, s):
+#             if s == 0: return True
+#             if i >= len(nums) or s<0: return False
+#             return subsetSum(i+1, s-nums[i]) or subsetSum(i+1, s)
+#         total_sum = sum(nums)
+#         return total_sum & 1 == 0 and subsetSum(0, total_sum//2)
+
+# class Solution:
+#     def canPartition(self, nums: [int]) -> bool:
+#         dp = {}
+#         def subsetSum(i, s):
+#             if s == 0: return True
+#             if i >= len(nums) or s<0: return False
+#             if (i, s) in dp: return dp[(i, s)]
+#             ans = subsetSum(i+1, s-nums[i]) or subsetSum(i+1, s)
+#             dp[(i, s)] = ans
+#             return ans
+#         total_sum = sum(nums)
+#         return total_sum & 1 == 0 or subsetSum(total_sum//2)
+
+
+nums = [1,5,11,5]
+# Output: true
+
+# nums = [1,2,3,5]
+# Output: false
+
+nums = [i for i in range(1, 201)]
+
+
+sol = Solution()
+print(sol.canPartition(nums))
