45c447d3a477e0f6f1ffaa662ee4142bf4390702
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2021-12-29 19:02:42 +0530

    added 29.connect.py

--- Leetcode_challenge/12. December 2021/25.calculate.py
+++ Leetcode_challenge/12. December 2021/25.calculate.py
@@ -41,6 +41,72 @@ s represents a valid expression.
 All the integers in the expression are non-negative integers in the range [0, 231 - 1].
 The answer is guaranteed to fit in a 32-bit integer.
 '''
+#using stack, O(n) space, time
+# class Solution:
+#     def calculate(self, s: str) -> int:
+#         stack = []
+#         res = 0
+#         curr_num = 0
+#         operation = '+'
+#         for i in range(len(s)):
+#             c = s[i]
+#             if c.isdigit():
+#                 curr_num = curr_num*10 + int(c)
+#             if not c.isdigit() and c != ' ' or i == len(s)-1:
+#                 if operation == "+":
+#                     stack.append(curr_num)
+#                 elif operation == "-":
+#                     stack.append(-curr_num)
+#                 elif operation == "/":
+#                     last_num = stack.pop()
+#                     stack.append(int(last_num/curr_num))
+#                 elif operation == "*":
+#                     last_num = stack.pop()
+#                     stack.append(last_num*curr_num)
+#                 operation = c
+#                 curr_num = 0
+#         while stack:
+#             res += stack.pop()
+#         return res
+
+#O(1) space, O(n) time
 class Solution:
     def calculate(self, s: str) -> int:
-        
+        last_num, curr_num, res, operation = 0, 0, 0, "+"
+        for i in range(len(s)):
+            c = s[i]
+            if c.isdigit():
+                curr_num = curr_num*10+int(c)
+            if not c.isdigit() and c != " " or i == len(s)-1:
+                if operation == "+":
+                    res += last_num
+                    last_num = curr_num
+                elif operation == "-":
+                    res += last_num
+                    last_num = -curr_num
+                elif operation == "/":
+                    last_num = int(last_num/curr_num)
+                elif operation == "*":
+                    last_num = last_num*curr_num
+                operation = c
+                curr_num = 0
+            # print(i, last_num, curr_num, res, operation)
+        return res+last_num
+
+s = "3+2*2"
+# Output: 7
+
+s = " 3/2 "
+# # # # # # Output: 1
+# 
+s = " 3+5 / 2 "
+# # # # # # Output: 5
+
+s = "0-2147483647"
+# # # # # Output: -2147483647
+
+s = "14-3/2"
+# # # Output: 13
+
+sol = Solution()
+print(sol.calculate(s))
---
+++ Leetcode_challenge/12. December 2021/29.connect.py
@@ -0,0 +1,100 @@
+'''
+116. Populating Next Right Pointers in Each Node
+Medium
+
+5252
+
+210
+
+Add to List
+
+Share
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+Example 2:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 212 - 1].
+-1000 <= Node.val <= 1000
+ 
+
+Follow-up:
+
+You may only use constant extra space.
+The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+Accepted
+630,173
+Submissions
+1,158,371
+'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+#iterative
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        next_level_node = root
+        
+        while next_level_node:
+            node = next_level_node
+            next_level_node = next_level_node.left
+            while node:
+                if node.left:
+                    node.left.next = node.right
+                    node.right.next = node.next.left if node.next else None
+                    node = node.next
+                else:
+                    break
+        return root
+
+#leetcode, stefan pochmann, iterative
+class Solution:
+    def connect(self, root):
+        while root and root.left:
+            next = root.left
+            while root:
+                root.left.next = root.right
+                root.right.next = root.next and root.next.left
+                root = root.next
+            root = next
+
+#recursive
+class Solution:
+    def connect(self, root):
+        if root and root.left and root.right:
+            root.left.next = root.right
+            if root.next:
+                root.right.next = root.next.left
+            self.connect(root.left)
+            self.connect(root.right)
+        return root
