d8feb26038d9a3fa340e7a20df966b1f2b24b631
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-01-06 00:34:50 +0530

    Update 05.partition.py

--- Leetcode_challenge/01. Jan 2022/05.partition.py
+++ Leetcode_challenge/01. Jan 2022/05.partition.py
@@ -34,28 +34,76 @@ Accepted
 Submissions
 684,312
 '''
+# class Solution:
+#     def partition(self, s: str) -> [[str]]:
+#         def is_palindrome(beg, end):
+#             while beg < end:
+#                 if s[beg] != s[end]:
+#                     return False
+#                 beg += 1
+#                 end -= 1
+#             return True
+#         self.res = []
+#         def dfs(temp_res, beg, end, inserted):
+#             if end == len(s):
+#                 if inserted:
+#                     self.res.append(temp_res)
+#                 return
+            
+#             if is_palindrome(beg, end):
+#                 dfs(temp_res+[s[beg:end+1]], end+1, end+1, True)
+#             dfs(temp_res, beg, end+1, False)
+                
+#         dfs([], 0, 0, False)
+#         return self.res
+
+
+# class Solution:
+#     def partition(self, s: str) -> [[str]]:
+#         self.res = []
+#         def is_palindrome(beg, end):
+#             while beg < end:
+#                 if s[beg] != s[end]:
+#                     return False
+#                 beg += 1
+#                 end -= 1
+#             return True
+
+#         def dfs(start, temp_res):
+#             if start == len(s): self.res.append(temp_res[:])
+#             for end in range(start, len(s)):
+#                 if is_palindrome(start, end):
+#                     dfs(end+1, temp_res+[s[start:end+1]])
+#         dfs(0, [])
+#         return self.res
+
+
+#using dp to check for palindrome
+# class Solution:
+#     def partition(self, s: str) -> [[str]]:
+#         self.ans = []
+#         self.dp = [[False for _ in range(len(s))] for _ in range(len(s))]
+#         def dfs(start, temp_res):
+#             if start == len(s): self.ans.append(temp_res[:])
+#             for end in range(start, len(s)):
+#                 if s[start] == s[end] and (end-start<=2 or self.dp[start+1][end-1]):
+#                     self.dp[start][end] = True
+#                     dfs(end+1, temp_res+[s[start:end+1]])
+#         dfs(0, [])
+#         return self.ans
+
+#bottom up dp solution
 class Solution:
     def partition(self, s: str) -> [[str]]:
-        def is_palindrome(beg, end):
-            while beg < end:
-                if s[beg] != s[end]:
-                    return False
-                beg += 1
-                end -= 1
-            return True
-        self.res = []
-        def dfs(temp_res, beg, end, inserted):
-            if end == len(s):
-                if inserted:
-                    self.res.append(temp_res)
-                return
-            
-            if is_palindrome(beg, end):
-                dfs(temp_res+[s[beg:end+1]], end+1, end+1, True)
-            dfs(temp_res, beg, end+1, False)
-                
-        dfs([], 0, 0, False)
-        return self.res
+        dp = [[] for _ in range(len(s)+1)]
+        dp[-1] = [[]]
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i+1, len(s)+1):
+                st = s[i:j]
+                if st == st[::-1]:
+                    for each in dp[j]:
+                        dp[i].append([st]+each)
+        return dp[0]
 
 s = "aab"
 # Output: [["a","a","b"],["aa","b"]]
