cf885909a39f05bfb6e7a5dfb70e21648e466316
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-01-17 10:04:34 +0530

    added 17.wordPattern.py

---
+++ Leetcode_challenge/2022/01. Jan/16.maxDistToClosest.py
@@ -0,0 +1,69 @@
+'''
+849. Maximize Distance to Closest Person
+Medium
+
+1728
+
+145
+
+Add to List
+
+Share
+You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 represents that the ith seat is empty (0-indexed).
+
+There is at least one empty seat, and at least one person sitting.
+
+Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized. 
+
+Return that maximum distance to the closest person.
+
+ 
+
+Example 1:
+
+
+Input: seats = [1,0,0,0,1,0,1]
+Output: 2
+Explanation: 
+If Alex sits in the second open seat (i.e. seats[2]), then the closest person has distance 2.
+If Alex sits in any other open seat, the closest person has distance 1.
+Thus, the maximum distance to the closest person is 2.
+Example 2:
+
+Input: seats = [1,0,0,0]
+Output: 3
+Explanation: 
+If Alex sits in the last seat (i.e. seats[3]), the closest person is 3 seats away.
+This is the maximum distance possible, so the answer is 3.
+Example 3:
+
+Input: seats = [0,1]
+Output: 1
+ 
+
+Constraints:
+
+2 <= seats.length <= 2 * 104
+seats[i] is 0 or 1.
+At least one seat is empty.
+At least one seat is occupied.
+'''
+#two pass, O(n) time, O(1) space
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        last_person_position_to_left = float("-inf")
+        for i in range(len(seats)):
+            if seats[i]:
+                last_person_position_to_left = i
+            else:
+                seats[i] = last_person_position_to_left-i
+        last_person_position_to_right = float("inf")
+        max_dist = float("inf")
+        for i in range(len(seats)-1, -1, -1):
+            if seats[i] == 1:
+                last_person_position_to_right = i
+            else:
+                seats[i] = max((i - last_person_position_to_right), seats[i])
+                max_dist = min(seats[i], max_dist)
+        return -max_dist
+                
---
+++ Leetcode_challenge/2022/01. Jan/17.wordPattern.py
@@ -0,0 +1,71 @@
+'''
+290. Word Pattern
+Easy
+
+2578
+
+288
+
+Add to List
+
+Share
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+ 
+
+Constraints:
+
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.
+Accepted
+302,496
+Submissions
+769,066
+'''
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+        if len(pattern) != len(s): return False
+        patt_to_s = {}
+        s_set = ()
+        for i in range(len(pattern)):
+            
+            if pattern[i] not in patt_to_s:
+                if s[i] in s_set: return False
+                patt_to_s[pattern[i]] = s[i]
+                s_set.add(pattern[i])
+            else:
+                if patt_to_s[pattern[i]] != s[i]:
+                    return False
+        return True
+
+
+pattern = "abba"
+s = "dog dog dog dog"
+#Output: false
+
+pattern = "abc"
+s = "b c a"
+#Output: true
+
+sol = 
