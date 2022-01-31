5d6971757e7a21b9811a29c8e46509b5733a0447
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-01-31 09:59:15 +0530

    added 31.maximumWealth..py

---
+++ Leetcode_challenge/2022/01. Jan/30.rotate.py
@@ -0,0 +1,62 @@
+'''
+189. Rotate Array
+Medium
+
+8152
+
+1195
+
+Add to List
+
+Share
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+ 
+
+Follow up:
+
+Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+Accepted
+982,243
+Submissions
+2,573,344
+'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = len(nums)
+        k %= l
+        temp = []
+        for i in range(l-k, l):
+            temp.append(nums[i])
+        for i in range(l-k-1, -1, -1):
+            nums[i+k] = nums[i]
+        for i in range(k):
+            nums[i] = temp[i]
