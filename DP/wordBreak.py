734e3072a26c9c41f94e753cf3ff52c9b4e0e59a
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-02-03 20:08:47 +0530

    Update wordBreak.py

--- DP/wordBreak.py
+++ DP/wordBreak.py
@@ -36,4 +36,34 @@ All the strings of wordDict are unique.
 class Solution:
     def wordBreak(self, s: str, wordDict: [str]) -> bool:
         dp = [False] * len(s)
-        for i in range()
+
+        for i in range(len(s)):
+            for word in wordDict:
+                if s[i] == word[-1]:
+                    l = len(word)
+                    if i - l >= -1 and s[i-l+1:i+1] == word:
+                        if i - l == -1:
+                            dp[i] = True
+                        else:
+                            dp[i] = dp[i] or dp[i-l]
+        print(dp)
+        return dp[-1]
+
+s = "leetcode"
+wordDict = ["leet","code"]
+# Output: true
+
+s = "applepenapple"
+wordDict = ["apple","pen"]
+# Output: true
+
+s = "catsandog"
+wordDict = ["cats","dog","sand","and","cat"]
+# # Output: false
+
+s = "dogs"
+wordDict = ["dog","s","gs"]
+# Output: true
+
+sol = Solution()
+print(sol.wordBreak(s, wordDict))
