c5c4f8837075bcf89c6fdbc78c513ea5f4dbc39a
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-02-05 18:48:40 +0530

    added 05.mergeKLists.py

--- DP/lengthOfLIS.py
+++ DP/lengthOfLIS.py
@@ -31,12 +31,36 @@ Constraints:
 
 Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
 '''
+#O(n*n)
+class Solution:
+    def lengthOfLIS(self, nums: [int]) -> int:
+        l = len(nums)
+        dp = [1] * l
+        ans = 1
+        for i in range(1, l):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1)
+                    ans = max(ans, dp[i])
+        return ans
+
 class Solution:
     def lengthOfLIS(self, nums: [int]) -> int:
 
-
 nums = [10,9,2,5,3,7,101,18]
 # Output: 4
+
+nums = [0,1,0,3,2,3]
+Output: 4
+
+# nums = [7,7,7,7,7,7,7]
+# Output: 1
+
+# nums = [4,10,4,3,8,9]
+# #Output: 3
+
+nums = [1,3,6,7,9,4,10,5,6]
+# Output: 6
 
 sol = Solution()
 print(sol.lengthOfLIS(nums))
