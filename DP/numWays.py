035ebc1f499f7116b4d9fa16d84018108fd95d60
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-02-10 02:22:23 +0530

    edited numWays.py

--- DP/numWays.py
+++ DP/numWays.py
@@ -39,6 +39,30 @@ Explanation:
     way3    1         0          
     way4    1         1       
     '''
+
+#dp
+# class Solution:
+#     """
+#     @param n: non-negative integer, n posts
+#     @param k: non-negative integer, k colors
+#     @return: an integer, the total number of ways
+#     """
+#     def numWays(self, n, k):
+#         # write your code here
+#         if k <= 1 and n > k*2: return 0
+#         if n == 1: return k
+#         dp = [[0]*(n+1) for _ in range(3)]
+#         dp[0][2] = k
+#         dp[1][2] = k*(k-1)
+#         dp[2][2] = dp[0][2]+dp[1][2]
+#         for i in range(3, n+1):
+#             dp[0][i] = dp[1][i-1]
+#             dp[1][i] = dp[2][i-1]*(k-1)
+#             dp[2][i] = dp[0][i]+dp[1][i]
+#         # print(dp)
+#         return dp[2][n]
+
+#memo
 class Solution:
     """
     @param n: non-negative integer, n posts
@@ -47,9 +71,37 @@ class Solution:
     """
     def numWays(self, n, k):
         # write your code here
+        if k <= 1 and n > k*2: return 0
+        if n == 1: return k
+        if n == 2: return k*k
+        dp = [[0]*(n+1) for _ in range(3)]
+        def helper(i, status):
+            if i == 2 and status == 0 : return k
+            if i == 2 and status == 1 : return k*(k-1)
+            if i == 2 and status == 2 : return k*k
+            if dp[status][i] != 0: return dp[status][i]
+            dp[0][i] = helper(i-1, 1)
+            dp[1][i] = helper(i-1, 2) * (k-1)
+            dp[2][i] = dp[0][i] + dp[1][i]
 
-        def helper(i, j):
-            if i == 1: return k
-            if i == 2: return k*k
-            score = helper(i-1, k-1)
-            if 
+            return dp[status][i]
+        return helper(n, 2)
+
+n=3
+k=2  
+# Output: 6
+
+n=2
+k=2  
+# # Output: 4
+
+n = 1
+k = 1
+# # Output: 1
+
+n = 10
+k = 3
+# # Output: 27408
+
+sol = Solution()
+print(sol.numWays(n, k))
