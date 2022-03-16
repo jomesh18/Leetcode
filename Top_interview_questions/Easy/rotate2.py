d343a42b058e5449219ff77ae3d1269ed88b95a1
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-03-16 20:24:05 +0530

    added 16.validateStackSequences.py

--- Top_interview_questions/Easy/rotate2.py
+++ Top_interview_questions/Easy/rotate2.py
@@ -58,3 +58,25 @@ class Solution:
             rt = (rt[0]+1, rt[1]-1)
             rb = (rb[0]-1, rb[1]-1)
             lb = (lb[0]-1, lb[1]+1)
+
+#transpose on main diagonal and reflect right to left
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        def transpose(mat):
+            for i in range(n):
+                for j in range(i+1, n):
+                    mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+                    
+        def reflect(mat):
+            for i in range(n):
+                for j in range(n//2):
+                    mat[i][j], mat[i][-j-1] = mat[i][-j-1], mat[i][j]
+            
+        transpose(matrix)
+        reflect(matrix)
+
+
