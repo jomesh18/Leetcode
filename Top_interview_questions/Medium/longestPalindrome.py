a8dabcd8fd65f5d046191f100f32dd21cc951e26
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-03-26 19:48:38 +0530

    added 26.search.py

--- Top_interview_questions/Medium/longestPalindrome.py
+++ Top_interview_questions/Medium/longestPalindrome.py
@@ -22,25 +22,52 @@ Constraints:
 1 <= s.length <= 1000
 s consist of only digits and English letters.
 '''
+#tle
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         palindrome = ""
+#         def is_palindrome(left, right):
+#             while right > left:
+#                 if s[left] != s[right]:
+#                     return False
+#                 left += 1
+#                 right -= 1
+#             return True
+
+#         for i in range(len(s)):
+#             for j in range(len(s)):
+#                 if is_palindrome(i, j):
+#                     if len(palindrome) < (j+1-i):
+#                         palindrome = s[i:j+1]
+#         return palindrome
+
+
+#accepted
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        palindrome = ""
-        def is_palindrome(left, right):
-            while right > left:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+        n = len(s)
+        dp = [[False]*n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        ans = [0,0]
+        for i in range(n):
+            for j in range(0, i+1):
+                if i != j:
+                    dp[j][i] = (s[i] == s[j] and (dp[j+1][i-1] if (j+1)<=(i-1) else True))
+                if dp[j][i]:
+                    # print(i, j)
+                    if i-j+1 > ans[1]-ans[0]:
+                        ans = [j, i]
+        # print(dp)
+        return s[ans[0]:ans[1]+1]
 
-        for i in range(len(s)):
-            for j in range(len(s)):
-                if is_palindrome(i, j):
-                    if len(palindrome) < (j+1-i):
-                        palindrome = s[i:j+1]
-        return palindrome
+s = "babad"
 
 s="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+# res = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+s = "aacabdkacaa"
+# res = "aca"
 
 print(len(s))
 
