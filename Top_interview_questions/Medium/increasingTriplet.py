404adc051d5038293795ec908303b0b420cc4373
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-04-20 17:38:14 +0530

    added 20.BSTIterator.py

---
+++ Top_interview_questions/Medium/increasingTriplet.py
@@ -0,0 +1,79 @@
+'''
+334. Increasing Triplet Subsequence
+Medium
+
+3784
+
+209
+
+Add to List
+
+Share
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+Accepted
+267,170
+Submissions
+644,147
+'''
+#O(nlogn)
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        sub = []
+        for num in nums:
+            i = bisect.bisect_left(sub, num)
+            if i == len(sub):
+                sub.append(num)
+            else:
+                sub[i] = num
+            if len(sub) >= 3: return True
+        return False
+
+
+#O(n)
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first, second = float("inf"), float("inf")
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else: return True
+        return False
+
+#O(n)
+class Solution:
+    def increasingTriplet(self, nums):
+        a = [float("inf")]*2
+        for elem in nums:
+            if elem < a[0] : a[0] = elem
+            if elem < a[1] and elem > a[0]: a[1] = elem
+            if elem > a[1] : return True
+        return False
