7213d65a1605b978819d91bd3e96bd4df211bf9d
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-05-15 17:39:40 +0530

    Create 15.deepestLeavesSum.py

---
+++ Leetcode_challenge/2022/05. May/15.deepestLeavesSum.py
@@ -0,0 +1,84 @@
+'''
+1302. Deepest Leaves Sum
+Medium
+
+2900
+
+81
+
+Add to List
+
+Share
+Given the root of a binary tree, return the sum of values of its deepest leaves.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15
+Example 2:
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 19
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+1 <= Node.val <= 100
+Accepted
+194,802
+Submissions
+224,849
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#bfs
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        q = [root]
+        while q:
+            s = sum(n.val for n in q)
+            q = [c for n in q for c in [n.left, n.right] if c]
+        return s
+
+#bfs
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        q = [root]
+        while q:
+            pre, q = q, [c for n in q for c in [n.left, n.right] if c]
+        return sum(n.val for n in pre)
+
+#dfs
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        self.s = 0
+        self.max_level = 0
+        
+        def dfs(node, level):
+            if not node: return
+            
+            if level == self.max_level:
+                self.s += node.val
+            elif level > self.max_level:
+                self.s = node.val
+                self.max_level = level
+                
+            dfs(node.left, level+1)
+            dfs(node.right, level+1)
+            
+        dfs(root, 0)
+        return self.s
