0605c82e87f4b6ad30e65e5df3853454acf3ffd8
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-06-02 19:37:19 +0530

    added 02.transpose.py

---
+++ Leetcode_challenge/2022/06. June/02.transpose.py
@@ -0,0 +1,56 @@
+'''
+867. Transpose Matrix
+Easy
+
+1743
+
+386
+
+Add to List
+
+Share
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+1 <= m * n <= 105
+-109 <= matrix[i][j] <= 109
+Accepted
+169,900
+Submissions
+268,875
+'''
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        
+        t_mat = [[0]*m for _ in range(n)]
+        
+        for i in range(m):
+            for j in range(n):
+                t_mat[j][i] = matrix[i][j]
+                
+        return t_mat
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        return zip(*matrix)
