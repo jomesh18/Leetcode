cddd8624b0d3b954cfd03d8cb5cdd37327a21d32
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-06-17 17:56:55 +0530

    Create 17.minCameraCover.py

---
+++ Leetcode_challenge/2022/06. June/17.minCameraCover.py
@@ -0,0 +1,56 @@
+'''
+968. Binary Tree Cameras
+Hard
+
+3312
+
+37
+
+Add to List
+
+Share
+You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a node can monitor its parent, itself, and its immediate children.
+
+Return the minimum number of cameras needed to monitor all nodes of the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+Example 2:
+
+
+Input: root = [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+Node.val == 0
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def dfs(node):
+            if not node: return 2
+            l, r = dfs(node.left), dfs(node.right)
+            if l == 0 or r == 0:
+                self.res += 1
+                return 1
+            if l == 1 or r == 1:
+                return 2
+            else:
+                return 0
+        return (dfs(root) == 0) + self.res
