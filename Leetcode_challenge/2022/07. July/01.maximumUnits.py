d488b33098954e3c0ff377ca19786b757d28dca0
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-07-01 14:44:28 +0530

    Create 01.maximumUnits.py

---
+++ Leetcode_challenge/2022/07. July/01.maximumUnits.py
@@ -0,0 +1,78 @@
+'''
+1710. Maximum Units on a Truck
+Easy
+
+2188
+
+131
+
+Add to List
+
+Share
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+ 
+
+Example 1:
+
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+Explanation: There are:
+- 1 box of the first type that contains 3 units.
+- 2 boxes of the second type that contain 2 units each.
+- 3 boxes of the third type that contain 1 unit each.
+You can take all the boxes of the first and second types, and one box of the third type.
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+Example 2:
+
+Input: boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+Output: 91
+ 
+
+Constraints:
+
+1 <= boxTypes.length <= 1000
+1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+1 <= truckSize <= 106
+Accepted
+175,247
+Submissions
+238,609
+'''
+#O(n)
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key= lambda x:-x[1])
+        ans = 0
+        rem = truckSize
+        for n, qty in boxTypes:
+            if n < rem:
+                rem -= n
+                ans += n*qty
+            else:
+                ans += rem*qty
+                break
+            
+        return ans
+
+#COUNTING SORT O(n) time
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        count = [0 for _ in range(1001)]
+        for n, qty in boxTypes:
+            count[qty] += n
+        ans = 0
+        for qty in range(1000, 0, -1):
+            if count[qty]:
+                fitin = min(count[qty], truckSize)
+                ans += fitin*qty
+                truckSize -= fitin
+                if truckSize == 0:
+                    return ans
+        return ans
