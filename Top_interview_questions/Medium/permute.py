14783e5d58a306b34152362672f3da2395d86652
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-08-24 19:40:15 +0530

    Update permute.py

--- Top_interview_questions/Medium/permute.py
+++ Top_interview_questions/Medium/permute.py
@@ -41,3 +41,16 @@ class Solution:
             
         backtrack([], 0)
         return self.res
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path[:])
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+            
