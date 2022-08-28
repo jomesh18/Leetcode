787949bd01980eefa67f7411e21448e8f74227be
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-08-28 15:24:49 +0530

    Update 28.diagonalSort.py

--- Leetcode_challenge/2022/08. Aug/28.diagonalSort.py
+++ Leetcode_challenge/2022/08. Aug/28.diagonalSort.py
@@ -61,3 +61,23 @@ class Solution:
             collect_sort_replace(r, 0)
         
         return mat
+
+
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        
+        d = defaultdict(list)
+        
+        for i in range(m):
+            for j in range(n):
+                d[i-j].append(mat[i][j])
+        
+        for k in d:
+            d[k].sort(reverse=True)
+            
+        for i in range(m):
+            for j in range(n):
+                mat[i][j] = d[i-j].pop()
+                
+        return mat
