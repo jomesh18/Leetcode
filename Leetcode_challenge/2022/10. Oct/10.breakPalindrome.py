d333dd06b78c71b210398e04c4be7e3559f35d39
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-10 13:30:33 +0530

    Update 10.breakPalindrome.py

--- Leetcode_challenge/2022/10. Oct/10.breakPalindrome.py
+++ Leetcode_challenge/2022/10. Oct/10.breakPalindrome.py
@@ -48,3 +48,11 @@ class Solution:
             for i in range(len(palindrome)):
                 if palindrome[i] != 'a':
                     return palindrome[:i]+'a'+palindrome[i+1:]
+
+
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        for i in range(len(palindrome)//2):
+            if palindrome[i] != 'a':
+                return palindrome[:i]+'a'+palindrome[i+1:]
+        return palindrome[:-1]+'b' if palindrome[:-1] else ''
