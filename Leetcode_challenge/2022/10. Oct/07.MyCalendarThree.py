720c350002c5408faf6fc5307fd9eb39c41d4aa7
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-14 10:25:00 +0530

    Update 07.MyCalendarThree.py

--- Leetcode_challenge/2022/10. Oct/07.MyCalendarThree.py
+++ Leetcode_challenge/2022/10. Oct/07.MyCalendarThree.py
@@ -64,3 +64,26 @@ class MyCalendarThree:
 # Your MyCalendarThree object will be instantiated and called as such:
 # obj = MyCalendarThree()
 # param_1 = obj.book(start,end)
+
+
+from sortedcontainers import SortedDict
+class MyCalendarThree:
+
+    def __init__(self):
+        self.d = SortedDict()
+
+    def book(self, start: int, end: int) -> int:
+        self.d[start] = self.d.get(start, 0) + 1
+        self.d[end] = self.d.get(end, 0) - 1
+        kbook = 0
+        ans = 0
+        for e in self.d.values():
+            kbook += e
+            ans = max(ans, kbook)
+        return ans
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
+
+
