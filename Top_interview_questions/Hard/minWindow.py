7923f1e1a5acfbe7eda867a1e8bb86b60e410dd1
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-21 19:17:45 +0530

    Update minWindow.py

--- Top_interview_questions/Hard/minWindow.py
+++ Top_interview_questions/Hard/minWindow.py
@@ -68,3 +68,33 @@ class Solution:
                     start = j
                 
         return '' if not ans else s[ans[0]: ans[1]+1]
+
+
+# not comparing two dictionarys, comparing two integers
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tcount = Counter(t)
+        window_count = Counter()
+        min_len = float('inf')
+        ans = None
+        j = 0
+        formed = 0
+        required = len(tcount)
+        for i in range(len(s)):
+            c = s[i]
+            if c in tcount:
+                window_count[c] = window_count.get(c, 0) + 1
+                if window_count[c] == tcount[c]: formed +=1
+                if formed == required:
+                    while formed == required:
+                        c = s[j]
+                        if c in window_count: 
+                            window_count[c] -= 1
+                            if window_count[c] < tcount[c]:
+                                formed -= 1
+                        j += 1
+                    if i-(j-1)+1 < min_len:
+                        min_len = i-j+2
+                        ans = (j-1, i)
+                
+        return '' if not ans else s[ans[0]: ans[1]+1]
