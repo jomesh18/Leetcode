86f457bd2c2febbd508b26a5ce2b412dc6d4dc27
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-21 13:29:13 +0530

    Update 21.containsNearbyDuplicate.py

--- Leetcode_challenge/2022/10. Oct/21.containsNearbyDuplicate.py
+++ Leetcode_challenge/2022/10. Oct/21.containsNearbyDuplicate.py
@@ -48,3 +48,15 @@ class Solution:
                 return True
             d[nums[i]] = i
         return False
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if k == 0: return False
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                if abs(d[nums[i]]-i) <= k:
+                    return True
+            d[nums[i]] = i
+        return False
