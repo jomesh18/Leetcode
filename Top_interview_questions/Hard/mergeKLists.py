e8d52dbb00035e6df9d2bdfe02b4613632878c79
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-22 16:17:53 +0530

    Create mergeKLists.py

---
+++ Top_interview_questions/Hard/mergeKLists.py
@@ -0,0 +1,60 @@
+'''
+Merge k Sorted Lists
+
+Solution
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        while any(lists):
+            t = ListNode(float('inf'))
+            pos = 0
+            for i in range(len(lists)):
+                if lists[i] and lists[i].val < t.val:
+                    t, pos = lists[i], i
+            curr.next = t
+            curr = curr.next
+            lists[pos] = t.next
+        return dummy.next
