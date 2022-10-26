85cf5a9b5790a9bcb35f94d5ac14d4b9d9afc387
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-26 16:34:13 +0530

    Update 25.arrayStringsAreEqual.py

--- Leetcode_challenge/2022/10. Oct/25.arrayStringsAreEqual.py
+++ Leetcode_challenge/2022/10. Oct/25.arrayStringsAreEqual.py
@@ -43,3 +43,30 @@ word1[i] and word2[i] consist of lowercase letters.
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         return ''.join(word1) == ''.join(word2)
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        def getchars(arr):
+            for s in arr:
+                for c in s:
+                    yield c
+            yield
+        return all(c1 == c2 for c1, c2 in zip(getchars(word1), getchars(word2)))
+
+
+#O(1) space
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        word1p, word2p, s1p, s2p = 0, 0, 0, 0
+        while word1p < len(word1) and word2p < len(word2):
+            if word1[word1p][s1p] != word2[word2p][s2p]:
+                return False
+            s1p += 1
+            s2p += 1
+            if s1p == len(word1[word1p]):
+                s1p = 0
+                word1p += 1
+            if s2p == len(word2[word2p]):
+                s2p = 0
+                word2p += 1
+        return (word1p == len(word1) and word2p == len(word2))
