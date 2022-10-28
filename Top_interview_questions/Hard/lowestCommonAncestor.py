00f270a295ba39eb48e9a6a115f351b8b6b58df3
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-28 16:41:38 +0530

    Update lowestCommonAncestor.py

--- Top_interview_questions/Hard/lowestCommonAncestor.py
+++ Top_interview_questions/Hard/lowestCommonAncestor.py
@@ -64,3 +64,32 @@ class Solution:
                 return ans
             ans = u
         return ans
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(node):
+            if not node: return None
+            # If looking for me, return myself
+            if node == p or node == q: return node
+            # else look in left and right child
+            l = dfs(node.left)
+            r = dfs(node.right)
+        # if both children returned a node, means
+        # both p and q found so parent is LCA
+            if l and r:
+                return node
+
+        # either one of the chidren returned a node, meaning either p or q found on left or right branch.
+        # Example: assuming 'p' found in left child, right child returned 'None'. This means 'q' is
+        # somewhere below node where 'p' was found we dont need to search all the way, 
+        # because in such scenarios, node where 'p' found is LCA
+            return l or r
+        return dfs(root)
