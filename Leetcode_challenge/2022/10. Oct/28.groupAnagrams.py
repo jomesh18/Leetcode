37e4d71658949e2a5bf4f0373c72ec18bd37b266
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-10-28 13:40:37 +0530

    Update 28.groupAnagrams.py

--- Leetcode_challenge/2022/10. Oct/28.groupAnagrams.py
+++ Leetcode_challenge/2022/10. Oct/28.groupAnagrams.py
@@ -45,3 +45,14 @@ class Solution:
                 arr[pos] = str(int(arr[pos]) + 1)
             d[','.join(arr)].append(s)
         return list(d.values())
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in strs:
+            arr = [0]*26
+            for c in s:
+                arr[(ord(c)-ord('a'))] += 1
+            d[tuple(arr)].append(s)
+        return list(d.values())
