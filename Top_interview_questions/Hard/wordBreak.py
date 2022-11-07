b48c8dabee7e3ef78c38e0f2df5a87b9875ad2f7
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-11-07 17:14:13 +0530

    Update wordBreak.py

--- Top_interview_questions/Hard/wordBreak.py
+++ Top_interview_questions/Hard/wordBreak.py
@@ -89,3 +89,16 @@ class Solution:
             memo[start] = False
             return memo[start]
         return helper(0)
+
+# dp
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        wordSet = set(wordDict)
+        dp = [True]+[False]*n
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
