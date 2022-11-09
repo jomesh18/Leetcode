1fcc61f487f3c8133ab65d2a19eb0fa948ac746b
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-11-09 19:04:34 +0530

    Update numDecodings.py

--- Top_interview_questions/Hard/numDecodings.py
+++ Top_interview_questions/Hard/numDecodings.py
@@ -57,3 +57,16 @@ class Solution:
             memo[i] = ans
             return ans
         return count(0)
+
+# dp
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s) 
+        dp = [0]*n + [1]
+        for i in range(n-1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+                dp[i] += dp[i+2] if (i+1 < n and int(s[i:i+2]) <= 26) else 0
+        return dp[0]
