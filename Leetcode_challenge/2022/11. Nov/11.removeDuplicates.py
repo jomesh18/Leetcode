b4616bf4295d518e91c1fee80fd89a4c46ae9e12
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-11-11 15:40:35 +0530

    Update 11.removeDuplicates.py

--- Leetcode_challenge/2022/11. Nov/11.removeDuplicates.py
+++ Leetcode_challenge/2022/11. Nov/11.removeDuplicates.py
@@ -72,3 +72,12 @@ class Solution:
             nums[j] = nums[-1]
             j += 1
         return j if j else j+1
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
