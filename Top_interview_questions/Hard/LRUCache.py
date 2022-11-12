8b3bacdce2a941ead1db79f0e87645377f514585
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-11-12 18:35:32 +0530

    Update LRUCache.py

--- Top_interview_questions/Hard/LRUCache.py
+++ Top_interview_questions/Hard/LRUCache.py
@@ -48,6 +48,7 @@ Constraints:
 0 <= value <= 105
 At most 2 * 105 calls will be made to get and put.
 '''
+# using doubly linked list
 class Node:
     def __init__(self, key, value):
         self.key = key
@@ -96,6 +97,32 @@ class LRUCache:
         node.next = self.tail
         self.tail.prev = node
         
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+# using ordered dict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.d = OrderedDict()
+        self.capacity = capacity
+        
+    def get(self, key: int) -> int:
+        if key in self.d:
+            self.d.move_to_end(key)
+            return self.d[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            self.d.move_to_end(key)
+        self.d[key] = value
+        if len(self.d) > self.capacity:
+            self.d.popitem(last=False)
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
