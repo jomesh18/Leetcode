561e85ffa7acc164207abd98936adec4f53e8ef4
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-12-06 14:21:21 +0530

    Update 06.oddEvenList.py

--- Leetcode_challenge/2022/12. Dec/06.oddEvenList.py
+++ Leetcode_challenge/2022/12. Dec/06.oddEvenList.py
@@ -48,3 +48,21 @@ class Solution:
         elif odd_prev: odd_prev.next = even_head.next
         even.next = None
         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not  head: return head
+        odd, even = head, head.next
+        even_head = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
