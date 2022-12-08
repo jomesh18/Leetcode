e1ea648c146fafbc956dcf02b6e3b9761bf817a4
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-12-08 13:25:51 +0530

    Update 08.leafSimilar.py

--- Leetcode_challenge/2022/12. Dec/08.leafSimilar.py
+++ Leetcode_challenge/2022/12. Dec/08.leafSimilar.py
@@ -56,3 +56,36 @@ class Solution:
                 ret.extend(dfs(node.right))
             return ret
         return dfs(root1) == dfs(root2)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+        return list(dfs(root1)) == list(dfs(root2))
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+        return all(a == b for a, b in zip_longest(dfs(root1), dfs(root2)))
