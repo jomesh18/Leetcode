4c1d76b700a7f686c5b327ce5c22b302f5d29cd0
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2022-12-29 17:11:37 +0530

    Create BIT.py

---
+++ Miscellaneous/BIT.py
@@ -0,0 +1,35 @@
+# Binary indexed tree/ Fenwick tree
+# for prefix sum with update
+
+class BIT:
+    def __init__(self, n):
+        self.bit = [0]*n
+
+    def sum(self, i):
+        s = 0
+        while i > 0:
+            s += self.bit[i]
+            i = self.get_parent(i)
+        return s
+
+    def update(self, i, delta):
+        while i < len(self.bit):
+            self.bit[i] += delta
+            i = self.get_next(i)
+
+    def get_next(self, i):
+        return i + (i & -i)
+
+    def get_parent(self, i):
+        return i - (i & -i)
+
+arr = [1, 2, 3, 4]
+bit = BIT(len(arr))
+for i in range(len(arr)):
+    bit.update(i, arr[i])
+print(bit.sum(2))
+#update index 2 value to 0
+old_val = arr[2]
+new_val = 0
+bit.update(2, -old_val+new_val)
+print(bit.sum(2))
