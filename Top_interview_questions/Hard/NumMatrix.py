250d95f8d8763ef3ad1bb93be971662d7a2b083e
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-01-03 15:35:06 +0530

    Update NumMatrix.py

--- Top_interview_questions/Hard/NumMatrix.py
+++ Top_interview_questions/Hard/NumMatrix.py
@@ -51,29 +51,9 @@ Output:
   1
   -1
 '''
-class BIT:
-    def __init__(self, n):
-        self.bit = [0]*(n+1)
 
-    def sum(self, i):
-        i += 1
-        s = 0
-        while i > 0:
-            s += self.bit[i]
-            i = self.get_parent(i)
-        return s
-
-    def update(self, i, delta):
-        i += 1
-        while i < len(self.bit):
-            self.bit[i] += delta
-            i = self.get_next(i)
-
-    def get_next(self, i):
-        return i + (i & -i)
-
-    def get_parent(self, i):
-        return i - (i & -i)
+'''
+# better naming,
 
 class NumMatrix(object):
 
@@ -81,16 +61,119 @@ class NumMatrix(object):
         """
         :type matrix: List[List[int]]
         """
-        self.matrix = matrix
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
+        self.bit = [[0 for _ in range(self.cols + 1)] for _ in range(self.rows + 1)]
+        self.matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.update(r, c, matrix[r][c])
+        
+    def lowbit(self, x):
+        return x & (-x)
+    def update(self, row, col, val):
+        """
+        :type row: int
+        :type col: int
+        :type val: int
+        :rtype: void
+        """
+        delta = val - self.matrix[row][col]
+        self.matrix[row][col] = val
+        r = row + 1
+        while r <= self.rows:
+            c = col + 1
+            while c <= self.cols:
+                self.bit[r][c] += delta
+                c += self.lowbit(c)
+            r += self.lowbit(r)
+    def getPrefixSum(self, row, col):
+        r = row + 1
+        sum = 0
+        while r > 0:
+            c = col + 1
+            while c > 0:
+                sum += self.bit[r][c]
+                c -= self.lowbit(c)
+            r -= self.lowbit(r)
+        return sum
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        sum = self.getPrefixSum(row2, col2)
+        sum_upside = self.getPrefixSum(row1 - 1, col2)
+        sum_leftside = self.getPrefixSum(row2, col1 - 1)
+        sum_small = self.getPrefixSum(row1 - 1, col1 - 1)
+        return sum - sum_upside - sum_leftside + sum_small
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# obj.update(row,col,val)
+# param_2 = obj.sumRegion(row1,col1,row2,col2)
+
+# Given matrix = [
+#   [3, 0, 1, 4, 2],
+#   [5, 6, 3, 2, 1],
+#   [1, 2, 0, 1, 5],
+#   [4, 1, 0, 1, 7],
+#   [1, 0, 3, 0, 5]
+# ]
+
+# sumRegion(2, 1, 4, 3) -> 8
+# update(3, 2, 2)
+# sumRegion(2, 1, 4, 3) -> 10
+
+test = NumMatrix([[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]])
+r = test.getPrefixSum(1, 1)
+print(r)
+r = test.getPrefixSum(2, 2)
+print(r)
+r = test.sumRegion(2, 1, 4, 3)
+print(r)
+print(test.matrix)
+print(test.bit)
+test.update(3, 2, 2)
+print(test.matrix)
+print(test.bit)
+r = test.sumRegion(2, 1, 4, 3)
+print(r)
+'''
+
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
         self.m, self.n = len(matrix), len(matrix[0])
-        self.bit = BIT((self.m+1)*(self.n+1))
-        for i in range(self.m):
-            for j in range(self.n):
-                pos = (i+1)*(self.n+1)+j+1
-                val = self.bit.sum((i+1)*(self.n+1)+j) + self.bit.sum(i*(self.n+1)+j+1) - self.bit.sum(i*(self.n+1)+j) + self.matrix[i][j]
-                self.bit.update(pos, val)
-                print('inside init', i, j, self.bit.bit)
-        print('final', self.bit.bit)
+        self.matrix = [[0]*self.n for _ in range(self.m)]
+        self.bit = [[0]*(self.n+1) for _ in range(self.m+1)]
+        for r in range(self.m):
+            for c in range(self.n):
+                self.update(r, c, matrix[r][c])
+        print(self.bit)
+        print(self.matrix)
+
+    def lowbit(self, a):
+        return a & (-a)
+
+    def get_sum(self, row, col):
+        r = row + 1
+        s = 0
+        while r > 0:
+            c = col + 1
+            while c > 0:
+                s += self.bit[r][c]
+                c -= self.lowbit(c)
+            r -= self.lowbit(r)
+        return s
 
     def update(self, row, col, val):
         """
@@ -99,11 +182,15 @@ class NumMatrix(object):
         :type val: int
         :rtype: void
         """
-        i = (row+1)*(self.n+1) + col + 1
-        delta = -self.matrix[row][col] + val
+        delta = val - self.matrix[row][col]
         self.matrix[row][col] = val
-        self.bit.update(i, delta)
-        print('updated', row, col, val, self.bit.bit)
+        r = row + 1
+        while r <= self.m:
+            c = col + 1
+            while c <= self.n:
+                self.bit[r][c] += delta
+                c += self.lowbit(c)
+            r += self.lowbit(r)
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -113,13 +200,11 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        a = self.bit.sum((row2+1)*(self.n+1)+col2+1)
-        b = self.bit.sum((row2+1)*(self.n+1)+col1)
-        c = self.bit.sum(row1*(self.n+1)+col2+1)
-        d = self.bit.sum(row1*(self.n+1)+col1)
-        ans = a-b-c+d
-        print('inside sum', a, b, c, d, ans)
-        return ans
+        tot = self.get_sum(row2, col2)
+        top_left = self.get_sum(row1-1, col1-1)
+        left = self.get_sum(row2, col1-1)
+        top = self.get_sum(row1-1, col2)
+        return tot - left - top + top_left
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
