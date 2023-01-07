cc046ff779dd76093c9c46e9a365c9b79e3d4c1f
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-01-07 19:24:26 +0530

    updates

--- Top_interview_questions/Hard/alien_order.py
+++ Top_interview_questions/Hard/alien_order.py
@@ -41,6 +41,7 @@ Explanation：
 from "z" and "x"，we can get 'z' < 'x'
 So return "zx"
 '''
+# post order dfs
 from typing import (
     List,
 )
@@ -52,3 +53,72 @@ class Solution:
     """
     def alien_order(self, words: List[str]) -> str:
         # Write your code here
+        d = {c: set() for word in words for c in word}
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]: return ""
+            for j in range(min_len):
+                if w1[j] != w2[j]:
+                    d[w1[j]].add(w2[j])
+                    break
+        visited = {}
+        ans = []
+        def dfs(i):
+            if i in visited:
+                return visited[i]
+            visited[i] = True
+            for nei in d[i]:
+                if dfs(nei): return True
+            visited[i] = False
+            ans.append(i)
+
+        for i in range(25, -1, -1):
+            c = chr(ord('a') + i)
+            if c in d and c not in visited:
+                if dfs(c):
+                    return ''
+        ans.reverse()
+        return ''.join(ans)
+
+
+# topological sort using heap instead of q (for lintcode extra condition)
+from typing import (
+    List,
+)
+from heapq import *
+class Solution:
+    """
+    @param words: a list of words
+    @return: a string which is correct order
+    """
+    def alien_order(self, words: List[str]) -> str:
+        # Write your code here
+        adj = {c: set() for word in words for c in word}
+        indegree = {c: 0 for word in words for c in word}
+
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]: return ''
+            for j in range(min_len):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]: indegree[w2[j]] += 1
+                    adj[w1[j]].add(w2[j])
+                    break
+
+        heap = [c for c in indegree if indegree[c] == 0]
+        heapify(heap)
+        order = []
+
+        while heap:
+            # print(indegree, heap)
+            for _ in range(len(heap)):
+                c = heappop(heap)
+                order.append(c)
+                for nei in adj[c]:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        heappush(heap, nei)
+        
+        return ''.join(order) if len(order) == len(adj) else ''
