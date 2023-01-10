096e74b4f10c6c651765004f3f06c25fb352b194
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-01-10 18:03:07 +0530

    Update inorderSuccessor.py

--- Top_interview_questions/Medium/inorderSuccessor.py
+++ Top_interview_questions/Medium/inorderSuccessor.py
@@ -68,3 +68,31 @@ class Solution:
             #     return  helper(node.right, p, parent)
 
         return helper(root, p, parent=None)
+
+
+"""
+Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the BST.
+    @param: p: You need find the successor node of p.
+    @return: Successor of p.
+    """
+    def inorderSuccessor(self, root, p):
+        # write your code here
+        successor = None
+        while root:
+            if root.val > p.val:
+                successor = root
+                root = root.left
+            else:
+                root = root.right
+        return successor
