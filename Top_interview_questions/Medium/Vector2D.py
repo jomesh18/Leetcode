fc402e47bd0578b3b2d269f34e7e746d57c1b127
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-01-11 18:48:20 +0530

    Update Vector2D.py

--- Top_interview_questions/Medium/Vector2D.py
+++ Top_interview_questions/Medium/Vector2D.py
@@ -56,3 +56,32 @@ class Vector2D(object):
 # Your Vector2D object will be instantiated and called as such:
 # i, v = Vector2D(vec2d), []
 # while i.hasNext(): v.append(i.next())
+
+
+# using pop()
+class Vector2D(object):
+
+    # @param vec2d {List[List[int]]}
+    def __init__(self, vec2d):
+        # Initialize your data structure here
+        self.arr = []
+        while vec2d:
+            vec = vec2d.pop()
+            while vec:
+                self.arr.append(vec.pop())
+
+    # @return {int} a next element
+    def next(self):
+        # Write your code here
+        if self.hasNext():
+            return self.arr.pop()
+
+    # @return {boolean} true if it has next element
+    # or false
+    def hasNext(self):
+        # Write your code here
+        return bool(self.arr)
+
+# Your Vector2D object will be instantiated and called as such:
+# i, v = Vector2D(vec2d), []
+# while i.hasNext(): v.append(i.next())
