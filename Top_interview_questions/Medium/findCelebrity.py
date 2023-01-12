4a014fd135771a916cfe01e7a5cb7943cf33e1a0
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-01-12 15:47:55 +0530

    Update findCelebrity.py

--- Top_interview_questions/Medium/findCelebrity.py
+++ Top_interview_questions/Medium/findCelebrity.py
@@ -92,3 +92,41 @@ class Solution:
                     break
             if is_celebrity: return k
         return -1
+
+
+
+class Solution:
+    # @param {int} n a party with n people
+    # @return {int} the celebrity's label or -1
+    def findCelebrity(self, n):
+        celeb = 0
+        
+        for i in range(1, n):
+            if Celebrity.knows(celeb, i):
+                celeb = i
+        
+        # Check if the final candicate is the celebrity
+        for i in range(n):
+            if celeb != i and Celebrity.knows(celeb, i):
+                return -1
+            if celeb != i and not Celebrity.knows(i, celeb):
+                return -1
+        
+        return celeb
+
+
+class Solution:
+    # @param {int} n a party with n people
+    # @return {int} the celebrity's label or -1
+    def findCelebrity(self, n):
+        # Write your code here
+        celeb = 0
+        for i in range(1, n):
+            if Celebrity.knows(celeb, i):
+                celeb = i
+
+        for i in range(n):
+            if i != celeb:
+                if not Celebrity.knows(i, celeb) or Celebrity.knows(celeb, i):
+                    return -1
+        return celeb
