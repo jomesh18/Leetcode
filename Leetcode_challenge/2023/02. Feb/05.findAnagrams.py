387cb15e8971ad1df8e56b1336cf8da8f2435b30
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-02-05 09:19:00 +0530

    Create 05.findAnagrams.py

---
+++ Leetcode_challenge/2023/02. Feb/05.findAnagrams.py
@@ -0,0 +1,54 @@
+'''
+438. Find All Anagrams in a String
+Medium
+
+9755
+
+295
+
+Add to List
+
+Share
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+ 
+
+Constraints:
+
+1 <= s.length, p.length <= 3 * 104
+s and p consist of lowercase English letters.
+'''
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        pc = Counter(p)
+        ans = []
+        sc = Counter(s[:len(p)])
+        if sc == pc: ans.append(0)
+        l = 0
+        for r in range(len(p), len(s)):
+            sc[s[l]] -= 1
+            sc[s[r]] += 1
+            l += 1
+            if sc == pc:
+                ans.append(l)
+        return ans
+            
