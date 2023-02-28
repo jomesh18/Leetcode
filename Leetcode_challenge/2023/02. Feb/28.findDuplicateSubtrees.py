31ea5f184b939479f171da84d8a9910852c40bc3
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-02-28 15:56:17 +0530

    Create 28.findDuplicateSubtrees.py

---
+++ Leetcode_challenge/2023/02. Feb/28.findDuplicateSubtrees.py
@@ -0,0 +1,64 @@
+'''
+652. Find Duplicate Subtrees
+Medium
+
+4569
+
+376
+
+Add to List
+
+Share
+Given the root of a binary tree, return all duplicate subtrees.
+
+For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+
+Two trees are duplicate if they have the same structure with the same node values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,null,2,4,null,null,4]
+Output: [[2,4],[4]]
+Example 2:
+
+
+Input: root = [2,1,1]
+Output: [[1]]
+Example 3:
+
+
+Input: root = [2,2,2,3,null,3,null]
+Output: [[2,3],[3]]
+ 
+
+Constraints:
+
+The number of the nodes in the tree will be in the range [1, 5000]
+-200 <= Node.val <= 200
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        d = {}
+        res = set()
+        def dfs(node):
+            if not node: return
+            left = dfs(node.left)
+            right = dfs(node.right)
+            path = left, node.val, right
+            # print(node.val, path)
+            if path in d:
+                res.add(d[path])
+            else:
+                d[path] = node
+            return path
+        dfs(root)
+        return res
