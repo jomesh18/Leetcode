3ea0029e1d1a0efc7fd97f4feb8e2df387a2912b
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-03-05 18:17:41 +0530

    Update 03.strStr.py

--- Leetcode_challenge/2023/03. March/03.strStr.py
+++ Leetcode_challenge/2023/03. March/03.strStr.py
@@ -35,3 +35,61 @@ haystack and needle consist of only lowercase English characters.
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
+
+# rabin karp single hash
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n, m = len(haystack), len(needle)
+        if n < m: return -1
+        radix, mod = 26, 10**9+33
+        max_weight = 1
+        for _ in range(m):
+            max_weight = (max_weight*radix)%mod
+            
+        curr_hash, needle_hash = 0, 0
+        for i in range(m):
+            curr_hash = (curr_hash+((ord(haystack[i])-ord('a'))*(radix**(m-1-i)))%mod)%mod
+            needle_hash = (needle_hash+((ord(needle[i])-ord('a'))*(radix**(m-1-i)))%mod)%mod
+        if curr_hash == needle_hash and haystack[:m] == needle: return 0
+        # print(curr_hash, needle_hash)
+        for i in range(1, n-m+1):
+            curr_hash = (curr_hash*radix)%mod
+            curr_hash = ((curr_hash-((ord(haystack[i-1])-ord('a'))*max_weight))+mod)%mod
+            curr_hash = (curr_hash+(ord(haystack[i+m-1])-ord('a')))%mod
+            # print(i, curr_hash)
+            if curr_hash == needle_hash and haystack[i:i+m] == needle: return i
+        return -1
+
+# rabin karp, multiple hash O(n+m)
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n, m = len(haystack), len(needle)
+        if n < m: return -1
+        radix1, radix2 = 26, 31
+        mod1, mod2 = 10**9+33, 10**9+7
+        max_w1, max_w2 = 1, 1
+        for _ in range(m):
+            max_w1 = (max_w1*radix1)%mod1
+            max_w2 = (max_w2*radix2)%mod2
+        def find_hash(word, start, end, radix, mod):
+            curr_hash = 0
+            factor = 1
+            for i in range(end, start-1, -1):
+                curr_hash = (curr_hash + ((ord(word[i])-ord('a'))*factor)%mod)%mod
+                factor = (factor*radix)%mod
+            return curr_hash
+        nhash1, nhash2 = find_hash(needle, 0, m-1, radix1, mod1), find_hash(needle, 0, m-1, radix2, mod2)
+        hhash1, hhash2 = find_hash(haystack, 0, m-1, radix1, mod1), find_hash(haystack, 0, m-1, radix2, mod2)
+        if hhash1 == nhash1 and hhash2 == nhash2: return 0
+        def find_rolling(curr_hash, word, length, start, radix, mod, max_w):
+            curr_hash = (curr_hash*radix)%mod
+            curr_hash = (curr_hash-((ord(word[start-1])-ord('a'))*max_w)+mod)%mod
+            curr_hash = (curr_hash+(ord(word[start+length-1])-ord('a')))%mod
+            return curr_hash
+        # print(hhash1, hhash2, nhash1, nhash2)
+        for i in range(1, n-m+1):
+            hhash1 = find_rolling(hhash1, haystack, m, i, radix1, mod1, max_w1)
+            hhash2 = find_rolling(hhash2, haystack, m, i, radix2, mod2, max_w2)
+            # print(hhash1, hhash2, i)
+            if (hhash1 == nhash1) and (hhash2 == nhash2): return i
+        return -1
