2a2bd7157f0c02035b9242d678ce2e1b11af3aeb
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-03-05 16:26:42 +0530

    Create 05.minJumps.py

---
+++ Leetcode_challenge/2023/03. March/05.minJumps.py
@@ -0,0 +1,77 @@
+'''
+1345. Jump Game IV
+Hard
+
+2597
+
+99
+
+Add to List
+
+Share
+Given an array of integers arr, you are initially positioned at the first index of the array.
+
+In one step you can jump from index i to index:
+
+i + 1 where: i + 1 < arr.length.
+i - 1 where: i - 1 >= 0.
+j where: arr[i] == arr[j] and i != j.
+Return the minimum number of steps to reach the last index of the array.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+Output: 3
+Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+Example 2:
+
+Input: arr = [7]
+Output: 0
+Explanation: Start index is the last index. You do not need to jump.
+Example 3:
+
+Input: arr = [7,6,9,6,9,6,9,7]
+Output: 1
+Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5 * 104
+-108 <= arr[i] <= 108
+'''
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        d = {}
+        for i, num in enumerate(arr):
+            if num not in d:
+                d[num] = [i]
+            else:
+                d[num].append(i)
+        q = [0]
+        visited = [False]*n
+        steps = 0
+        while q:
+            tq = []
+            for curr in q:
+                if curr == n-1: return steps
+                if arr[curr] in d:
+                    for i in d[arr[curr]]:
+                        if i != curr and not visited[i]:
+                            visited[i] = True
+                            tq.append(i)
+                    del d[arr[curr]]
+                if not visited[curr+1]:
+                    visited[curr+1] = True
+                    tq.append(curr + 1)
+                if curr != 0 and not visited[curr-1]:
+                    visited[curr-1] = True
+                    tq.append(curr - 1)
+            steps += 1
+            q = tq
+        
