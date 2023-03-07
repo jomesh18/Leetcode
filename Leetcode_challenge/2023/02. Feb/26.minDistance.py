0d9404e9ed59d67f67b3db63cfd3db2757a1aea7
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-03-07 20:00:30 +0530

    Update 26.minDistance.py

--- Leetcode_challenge/2023/02. Feb/26.minDistance.py
+++ Leetcode_challenge/2023/02. Feb/26.minDistance.py
@@ -43,6 +43,7 @@ Constraints:
 0 <= word1.length, word2.length <= 500
 word1 and word2 consist of lowercase English letters.
 '''
+# O(n1*n2), memo
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n1, n2 = len(word1), len(word2)
@@ -62,3 +63,20 @@ class Solution:
                 memo[(i, j)] = ans
             return memo[(i, j)]
         return helper(0, 0)
+
+# O(n1*n2), tabulation
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:        
+        n1, n2 = len(word1), len(word2)
+        dp = [[0]*(n2+1) for _ in range(n1+1)]
+        for i in range(n1):
+            dp[i][n2] = n1-i
+        for i in range(n2):
+            dp[n1][i] = n2-i
+        for i in range(n1-1, -1, -1):
+            for j in range(n2-1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
+        return dp[0][0]
