516114b01cd44b8fb5f43f7c9405d6ea28a6c222
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-05-10 12:21:51 +0530

    Create 10.generateMatrix.py

---
+++ Leetcode_challenge/2023/05. May/10.generateMatrix.py
@@ -0,0 +1,57 @@
+'''
+59. Spiral Matrix II
+Medium
+
+5171
+
+219
+
+Add to List
+
+Share
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+ 
+
+Example 1:
+
+
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+Example 2:
+
+Input: n = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= n <= 20
+'''
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        ans = [[0]*n for _ in range(n)]
+        
+        lr, hr, lc, hc = 0, n-1, 0, n-1
+        i = 1
+        while lr <= hr and lc <= hc:
+            for c in range(lc, hc+1):
+                ans[lr][c] = i
+                i += 1
+            for r in range(lr+1, hr+1):
+                ans[r][hc] = i
+                i += 1
+            if lr != hr:
+                for c in range(hc-1, lc-1, -1):
+                    ans[hr][c] = i
+                    i += 1
+            if lc != hc:
+                for r in range(hr-1, lr, -1):
+                    ans[r][lc] = i
+                    i += 1
+            lr += 1
+            lc += 1
+            hc -= 1
+            hr -= 1
+            
+        return ans
