a68bf185baaf459418c85a29d50b5242b862c50f
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-06-01 16:08:15 +0530

    Create 01.shortestPathBinaryMatrix.py

---
+++ Leetcode_challenge/2023/06. June/01.shortestPathBinaryMatrix.py
@@ -0,0 +1,62 @@
+'''
+1091. Shortest Path in Binary Matrix
+Medium
+
+5111
+
+194
+
+Add to List
+
+Share
+Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+The length of a clear path is the number of visited cells of this path.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1],[1,0]]
+Output: 2
+Example 2:
+
+
+Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+Output: -1
+ 
+
+Constraints:
+
+n == grid.length
+n == grid[i].length
+1 <= n <= 100
+grid[i][j] is 0 or 1
+'''
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        if grid[0][0] != 0: return -1
+        q = [(0, 0)]
+        level = 1
+        while q:
+            nq = []
+            for i, j in q:
+                if i == m-1 and j == n-1: return level
+                for di, dj in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                    ni, nj = i+di, j+dj
+                    if 0<=ni<m and 0<=nj<n and grid[ni][nj] == 0:
+                        nq.append((ni, nj))
+                        grid[ni][nj] = 1
+            level += 1
+            q = nq
+        return -1
