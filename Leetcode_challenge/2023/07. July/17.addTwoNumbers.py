f65f4f89f23e0e3fafbeb75facf37677f8f5ce46
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-07-17 14:26:48 +0530

    Update 17.addTwoNumbers.py

--- Leetcode_challenge/2023/07. July/17.addTwoNumbers.py
+++ Leetcode_challenge/2023/07. July/17.addTwoNumbers.py
@@ -70,3 +70,26 @@ class Solution:
             curr.next = ListNode(v)
             curr = curr.next
         return reverse(dummy.next)
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1, num2 = 0, 0
+        while l1:
+            num1 = num1*10+l1.val
+            l1 = l1.next
+        while l2:
+            num2 = num2*10+l2.val
+            l2 = l2.next
+        num = num1 + num2
+        dummy = curr = ListNode(0)
+        num = str(num)
+        for i in range(len(num)):
+            curr.next = ListNode(int(num[i]))
+            curr = curr.next
+        return dummy.next
