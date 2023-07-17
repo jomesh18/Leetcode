50aacc94f3368a1fc6bb502ae4ea58c557293bd3
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-07-17 14:18:15 +0530

    Create 17.addTwoNumbers.py

---
+++ Leetcode_challenge/2023/07. July/17.addTwoNumbers.py
@@ -0,0 +1,72 @@
+'''
+445. Add Two Numbers II
+Medium
+
+4982
+
+255
+
+Add to List
+
+Share
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
+Example 2:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+Example 3:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ 
+
+Follow up: Could you solve it without reversing the input lists?
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def reverse(node):
+            prev = None
+            curr = node
+            while curr:
+                t = curr.next
+                curr.next = prev
+                prev = curr
+                curr = t
+            return prev if prev else curr
+        
+        l1, l2 = reverse(l1), reverse(l2)
+        c = 0
+        curr = dummy = ListNode(0)
+        while l1 or l2 or c:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            t = v1+v2+c
+            c, v = t//10, t%10
+            curr.next = ListNode(v)
+            curr = curr.next
+        return reverse(dummy.next)
