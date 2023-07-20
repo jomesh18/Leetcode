e4d070a2242bce0c75fc5e6bd4e78878960d44b2
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-07-20 12:21:07 +0530

    Create 20.asteroidCollision.py

---
+++ Leetcode_challenge/2023/07. July/20.asteroidCollision.py
@@ -0,0 +1,66 @@
+'''
+735. Asteroid Collision
+Medium
+
+5767
+
+578
+
+Add to List
+
+Share
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+ 
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ 
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
+'''
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            if not stack or a > 0 or stack[-1] < 0:
+                stack.append(a)
+            else:
+                add = False
+                while stack:
+                    if stack[-1] < 0:
+                        add = True
+                        break
+                    elif stack[-1] >= -a:
+                        if stack[-1] == -a:
+                            stack.pop()
+                        add = False
+                        break
+                    else:
+                        stack.pop()
+                        add = True
+                if add:
+                    stack.append(a)
+                
+        return stack
