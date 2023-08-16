d5b6fef4d0f5aa77ccfd181fe967d6a821d3a236
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-08-16 16:15:11 +0530

    Create 16.maxSlidingWindow.py

---
+++ Leetcode_challenge/2023/08. August/16.maxSlidingWindow.py
@@ -0,0 +1,54 @@
+'''
+239. Sliding Window Maximum
+Hard
+
+16084
+
+537
+
+Add to List
+
+Share
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+'''
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        heap = [(-nums[i], i) for i in range(k)]
+        heapify(heap)
+        ans = [-heap[0][0]]
+        for i in range(k, len(nums)):
+            heappush(heap, (-nums[i], i))
+            while len(heap) > k and heap[0][1] <= i-k:
+                heappop(heap)
+            ans.append(-heap[0][0])
+        return ans
+            
