065253cfd57ffe220e5d5f7a3e7f08bd2f2b8e0c
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-09-21 17:34:39 +0530

    Create 17.shortestPathLength.py

---
+++ Leetcode_challenge/2023/09. September/17.shortestPathLength.py
@@ -0,0 +1,63 @@
+'''
+847. Shortest Path Visiting All Nodes
+Hard
+
+4070
+
+160
+
+Add to List
+
+Share
+You have an undirected, connected graph of n nodes labeled from 0 to n - 1. You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
+
+Return the length of the shortest path that visits every node. You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+
+ 
+
+Example 1:
+
+
+Input: graph = [[1,2,3],[0],[0],[0]]
+Output: 4
+Explanation: One possible path is [1,0,2,0,3]
+Example 2:
+
+
+Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+Output: 4
+Explanation: One possible path is [0,1,4,2,3]
+ 
+
+Constraints:
+
+n == graph.length
+1 <= n <= 12
+0 <= graph[i].length < n
+graph[i] does not contain i.
+If graph[a] contains b, then graph[b] contains a.
+The input graph is always connected.
+'''
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        if len(graph) == 1: return 0
+        n = len(graph)
+        q = []
+        all_mask = (1<<n)-1
+        q = [(i, 1<<i) for i in range(n)]
+        seen = set(q)
+        steps = 0
+        while q:
+            next_q = []
+            for i, mask in q:
+                for nei in graph[i]:
+                    next_mask = mask | (1<<nei)
+                    if next_mask == all_mask:
+                        return steps + 1
+                    if (nei, next_mask) not in seen:
+                        seen.add((nei, next_mask))
+                        next_q.append((nei, next_mask))
+            q = next_q
+            steps += 1
+            
+        return -1
