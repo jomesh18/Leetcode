2f28a9ba54890f734947d334b80b9e8b1c8972e7
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-09-28 12:10:40 +0530

    Create 28.sortArrayByParity.py

---
+++ Leetcode_challenge/2023/09. September/28.sortArrayByParity.py
@@ -0,0 +1,44 @@
+'''
+905. Sort Array By Parity
+Easy
+
+4781
+
+136
+
+Add to List
+
+Share
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
+'''
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        pos = len(nums)-1
+        i = 0
+        while i < pos:
+            if nums[i] & 1:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos -= 1
+            else:
+                i += 1
+        return nums
