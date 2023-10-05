422199e3c37ef6c762249a6446ceddacb9502490
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-10-05 13:18:56 +0530

    Create 05.majorityElement.py

---
+++ Leetcode_challenge/2023/10. Oct/05.majorityElement.py
@@ -0,0 +1,57 @@
+'''
+229. Majority Element II
+Medium
+
+8495
+
+377
+
+Add to List
+
+Share
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow up: Could you solve the problem in linear time and in O(1) space?
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        return [key for key, val in Counter(nums).items() if val > len(nums)//3]
+
+# Bayes Moore Voting Algorithm
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count1, count2, candidate1, candidate2 = 0, 0, None, None
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1, count2 = count1-1, count2-1
+        return [num for num in (candidate1, candidate2) if nums.count(num) > len(nums)//3]
