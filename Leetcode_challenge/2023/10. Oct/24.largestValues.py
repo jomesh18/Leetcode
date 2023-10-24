fd06b2509ed6aa71e7e804de345abe055b0e7fe6
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-10-24 12:18:26 +0530

    Create 24.largestValues.py

---
+++ Leetcode_challenge/2023/10. Oct/24.largestValues.py
@@ -0,0 +1,46 @@
+'''
+515. Find Largest Value in Each Tree Row
+Medium
+
+3046
+
+103
+
+Add to List
+
+Share
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+ 
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+ 
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-231 <= Node.val <= 231 - 1
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        ans = []
+        curr_row = [root]
+        while curr_row:
+            ans.append(max(n.val for n in curr_row))
+            curr_row = [n for node in curr_row for n in [node.left, node.right] if n]
+        return ans
