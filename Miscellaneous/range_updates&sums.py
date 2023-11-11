bd4cedde46cb505afe64aacb344527e10ae1bb61
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-11-11 19:05:02 +0530

    Update range_updates&sums.py

--- Miscellaneous/range_updates&sums.py
+++ Miscellaneous/range_updates&sums.py
@@ -1,46 +1,10 @@
 '''
 https://cses.fi/problemset/task/1735/
 
-CSES Problem Set
-      Range Updates and Sums
-
-Task
-Submit
-Results
-Statistics
-
-    
-    
-  
-
-  
-
-    
-
-
 CSES - Range Updates and Sums
-
-
-
-
-addEventListener("DOMContentLoaded", function (e) {
-    const mathElements = document.getElementsByClassName("math");
-    const macros = {};
-    for (let element of mathElements) {
-        katex.render(element.textContent, element, {
-            displayMode: element.classList.contains("display"),
-            throwOnError: false,
-            globalGroup: true,
-            macros,
-        });
-    }
-});
-
 
 Time limit: 1.00 s
 Memory limit: 512 MB
-
-
 
 Your task is to maintain an array of n values and efficiently process the following types of queries:
 
@@ -80,6 +44,7 @@ class SegmentTree:
 	def __init__(self, a):
 		self.n = len(a)
 		self.t = [0]*(4*self.n)
+		self.lazy = [0]*(4*self.n)
 		self.build(a, 1, 0, self.n-1)
 
 	def build(self, a, v, tl, tr):
@@ -91,6 +56,13 @@ class SegmentTree:
 			self.build(a, 2*v+1, tm+1, tr)
 			self.t[v] = self.t[2*v] + self.t[2*v+1]
 
+	def push(v):
+		self.t[2*v] += lazy[v]
+		self.t[2*v+1] += lazy[v]
+		self.lazy[2*v] += lazy[v]
+		self.lazy[2*v+1] += lazy[v]
+		self.lazy[v] = 0
+
 	def update_range(self, l, r, add):
 		self.update_range_util(0, 0, self.n-1, l, r, add)
 
@@ -99,33 +71,40 @@ class SegmentTree:
 			return 
 		if tl == l and tr == r:
 			self.t[v] += add
+			self.lazy[v] += add
 		else:
+			self.push(v)
 			tm = (tl + tr) // 2
 			self.update_range_util(2*v, tl, tm, l, min(tm, r), add)
 			self.update_range_util(2*v+1, tm+1, tl, max(l, tm+1), r, add)
+			self.t[v] = self.t[2*v]+self.t[2*v+1]
 
-	def query(self, pos):
-		return self.query_util(pos, 0, self.n-1, 0)
+	def query_range(self, l, r):
+		return self.query_util(0, 0, self.n-1, l, r)
 
-	def query_util(self, v, tl, tr, pos):
-		if tl == tr:
+	def query_util(self, v, tl, tr, l, r):
+		if l > r:
+			return 0
+		if l == tl and r == tr:
 			return self.t[v]
+		push(v)
 		tm = (tl + tr) // 2
-		if pos <= tm:
-			return t[v] + self.query_util(2*v, tl, tm, pos)
-		return t[v] + self.query_util(2*v+1, tm+1, tr, pos)
+		return self.query_util(2*v, tl, tm, l, min(tm, r)) + self.query_util(2*v+1, tm+1, tr, max(l, tm+1), r)
 
-n, q = [int(i) for i in input().split()]
-a = [int(i) for i in input().split()]
+# n, q = [int(i) for i in input().split()]
+# a = [int(i) for i in input().split()]
 
-obj = SegmentTree(a)
+# obj = SegmentTree(a)
 
-for _ in range(q):
-	inp = [int(i) for i in input().split()]
-	a, b = inp[1], inp[2]
-	if inp[0] == 1:
-		obj.update_range(a, b, inp[3])
-	elif inp[0] == 2:
-		obj.set_range(a, b, inp[3])
-	else:
-		print(obj.query(a, b))
+# for _ in range(q):
+# 	inp = [int(i) for i in input().split()]
+# 	a, b = inp[1], inp[2]
+# 	if inp[0] == 1:
+# 		obj.update_range(a, b, inp[3])
+# 	elif inp[0] == 2:
+# 		obj.set_range(a, b, inp[3])
+# 	else:
+# 		print(obj.query(a, b))
+
+
+a = [2, 3, 1, 1, 5, 3]
