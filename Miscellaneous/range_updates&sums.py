1330eebb69f672cf8c7e9f8082bb108ecbe09587
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-11-21 09:42:04 +0530

    added 21.countNicePairs.py

--- Miscellaneous/range_updates&sums.py
+++ Miscellaneous/range_updates&sums.py
@@ -41,56 +41,30 @@ Output:
 15
 
 '''
-class SegmentTree:
-	def __init__(self, a):
-		self.n = len(a)
-		self.t = [0]*(4*self.n)
-		self.lazy = [0]*(4*self.n)
-		self.build(a, 1, 0, self.n-1)
+a = [2, 3, 1, 1, 5, 3]
+n = len(a)
+st = [0]*(4*n)
+lazy = [0]*(4*n)
+marked = [False]*(4*n)
 
-	def build(self, a, v, tl, tr):
-		if tl == tr:
-			self.t[v] = a[tl]
-		else:
-			tm = (tl + tr) // 2
-			self.build(a, 2*v, tl, tm)
-			self.build(a, 2*v+1, tm+1, tr)
-			self.t[v] = self.t[2*v] + self.t[2*v+1]
+def build(a, tl=0, tr=n-1, pos=1):
+	if tl == tr:
+		st[pos] = a[tl]
+		return
+	tm = (tl+tr)//2
+	build(a, tl, tm, 2*pos)
+	build(a, tm+1, tr, 2*pos+1)
+	st[pos] = st[2*pos] + st[2*pos+1]
 
-	def push(v):
-		self.t[2*v] += lazy[v]
-		self.t[2*v+1] += lazy[v]
-		self.lazy[2*v] += lazy[v]
-		self.lazy[2*v+1] += lazy[v]
-		self.lazy[v] = 0
+def add(l, r, value, tl=0, tr=n-1, pos=1):
+	if 
 
-	def update_range(self, l, r, add):
-		self.update_range_util(0, 0, self.n-1, l, r, add)
+def replace(l, r, value, tl=0, tr=n-1, pos=1):
 
-	def update_range_util(self, v, tl, tr, l, r, add):
-		if l > r:
-			return 
-		if tl == l and tr == r:
-			self.t[v] += add
-			self.lazy[v] += add
-		else:
-			self.push(v)
-			tm = (tl + tr) // 2
-			self.update_range_util(2*v, tl, tm, l, min(tm, r), add)
-			self.update_range_util(2*v+1, tm+1, tl, max(l, tm+1), r, add)
-			self.t[v] = self.t[2*v]+self.t[2*v+1]
 
-	def query_range(self, l, r):
-		return self.query_util(0, 0, self.n-1, l, r)
+def query(l, r, tl=0, tr=n-1, pos=1):
 
-	def query_util(self, v, tl, tr, l, r):
-		if l > r:
-			return 0
-		if l == tl and r == tr:
-			return self.t[v]
-		push(v)
-		tm = (tl + tr) // 2
-		return self.query_util(2*v, tl, tm, l, min(tm, r)) + self.query_util(2*v+1, tm+1, tr, max(l, tm+1), r)
+
 
 # n, q = [int(i) for i in input().split()]
 # a = [int(i) for i in input().split()]
@@ -107,5 +81,3 @@ class SegmentTree:
 # 	else:
 # 		print(obj.query(a, b))
 
-
-a = [2, 3, 1, 1, 5, 3]
