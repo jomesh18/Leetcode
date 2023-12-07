d62ac50cbe98e2a257aa11ae561b615832d2e3c4
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-12-07 19:00:47 +0530

    Create 06.totalMoney.py

---
+++ Leetcode_challenge/2023/12. Dec/06.totalMoney.py
@@ -0,0 +1,50 @@
+'''
+1716. Calculate Money in Leetcode Bank
+Easy
+
+1326
+
+41
+
+Add to List
+
+Share
+Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+
+He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 10
+Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+Example 2:
+
+Input: n = 10
+Output: 37
+Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+Example 3:
+
+Input: n = 20
+Output: 96
+Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+ 
+
+Constraints:
+
+1 <= n <= 1000
+'''
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        ans = 0
+        start = 1
+        while start <= n//7:
+            ans += 7*start + (6*7//2)
+            start += 1
+        rem = n % 7
+        ans += (rem*start + (rem*(rem-1)//2))
+        return ans
+                
