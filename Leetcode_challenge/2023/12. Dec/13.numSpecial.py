d09be7f4a28786df2ba15915e983413da4e2fb49
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-12-13 14:30:18 +0530

    Update 13.numSpecial.py

--- Leetcode_challenge/2023/12. Dec/13.numSpecial.py
+++ Leetcode_challenge/2023/12. Dec/13.numSpecial.py
@@ -59,3 +59,23 @@ class Solution:
                     if ok(i, j):
                         count += 1
         return count
+
+
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        count = 0
+        rows = [0]*m
+        cols = [0]*n
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    rows[i] += 1
+                    cols[j] += 1
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and rows[i] == 1 and cols[j] == 1:
+                    count += 1
+                    
+        return count
+                    
