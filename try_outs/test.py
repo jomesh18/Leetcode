2b25cc2ab963a6a46e18c48361e2f69e32ff780f
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2023-12-21 19:20:46 +0530

    Update test.py

--- try_outs/test.py
+++ try_outs/test.py
@@ -1,68 +1,37 @@
 import sys
-def get_ints(): return map(int, sys.stdin.readline().strip().split())
-def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
-def get_string(): return sys.stdin.readline().strip()
 
-n, q = get_ints()
-
-st = [[] for _ in range(4*n)]
-
-def merge(a, b):
-    num1, f1 = a
-    num2, f2 = b
-    ans = num1 + num2
-    for i in range(len(f2)):
-        if f2[i] > 0:
-            for j in range(i+1, len(f1)):
-                if f1[j] > 0:
-                    ans += f2[i]*f1[j]
-    return [ans, [i+j for i, j in zip(f1, f2)]]
-
-def query_util(l, r, tl, tr, pos):
-    if l > r:
-        return [0, [0]*40]
-    elif tl == l and tr == r:
+n, m = map(int, sys.stdin.readline().split())
+st = [0]*(4*n)
+big_num = 
+    
+def query(l, r, v, tl=0, tr=n-1, pos=1):
+    if tl == tr:
         return st[pos]
-    else:
-        tm = (tl+tr)//2
-        return merge(query_util(l, min(r, tm), tl, tm, 2*pos), query_util(max(l, tm+1), r, tm+1, tr, 2*pos+1))
-
-def query(l, r):
-    return query_util(l, r, 0, n-1, 1)[0]
+    elif l > r:
+        return big_num
+    tm = (tl+tr)//2
+    lef = query(l, min(r, tm), tl, tm, 2*pos)
+    rig = query(max(l, tm+1), r, tm+1, tr, 2*pos+1)
+    return lef + rig
 
 def update(i, v, tl=0, tr=n-1, pos=1):
     if tl == tr:
-        count = [0]*40
-        count[v-1] = 1
-        st[pos] = [0, count]
+        st[pos] = [[v, 1]]
     else:
         tm = (tl+tr)//2
         if i <= tm:
             update(i, v, tl, tm, 2*pos)
         else:
             update(i, v, tm+1, tr, 2*pos+1)
+        print(st)
         st[pos] = merge(st[2*pos], st[2*pos+1])
 
-def take_and_build():
-    def build(a, tl=0, tr=n-1, pos=1):
-        if tl == tr:
-            count = [0]*40
-            count[a[tl]-1] = 1
-            st[pos] = [0, count]
-        else:
-            tm = (tl+tr)//2
-            build(a, tl, tm, 2*pos)
-            build(a, tm+1, tr, 2*pos+1)
-            st[pos] = merge(st[2*pos], st[2*pos+1])
-
-    arr = get_list()
-    build(arr)
-
-take_and_build()
-
-for _ in range(q):
-    a, b, c = [int(i) for i in stdin.readline().split()]
-    if a == 1:
-        stdout.write(str(query(b-1, c-1))+"\n")
+Q = []
+for _ in range(m):
+    ins = list(map(int, sys.stdin.readline().split()))
+    if ins[0] == 1:
+        update(ins[1], ins[2])
     else:
-        update(b-1, c)
+        Q.append(query(ins[1], ins[2]-1, ins[3]))
+
+print(*Q, sep="\n")
