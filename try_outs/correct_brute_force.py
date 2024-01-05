b70907db5935f82f6d9dda8429cc9afcddac49f5
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-01-05 18:54:55 +0530

    edited

--- try_outs/correct_brute_force.py
+++ try_outs/correct_brute_force.py
@@ -3,15 +3,17 @@ import sys
 # # For getting input from input.txt file 
 # sys.stdin = open('input.txt', 'r')
 
+MOD = 10**9+7
+
 n, m = [int(i) for i in sys.stdin.readline().split()]
-arr = [0]*n
+arr = [1]*n
 Q = []
 for _ in range(m):
     inp = list(map(int, sys.stdin.readline().split()))
     if inp[0] == 1:
         for i in range(inp[1], inp[2]):
-            arr[i] += inp[3]
+            arr[i] *= inp[3]
     else:
-        Q.append(min(arr[inp[1]: inp[2]]))
+        Q.append(sum(arr[inp[1]: inp[2]]))
 
 print(*Q, sep="\n")
--- try_outs/test.py
+++ try_outs/test.py
@@ -1,38 +1,35 @@
-import sys 
+import sys
 
-# # For getting input from input.txt file 
-# sys.stdin = open('input.txt', 'r')
-  
-# # Printing the Output to output.txt file 
-# sys.stdout = open('my_output.txt', 'w')
+sys.stdin = open('input.txt', 'r')
 
 n, m = map(int, sys.stdin.readline().split())
+MOD = 10**9+7
+sums = [1]*(4*n)
+muls = [1]*(4*n)
 
-st = [(0, 0)]*(4*n)
-
-def add(l, r, v, tl=0, tr=n-1, pos=1):
-    if l == tl and r == tr:
-        st[pos] = (st[pos][0]+v, st[pos][1]+v)
+def mul(l, r, v, tl=0, tr=n-1, pos=1):
+    if tl == l and tr == r:
+        sums[pos] = (sums[pos]*v)%MOD
+        muls[pos] = (muls[pos]*v)%MOD
     elif l <= r:
         tm = (tl+tr)//2
-        add(l, min(r, tm), v, tl, tm, 2*pos)
-        add(max(l, tm+1), r, v, tm+1, tr, 2*pos+1)
-        st[pos] = (min(st[2*pos][0], st[2*pos+1][0])+st[pos][1], st[pos][1])
+        mul(l, min(r, tm), v, tl, tm, 2*pos)
+        mul(max(l, tm+1), r, v, tm+1, tr, 2*pos+1)
+        sums[pos] = (muls[pos] * (sums[2*pos] + sums[2*pos+1])) % MOD
 
 def query(l, r, tl=0, tr=n-1, pos=1):
     if l == tl and r == tr:
-        return st[pos][0]
-    if l > r:
-        return float('inf')
+        return sums[pos]
+    elif l > r:
+        return 0
     tm = (tl+tr)//2
-    
-    return st[pos][1] + min(query(l, min(r, tm), tl, tm, 2*pos), query(max(l, tm+1), r, tm+1, tr, 2*pos+1))
+    return (muls[pos] * ((query(l, min(r, tm), tl, tm, 2*pos) + query(max(l, tm+1), r, tm+1, tr, 2*pos+1))) % MOD) %MOD
 
 Q = []
 for _ in range(m):
     inp = list(map(int, sys.stdin.readline().split()))
     if inp[0] == 1:
-        add(inp[1], inp[2]-1, inp[3])
+        mul(inp[1], inp[2]-1, inp[3])
     else:
         Q.append(query(inp[1], inp[2]-1))
 
