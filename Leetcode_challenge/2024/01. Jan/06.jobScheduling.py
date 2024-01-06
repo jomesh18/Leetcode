f491f73a4b9b88f701cdd7da6d97b80e397f03e7
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-01-06 14:49:25 +0530

    Create 06.jobScheduling.py

---
+++ Leetcode_challenge/2024/01. Jan/06.jobScheduling.py
@@ -0,0 +1,67 @@
+'''
+1235. Maximum Profit in Job Scheduling
+Hard
+
+5833
+
+75
+
+Add to List
+
+Share
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+ 
+
+Example 1:
+
+
+
+Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+Output: 120
+Explanation: The subset chosen is the first and fourth job. 
+Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+Example 2:
+
+
+
+Input: startTime = [1,2,3,4,6], endTime = [3,5,10,6,9], profit = [20,20,100,70,60]
+Output: 150
+Explanation: The subset chosen is the first, fourth and fifth job. 
+Profit obtained 150 = 20 + 70 + 60.
+Example 3:
+
+
+
+Input: startTime = [1,1,1], endTime = [2,3,4], profit = [5,6,4]
+Output: 6
+ 
+
+Constraints:
+
+1 <= startTime.length == endTime.length == profit.length <= 5 * 104
+1 <= startTime[i] < endTime[i] <= 109
+1 <= profit[i] <= 104
+'''
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+        sep = [(startTime[x], endTime[x], profit[x]) for x in range(n)]
+        sep.sort()
+        memo = {}
+        def helper(i):
+            if i == n:
+                return 0
+            if i in memo: return memo[i]
+            ans = helper(i+1)
+            s, e, p = sep[i]
+            j = bisect_left(sep, (e, 0, 0))       
+            ans = max(ans, p+helper(j))
+            memo[i] = ans
+            return ans
+            
+        return helper(0)
