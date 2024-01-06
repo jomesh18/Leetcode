b4f342c3def4f6b3a358fd73abd7d9ea0d8a78f9
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-01-06 18:58:23 +0530

    Update test.py

--- try_outs/test.py
+++ try_outs/test.py
@@ -3,34 +3,50 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 n, m = map(int, sys.stdin.readline().split())
-MOD = 10**9+7
-sums = [1]*(4*n)
-muls = [1]*(4*n)
 
-def mul(l, r, v, tl=0, tr=n-1, pos=1):
+ors = [0]*(4*n)
+ands = [1]*(4*n)
+
+def build(i, tl=0, tr=n-1, pos=1):
+    if tl == tr:
+        ands[pos] = 0
+    else:
+        tm = (tl+tr)//2
+        if i <= tm:
+            build(i, tl, tm, 2*pos)
+        else:
+            build(i, tm+1, tr, 2*pos+1)
+
+for i in range(n):
+    build(i)
+
+def apply_or(l, r, v, tl=0, tr=n-1, pos=1):
     if tl == l and tr == r:
-        sums[pos] = (sums[pos]*v)%MOD
-        muls[pos] = (muls[pos]*v)%MOD
+        ors[pos] |= v
+        ands[pos] |= v
     elif l <= r:
         tm = (tl+tr)//2
-        mul(l, min(r, tm), v, tl, tm, 2*pos)
-        mul(max(l, tm+1), r, v, tm+1, tr, 2*pos+1)
-        sums[pos] = (muls[pos] * (sums[2*pos] + sums[2*pos+1])) % MOD
+        apply_or(l, min(r, tm), v, tl, tm, 2*pos)
+        apply_or(max(l, tm+1), r, v, tm+1, tr, 2*pos+1)
+        ands[pos] = ors[pos] & (ands[2*pos] | ands[2*pos+1])
 
-def query(l, r, tl=0, tr=n-1, pos=1):
-    if l == tl and r == tr:
-        return sums[pos]
+def find_and(l, r, tl=0, tr=n-1, pos=1):
+    if tl == l and tr == r:
+        return ands[pos]
     elif l > r:
         return 0
     tm = (tl+tr)//2
-    return (muls[pos] * ((query(l, min(r, tm), tl, tm, 2*pos) + query(max(l, tm+1), r, tm+1, tr, 2*pos+1))) % MOD) %MOD
+    return ands[pos] & (find_and(l, min(r, tm), tl, tm, 2*pos) | find_and(max(l, tm+1), r, tm+1, tr, 2*pos+1))
 
 Q = []
 for _ in range(m):
     inp = list(map(int, sys.stdin.readline().split()))
     if inp[0] == 1:
-        mul(inp[1], inp[2]-1, inp[3])
+        apply_or(inp[1], inp[2]-1, inp[3])
     else:
-        Q.append(query(inp[1], inp[2]-1))
+        Q.append(find_and(inp[1], inp[2]-1))
+    print(inp)
+    print(ors[:10])
+    print(ands[:10])
 
 print(*Q, sep="\n")
