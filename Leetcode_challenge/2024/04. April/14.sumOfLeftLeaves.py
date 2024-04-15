6c1bde5f69bc825261ad03214f953214bbd8ec3a
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-04-15 13:46:01 +0530

    Create 14.sumOfLeftLeaves.py

---
+++ Leetcode_challenge/2024/04. April/14.sumOfLeftLeaves.py
@@ -0,0 +1,49 @@
+'''
+404. Sum of Left Leaves
+Easy
+
+5399
+
+300
+
+Add to List
+
+Share
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+
+Input: root = [1]
+Output: 0
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, is_left):
+            if not node: return 0
+            if not node.left and not node.right:
+                return node.val if is_left else 0
+            return dfs(node.left, True) + dfs(node.right, False)
+        return dfs(root, False)
+    
