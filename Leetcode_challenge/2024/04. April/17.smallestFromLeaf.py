c44de79aba1695810bccfe448561744b21537866
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-04-17 13:31:01 +0530

    Create 17.smallestFromLeaf.py

---
+++ Leetcode_challenge/2024/04. April/17.smallestFromLeaf.py
@@ -0,0 +1,64 @@
+'''
+988. Smallest String Starting From Leaf
+Medium
+
+1903
+
+266
+
+Add to List
+
+Share
+You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+
+Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+
+As a reminder, any shorter prefix of a string is lexicographically smaller.
+
+For example, "ab" is lexicographically smaller than "aba".
+A leaf of a node is a node that has no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [0,1,2,3,4,3,4]
+Output: "dba"
+Example 2:
+
+
+Input: root = [25,1,3,1,3,0,2]
+Output: "adz"
+Example 3:
+
+
+Input: root = [2,2,1,null,1,0,null,0]
+Output: "abc"
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 8500].
+0 <= Node.val <= 25
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        self.ans = '{'
+        def dfs(node, curr):
+            curr = curr + [chr(node.val+ord('a'))]
+            if not node.left and not node.right:
+                self.ans = min(self.ans, ''.join(curr[::-1]))
+            if node.left:
+                dfs(node.left, curr)
+            if node.right:
+                dfs(node.right, curr)
+            curr.pop()
+        dfs(root, [])
+        return self.ans
