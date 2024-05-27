dbb915093d4ba94b7ec0ee51153e28e9a1a14085
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-05-27 14:57:55 +0530

    Update 27.specialArray.py

--- Leetcode_challenge/2024/05. May/27.specialArray.py
+++ Leetcode_challenge/2024/05. May/27.specialArray.py
@@ -56,3 +56,17 @@ class Solution:
             else:
                 hi = mid - 1
         return -1
+
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq = [0]*(n+1)
+        for num in nums:
+            freq[min(n, num)] += 1
+        count = 0
+        for i in range(n, 0, -1):
+            count += freq[i]
+            if count == i:
+                return i
+        return -1
