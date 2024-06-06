450cae97c844c3f1890a13a186244832865e3071
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-06-06 18:14:22 +0530

    Update 06.isNStraightHand.py

--- Leetcode_challenge/2024/06. June/06.isNStraightHand.py
+++ Leetcode_challenge/2024/06. June/06.isNStraightHand.py
@@ -37,6 +37,7 @@ Constraints:
 
 Note: This question is the same as 1296: https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
 '''
+# O(nlogn)
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize: return False
@@ -49,4 +50,23 @@ class Solution:
                         return False
                     count[key+delta] -= val
 
+        return True
+
+
+# O(n) time
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        count = Counter(hand)
+        for card in count.keys():
+            start = card
+            while count[start-1]:
+                start -= 1
+            while start <= card:
+                if count[start]:
+                    val = count[start]
+                    count[start] -= val
+                    for curr in range(start+1, start+groupSize):
+                        count[curr] -= val
+                        if count[curr] < 0: return False
+                start += 1
         return True
