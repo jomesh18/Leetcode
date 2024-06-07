3ede28c6a7acb4854d3d5ac24201da1f465801c9
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-06-07 13:42:31 +0530

    Create 07.replaceWords.py

---
+++ Leetcode_challenge/2024/06. June/07.replaceWords.py
@@ -0,0 +1,61 @@
+'''
+648. Replace Words
+Medium
+
+2505
+
+188
+
+Add to List
+
+Share
+In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word derivative. For example, when the root "help" is followed by the word "ful", we can form a derivative "helpful".
+
+Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the derivatives in the sentence with the root forming it. If a derivative can be replaced by more than one root, replace it with the root that has the shortest length.
+
+Return the sentence after the replacement.
+
+ 
+
+Example 1:
+
+Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+Example 2:
+
+Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+Output: "a a b c"
+ 
+
+Constraints:
+
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 100
+dictionary[i] consists of only lower-case letters.
+1 <= sentence.length <= 106
+sentence consists of only lower-case letters and spaces.
+The number of words in sentence is in the range [1, 1000]
+The length of each word in sentence is in the range [1, 1000]
+Every two consecutive words in sentence will be separated by exactly one space.
+sentence does not have leading or trailing spaces.
+'''
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = {}
+        for word in dictionary:
+            curr = trie
+            for c in word:
+                curr[c] = curr.get(c, {})
+                curr = curr[c]
+            curr['#'] = word
+            
+        ret = sentence.split()
+        for i, word in enumerate(ret):
+            curr = trie
+            for c in word:
+                curr = curr.get(c, None)
+                if not curr: break
+                if '#' in curr: 
+                    ret[i] = curr['#']
+                    break
+        return ' '.join(ret)
