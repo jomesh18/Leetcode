23e3c9d9a3b1b685cfb63a4c6fe49d1f87b828d9
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-07-08 15:26:40 +0530

    Update 08.findTheWinner.py

--- Leetcode_challenge/2024/07. July/08.findTheWinner.py
+++ Leetcode_challenge/2024/07. July/08.findTheWinner.py
@@ -62,3 +62,11 @@ class Solution:
                 q.append(curr)
             q.popleft()
         return q.popleft()
+
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        ans = 0
+        for i in range(2, n+1):
+            ans = (ans + k) % i
+        return ans + 1
