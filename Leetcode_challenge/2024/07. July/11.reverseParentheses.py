46d1a3638408d6aaaeac2725e667e45c53da97fd
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-07-11 13:44:29 +0530

    Create 11.reverseParentheses.py

---
+++ Leetcode_challenge/2024/07. July/11.reverseParentheses.py
@@ -0,0 +1,55 @@
+'''
+1190. Reverse Substrings Between Each Pair of Parentheses
+Medium
+
+2245
+
+83
+
+Add to List
+
+Share
+You are given a string s that consists of lower case English letters and brackets.
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+ 
+
+Example 1:
+
+Input: s = "(abcd)"
+Output: "dcba"
+Example 2:
+
+Input: s = "(u(love)i)"
+Output: "iloveu"
+Explanation: The substring "love" is reversed first, then the whole string is reversed.
+Example 3:
+
+Input: s = "(ed(et(oc))el)"
+Output: "leetcode"
+Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s only contains lower case English characters and parentheses.
+It is guaranteed that all parentheses are balanced.
+'''
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c == ')':
+                curr = []
+                while stack[-1] != '(':
+                    curr.append(stack.pop())
+                stack.pop()
+                for c2 in curr:
+                    stack.append(c2)
+            else:
+                stack.append(c)
+        return ''.join(stack)
