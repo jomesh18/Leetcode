23e420c23972ef18269ccd1f4cb6a4dbf0128fa3
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-07-12 15:53:11 +0530

    Create 12.maximumGain.py

---
+++ Leetcode_challenge/2024/07. July/12.maximumGain.py
@@ -0,0 +1,88 @@
+'''
+1717. Maximum Score From Removing Substrings
+Medium
+
+891
+
+64
+
+Add to List
+
+Share
+You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+Remove substring "ab" and gain x points.
+For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+Remove substring "ba" and gain y points.
+For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+Return the maximum points you can gain after applying the above operations on s.
+
+ 
+
+Example 1:
+
+Input: s = "cdbcbbaaabab", x = 4, y = 5
+Output: 19
+Explanation:
+- Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+- Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+- Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+- Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+Total score = 5 + 4 + 5 + 5 = 19.
+Example 2:
+
+Input: s = "aabbaaxybbaabb", x = 5, y = 4
+Output: 20
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+1 <= x, y <= 104
+s consists of lowercase English letters.
+'''
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        stack = []
+        ans = 0
+        # remove ab first
+        for c in s:
+            if c == 'b' and stack and stack[-1] == 'a':
+                stack.pop()
+                ans += x
+            else:
+                stack.append(c)
+        # print(ans)
+
+        stack2 = []
+        for c in stack:
+            if c == 'a' and stack2 and stack2[-1] == 'b':
+                stack2.pop()
+                ans += y
+            else:
+                stack2.append(c)
+        # print(ans)
+
+        curr = ans
+        ans = 0
+        
+        stack = []
+        # remove ab first
+        for c in s:
+            if c == 'a' and stack and stack[-1] == 'b':
+                stack.pop()
+                ans += y
+            else:
+                stack.append(c)
+        # print(ans)
+
+        stack2 = []
+        for c in stack:
+            if c == 'b' and stack2 and stack2[-1] == 'a':
+                stack2.pop()
+                ans += x
+            else:
+                stack2.append(c)
+        # print(ans)
+
+        return max(curr, ans)
