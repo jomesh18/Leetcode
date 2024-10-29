f16cf2c8e38a671db19f479be4008d81ccf3583e
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-10-29 13:48:12 +0530

    Create 29.maxMoves.py

---
+++ Leetcode_challenge/2024/10. Oct/29.maxMoves.py
@@ -0,0 +1,61 @@
+'''
+2684. Maximum Number of Moves in a Grid
+Medium
+
+598
+
+13
+
+Add to List
+
+Share
+You are given a 0-indexed m x n matrix grid consisting of positive integers.
+
+You can start at any cell in the first column of the matrix, and traverse the grid in the following way:
+
+From a cell (row, col), you can move to any of the cells: (row - 1, col + 1), (row, col + 1) and (row + 1, col + 1) such that the value of the cell you move to, should be strictly bigger than the value of the current cell.
+Return the maximum number of moves that you can perform.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[2,4,3,5],[5,4,9,3],[3,4,2,11],[10,9,13,15]]
+Output: 3
+Explanation: We can start at the cell (0, 0) and make the following moves:
+- (0, 0) -> (0, 1).
+- (0, 1) -> (1, 2).
+- (1, 2) -> (2, 3).
+It can be shown that it is the maximum number of moves that can be made.
+Example 2:
+
+
+Input: grid = [[3,2,4],[2,1,9],[1,1,7]]
+Output: 0
+Explanation: Starting from any cell in the first column we cannot perform any moves.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+2 <= m, n <= 1000
+4 <= m * n <= 105
+1 <= grid[i][j] <= 106
+'''
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        prev = [1]*m
+        for j in range(1, n):
+            curr = [0]*m
+            for i in range(m):
+                if (i > 0 and prev[i-1] and grid[i-1][j-1] < grid[i][j]) or (prev[i] and grid[i][j-1] < grid[i][j]) or (i < m-1 and prev[i+1] and grid[i+1][j-1] < grid[i][j]):
+                    ans = j
+                    curr[i] = 1
+            prev = curr[:]
+            if ans != j:
+                break
+        return ans
