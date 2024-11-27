cba3255c6f05373ecb840a793836571e43f448ab
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-11-27 14:21:49 +0530

    Create 27.shortestDistanceAfterQueries.py

---
+++ Leetcode_challenge/2024/11. Nov/27.shortestDistanceAfterQueries.py
@@ -0,0 +1,96 @@
+'''
+3243. Shortest Distance After Road Addition Queries I
+Medium
+
+276
+
+15
+
+Add to List
+
+Share
+You are given an integer n and a 2D integer array queries.
+
+There are n cities numbered from 0 to n - 1. Initially, there is a unidirectional road from city i to city i + 1 for all 0 <= i < n - 1.
+
+queries[i] = [ui, vi] represents the addition of a new unidirectional road from city ui to city vi. After each query, you need to find the length of the shortest path from city 0 to city n - 1.
+
+Return an array answer where for each i in the range [0, queries.length - 1], answer[i] is the length of the shortest path from city 0 to city n - 1 after processing the first i + 1 queries.
+
+ 
+
+Example 1:
+
+Input: n = 5, queries = [[2,4],[0,2],[0,4]]
+
+Output: [3,2,1]
+
+Explanation:
+
+
+
+After the addition of the road from 2 to 4, the length of the shortest path from 0 to 4 is 3.
+
+
+
+After the addition of the road from 0 to 2, the length of the shortest path from 0 to 4 is 2.
+
+
+
+After the addition of the road from 0 to 4, the length of the shortest path from 0 to 4 is 1.
+
+Example 2:
+
+Input: n = 4, queries = [[0,3],[0,2]]
+
+Output: [1,1]
+
+Explanation:
+
+
+
+After the addition of the road from 0 to 3, the length of the shortest path from 0 to 3 is 1.
+
+
+
+After the addition of the road from 0 to 2, the length of the shortest path remains 1.
+
+ 
+
+Constraints:
+
+3 <= n <= 500
+1 <= queries.length <= 500
+queries[i].length == 2
+0 <= queries[i][0] < queries[i][1] < n
+1 < queries[i][1] - queries[i][0]
+There are no repeated roads among the queries.
+'''
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(n)]
+        for i in range(n-1):
+            adj[i].append(i+1)
+        
+        def shortest_dist(adj, start, dest):
+            q = deque([start])
+            dist = 0
+            visited = [False]*n
+            visited[start] = True
+            while q:
+                size = len(q)
+                for _ in range(size):
+                    curr = q.popleft()
+                    if curr == dest:
+                        return dist
+                    for nei in adj[curr]:
+                        if not visited[nei]:
+                            visited[nei] = True
+                            q.append(nei)
+                dist += 1
+            
+        ans = [0]*len(queries)
+        for i, (s, e) in enumerate(queries):
+            adj[s].append(e)
+            ans[i] = shortest_dist(adj, 0, n-1)
+        return ans
