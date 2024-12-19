e8b21a74ff866d59d84dc2d4691ea7c40231a2d1
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-12-19 15:07:40 +0530

    Create 19.maxChunksToSorted.py

---
+++ Leetcode_challenge/2024/12. Dec/19.maxChunksToSorted.py
@@ -0,0 +1,58 @@
+'''
+769. Max Chunks To Make Sorted
+Medium
+
+3104
+
+284
+
+Add to List
+
+Share
+You are given an integer array arr of length n that represents a permutation of the integers in the range [0, n - 1].
+
+We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
+
+Return the largest number of chunks we can make to sort the array.
+
+ 
+
+Example 1:
+
+Input: arr = [4,3,2,1,0]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+Example 2:
+
+Input: arr = [1,0,2,3,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [1, 0], [2, 3, 4].
+However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible.
+ 
+
+Constraints:
+
+n == arr.length
+1 <= n <= 10
+0 <= arr[i] < n
+All the elements of arr are unique.
+'''
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        ans = 0
+        stack = []
+        filled = 0
+        for i, num in enumerate(arr):
+            while stack and stack[-1] <= i:
+                stack.pop()
+                filled += 1
+            if num <= i:
+                filled += 1
+                if filled == i+1:
+                    ans += 1
+            else:
+                stack.append(num)            
+        return ans
