3fab9e3a5a4c5790c7d31ad26a06367311106430
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2025-03-24 10:13:53 +0530

    Create 24.countDays.py

---
+++ Leetcode_challenge/2025/03. March/24.countDays.py
@@ -0,0 +1,69 @@
+'''
+3169. Count Days Without Meetings
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given a positive integer days representing the total number of days an employee is available for work (starting from day 1). You are also given a 2D array meetings of size n where, meetings[i] = [start_i, end_i] represents the starting and ending days of meeting i (inclusive).
+
+Return the count of days when the employee is available for work but no meetings are scheduled.
+
+Note: The meetings may overlap.
+
+ 
+
+Example 1:
+
+Input: days = 10, meetings = [[5,7],[1,3],[9,10]]
+
+Output: 2
+
+Explanation:
+
+There is no meeting scheduled on the 4th and 8th days.
+
+Example 2:
+
+Input: days = 5, meetings = [[2,4],[1,3]]
+
+Output: 1
+
+Explanation:
+
+There is no meeting scheduled on the 5th day.
+
+Example 3:
+
+Input: days = 6, meetings = [[1,6]]
+
+Output: 0
+
+Explanation:
+
+Meetings are scheduled for all working days.
+
+ 
+
+Constraints:
+
+1 <= days <= 109
+1 <= meetings.length <= 105
+meetings[i].length == 2
+1 <= meetings[i][0] <= meetings[i][1] <= days
+'''
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        ans = 0
+        curr = 1
+        meetings.sort()
+        for s, e in meetings:
+            if curr < s:
+                ans += s-curr
+            if curr <= e:
+                curr = e + 1
+            if curr > days:
+                break
+        if curr <= days:
+            ans += days-curr+1
+        return ans 
