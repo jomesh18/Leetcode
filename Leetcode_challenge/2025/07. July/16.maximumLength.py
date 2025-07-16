b2e67aa054f50940ae32315c9487550fdd8e5fd7
Jomesh <jomesh18@gmail.com>
2025-07-16 15:04:48 +0530

    16.maximumLength.py

---
+++ Leetcode_challenge/2025/07. July/16.maximumLength.py
@@ -0,0 +1,72 @@
+'''
+3201. Find the Maximum Length of Valid Subsequence I
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums.
+A subsequence sub of nums with length x is called valid if it satisfies:
+
+(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.
+Return the length of the longest valid subsequence of nums.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+
+Output: 4
+
+Explanation:
+
+The longest valid subsequence is [1, 2, 3, 4].
+
+Example 2:
+
+Input: nums = [1,2,1,1,2,1,2]
+
+Output: 6
+
+Explanation:
+
+The longest valid subsequence is [1, 2, 1, 2, 1, 2].
+
+Example 3:
+
+Input: nums = [1,3]
+
+Output: 2
+
+Explanation:
+
+The longest valid subsequence is [1, 3].
+
+ 
+
+Constraints:
+
+2 <= nums.length <= 2 * 105
+1 <= nums[i] <= 107
+'''
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [[0]*4 for _ in range(n+1)]
+        for i in range(n):
+            if nums[i] & 1:
+                dp[i+1][1] = 1 + dp[i][1]
+                dp[i+1][3] = 1 + dp[i][2]
+                dp[i+1][0] = dp[i][0]
+                dp[i+1][2] = dp[i][2]
+            else:
+                dp[i+1][0] = 1 + dp[i][0]
+                dp[i+1][2] = 1 + dp[i][3]
+                dp[i+1][1] = dp[i][1]
+                dp[i+1][3] = dp[i][3]
+        
+        return max(dp[-1])
