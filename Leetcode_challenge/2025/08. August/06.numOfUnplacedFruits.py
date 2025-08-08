a1c16500684934b99eebaf0448e2e1fa07b72ca4
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2025-08-08 14:00:01 +0530

    added 06.numOfUnplacedFruits.py

---
+++ Leetcode_challenge/2025/08. August/06.numOfUnplacedFruits.py
@@ -0,0 +1,94 @@
+'''
+3479. Fruits Into Baskets III
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+
+From left to right, place the fruits according to these rules:
+
+Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+Each basket can hold only one type of fruit.
+If a fruit type cannot be placed in any basket, it remains unplaced.
+Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+ 
+
+Example 1:
+
+Input: fruits = [4,2,5], baskets = [3,5,4]
+
+Output: 1
+
+Explanation:
+
+fruits[0] = 4 is placed in baskets[1] = 5.
+fruits[1] = 2 is placed in baskets[0] = 3.
+fruits[2] = 5 cannot be placed in baskets[2] = 4.
+Since one fruit type remains unplaced, we return 1.
+
+Example 2:
+
+Input: fruits = [3,6,1], baskets = [6,4,7]
+
+Output: 0
+
+Explanation:
+
+fruits[0] = 3 is placed in baskets[0] = 6.
+fruits[1] = 6 cannot be placed in baskets[1] = 4 (insufficient capacity) but can be placed in the next available basket, baskets[2] = 7.
+fruits[2] = 1 is placed in baskets[1] = 4.
+Since all fruits are successfully placed, we return 0.
+
+ 
+
+Constraints:
+
+n == fruits.length == baskets.length
+1 <= n <= 105
+1 <= fruits[i], baskets[i] <= 109
+'''
+class SegmentTree:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.t = [0]*(4*self.n)
+        self.build(arr, 0, self.n-1, 1)
+
+    def merge(self, v):
+        self.t[v] = max(self.t[2*v], self.t[2*v+1])
+
+    def build(self, arr, tl, tr, v):
+        if tl == tr:
+            self.t[v] = arr[tl]
+            return
+        tm = (tl + tr) // 2
+        self.build(arr, tl, tm, 2*v)
+        self.build(arr, tm+1, tr, 2*v+1)
+        self.merge(v)
+
+    def find_first_and_update(self, v, tl, tr, val):
+        if self.t[v] < val:
+            return -1
+        if tl == tr:
+            self.t[v] = -1
+            return tl
+        tm = (tl + tr) // 2
+        i = self.find_first_and_update(2*v, tl, min(tm, tr), val)
+        if i == -1:
+            i = self.find_first_and_update(2*v+1, max(tm+1, tl), tr, val)
+        self.merge(v)
+        return i
+
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        t = SegmentTree(baskets)
+        n = len(baskets)
+        ans = 0
+        for f in fruits:
+            if t.find_first_and_update(1, 0, n-1, f) == -1:
+                ans += 1
+
+        return ans
