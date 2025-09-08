0d4d5f3b7889063f0fe416fb13d9576222985434
Jomesh George <jomesh18@gmail.com>
2025-09-08 14:45:58 +0530

    added 08.getNoZeroIntegers.py

---
+++ Leetcode_challenge/2025/09. Sept/08.getNoZeroIntegers.py
@@ -0,0 +1,47 @@
+'''
+1317. Convert Integer to the Sum of Two No-Zero Integers
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+
+Given an integer n, return a list of two integers [a, b] where:
+
+a and b are No-Zero integers.
+a + b = n
+The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [1,1]
+Explanation: Let a = 1 and b = 1.
+Both a and b are no-zero integers, and a + b = 2 = n.
+Example 2:
+
+Input: n = 11
+Output: [2,9]
+Explanation: Let a = 2 and b = 9.
+Both a and b are no-zero integers, and a + b = 11 = n.
+Note that there are other valid answers as [8, 3] that can be accepted.
+ 
+
+Constraints:
+
+2 <= n <= 104
+'''
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def is_no_zero(a, b):
+            if '0' in str(a): return False
+            if '0' in str(b): return False
+            return True
+
+        for i in range(1, n//2+1):
+            if is_no_zero(i, n-i):
+                return [i, n-i]
