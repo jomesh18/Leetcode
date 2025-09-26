9f020a289b215506a061f26d27a66352fb3eb6c4
Jomesh George <jomesh18@gmail.com>
2025-09-26 14:26:12 +0530

    added 26.triangleNumber.py

---
+++ Leetcode_challenge/2025/09. Sept/26.triangleNumber.py
@@ -0,0 +1,41 @@
+'''
+611. Valid Triangle Number
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,4]
+Output: 3
+Explanation: Valid combinations are: 
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+Example 2:
+
+Input: nums = [4,2,3,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 1000
+'''
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        ans = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                t = nums[i] + nums[j]
+                k = bisect_right(nums, t-1)
+                ans += max(0, k-j-1)
+        return ans
