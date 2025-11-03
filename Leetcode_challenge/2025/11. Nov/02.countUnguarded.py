25bc7efb2ea12623acb96b651da4eceb16872665
Jomesh George <jomesh18@gmail.com>
2025-11-03 17:11:27 +0530

    added 02.countUnguarded.py

---
+++ Leetcode_challenge/2025/11. Nov/02.countUnguarded.py
@@ -0,0 +1,91 @@
+'''
+2257. Count Unguarded Cells in the Grid
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given two integers m and n representing a 0-indexed m x n grid. You are also given two 2D integer arrays guards and walls where guards[i] = [rowi, coli] and walls[j] = [rowj, colj] represent the positions of the ith guard and jth wall respectively.
+
+A guard can see every cell in the four cardinal directions (north, east, south, or west) starting from their position unless obstructed by a wall or another guard. A cell is guarded if there is at least one guard that can see it.
+
+Return the number of unoccupied cells that are not guarded.
+
+ 
+
+Example 1:
+
+
+Input: m = 4, n = 6, guards = [[0,0],[1,1],[2,3]], walls = [[0,1],[2,2],[1,4]]
+Output: 7
+Explanation: The guarded and unguarded cells are shown in red and green respectively in the above diagram.
+There are a total of 7 unguarded cells, so we return 7.
+Example 2:
+
+
+Input: m = 3, n = 3, guards = [[1,1]], walls = [[0,1],[1,0],[2,1],[1,2]]
+Output: 4
+Explanation: The unguarded cells are shown in green in the above diagram.
+There are a total of 4 unguarded cells, so we return 4.
+ 
+
+Constraints:
+
+1 <= m, n <= 105
+2 <= m * n <= 105
+1 <= guards.length, walls.length <= 5 * 104
+2 <= guards.length + walls.length <= m * n
+guards[i].length == walls[j].length == 2
+0 <= rowi, rowj < m
+0 <= coli, colj < n
+All the positions in guards and walls are unique.
+'''
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        W = 2
+        G = 1
+        F = 0
+        G1 = 3
+
+        grid = [[F]*n for _ in range(m)]
+        for i, j in guards:
+            grid[i][j] = G
+        for i, j in walls:
+            grid[i][j] = W
+
+        from_above = [F]*n
+        for i in range(m):
+            prev = F
+            for j in range(n):
+                if grid[i][j] == F:
+                    if prev == G or from_above[j]:
+                        grid[i][j] = G1
+                elif grid[i][j] == W:
+                    from_above[j] = F
+                    prev = F
+                elif grid[i][j] == G:
+                    from_above[j] = G
+                    prev = G
+        # print(grid)
+        from_below = [F]*n
+        for i in range(m-1, -1, -1):
+            prev = F
+            for j in range(n-1, -1, -1):
+                if grid[i][j] == F:
+                    if from_below[j] == G or prev == G:
+                        grid[i][j] = G1
+                elif grid[i][j] == G:
+                    from_below[j] = G
+                    prev = G
+                elif grid[i][j] == W:
+                    from_below[j] = F
+                    prev = F
+        
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == F:
+                    ans += 1
+        # print(grid)
+        return ans
