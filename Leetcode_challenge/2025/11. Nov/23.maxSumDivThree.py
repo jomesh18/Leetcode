1d61f5eb83f62d8a25c49356d253376adbed8aee
Jomesh George <jomesh18@gmail.com>
2025-11-24 16:26:04 +0530

    added 23.maxSumDivThree.py

---
+++ Leetcode_challenge/2025/11. Nov/23.maxSumDivThree.py
@@ -0,0 +1,43 @@
+'''
+1262. Greatest Sum Divisible by Three
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, return the maximum possible sum of elements of the array such that it is divisible by three.
+
+ 
+
+Example 1:
+
+Input: nums = [3,6,5,1,8]
+Output: 18
+Explanation: Pick numbers 3, 6, 1 and 8 their sum is 18 (maximum sum divisible by 3).
+Example 2:
+
+Input: nums = [4]
+Output: 0
+Explanation: Since 4 is not divisible by 3, do not pick any number.
+Example 3:
+
+Input: nums = [1,2,3,4,4]
+Output: 12
+Explanation: Pick numbers 1, 3, 4 and 4 their sum is 12 (maximum sum divisible by 3).
+ 
+
+Constraints:
+
+1 <= nums.length <= 4 * 104
+1 <= nums[i] <= 104
+'''
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        prev = [0, float('-inf'), float('-inf')]
+        for num in nums:
+            curr = prev[:]
+            for i in range(3):
+                curr[(i+(num%3))%3] = max(prev[i] + num, curr[(i+(num%3))%3])
+            prev = curr[:]
+        return prev[0]
