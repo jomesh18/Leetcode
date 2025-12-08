25da23ff206e916cc3d2ed12c35642bc290722a2
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2025-12-08 11:06:21 +0530

    added 08.countTriples.py

---
+++ Leetcode_challenge/2025/12. Dec/08.countTriples.py
@@ -0,0 +1,43 @@
+'''
+1925. Count Square Sum Triples
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: The square triples are (3,4,5) and (4,3,5).
+Example 2:
+
+Input: n = 10
+Output: 4
+Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ 
+
+Constraints:
+
+1 <= n <= 250
+'''
+class Solution:
+    def countTriples(self, n: int) -> int:
+        sqr = n*n
+        ans = 0
+        for i in range(1, n-1):
+            for j in range(i+1, n):
+                curr = i*i + j*j
+                if curr > sqr:
+                    break
+                curr_sqrt = int(curr**.5)
+                if curr == curr_sqrt*curr_sqrt:
+                    ans += 2
+        return ans
