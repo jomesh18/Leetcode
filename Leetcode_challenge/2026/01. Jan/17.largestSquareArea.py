e722227dc76d5aa4e982ee553f73c879a3b73ec0
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2026-01-25 20:08:36 +0530

    added 17.largestSquareArea.py

---
+++ Leetcode_challenge/2026/01. Jan/17.largestSquareArea.py
@@ -0,0 +1,86 @@
+'''
+3047. Find the Largest Area of Square Inside Two Rectangles
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+There exist n rectangles in a 2D plane with edges parallel to the x and y axis. You are given two 2D integer arrays bottomLeft and topRight where bottomLeft[i] = [a_i, b_i] and topRight[i] = [c_i, d_i] represent the bottom-left and top-right coordinates of the ith rectangle, respectively.
+
+You need to find the maximum area of a square that can fit inside the intersecting region of at least two rectangles. Return 0 if such a square does not exist.
+
+ 
+
+Example 1:
+
+
+Input: bottomLeft = [[1,1],[2,2],[3,1]], topRight = [[3,3],[4,4],[6,6]]
+
+Output: 1
+
+Explanation:
+
+A square with side length 1 can fit inside either the intersecting region of rectangles 0 and 1 or the intersecting region of rectangles 1 and 2. Hence the maximum area is 1. It can be shown that a square with a greater side length can not fit inside any intersecting region of two rectangles.
+
+Example 2:
+
+
+Input: bottomLeft = [[1,1],[1,3],[1,5]], topRight = [[5,5],[5,7],[5,9]]
+
+Output: 4
+
+Explanation:
+
+A square with side length 2 can fit inside either the intersecting region of rectangles 0 and 1 or the intersecting region of rectangles 1 and 2. Hence the maximum area is 2 * 2 = 4. It can be shown that a square with a greater side length can not fit inside any intersecting region of two rectangles.
+
+Example 3:
+
+  
+Input: bottomLeft = [[1,1],[2,2],[1,2]], topRight = [[3,3],[4,4],[3,4]]
+
+Output: 1
+
+Explanation:
+
+A square with side length 1 can fit inside the intersecting region of any two rectangles. Also, no larger square can, so the maximum area is 1. Note that the region can be formed by the intersection of more than 2 rectangles.
+
+Example 4:
+
+  
+Input: bottomLeft = [[1,1],[3,3],[3,1]], topRight = [[2,2],[4,4],[4,2]]
+
+Output: 0
+
+Explanation:
+
+No pair of rectangles intersect, hence, the answer is 0.
+
+ 
+
+Constraints:
+
+n == bottomLeft.length == topRight.length
+2 <= n <= 103
+bottomLeft[i].length == topRight[i].length == 2
+1 <= bottomLeft[i][0], bottomLeft[i][1] <= 107
+1 <= topRight[i][0], topRight[i][1] <= 107
+bottomLeft[i][0] < topRight[i][0]
+bottomLeft[i][1] < topRight[i][1]
+'''
+class Solution:
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        coords = []
+        for (a, b), (c, d) in zip(bottomLeft, topRight):
+            coords.append((a,b,c,d))
+        coords.sort()
+        ans = 0
+        for i in range(len(coords)):
+            for j in range(i+1, len(coords)):
+                x1, y1, x2, y2 = coords[i]
+                x11, y11, x22, y22 = coords[j]
+                if x11 >= x2 or y11 >= y2 or y22 <= y1:
+                    continue
+                a = min(abs(max(x11, x1)-min(x22, x2)), abs(max(y11, y1)-min(y22, y2)))
+                ans = max(ans, a*a)
+        return ans
