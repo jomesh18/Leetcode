9c3723f74747edbd05736f18da230f61f4b60bae
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2026-02-10 12:24:05 +0530

    added 10.longestBalanced.py

---
+++ Leetcode_challenge/2026/02. Feb/10.longestBalanced.py
@@ -0,0 +1,68 @@
+'''
+3719. Longest Balanced Subarray I
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums.
+
+A subarray is called balanced if the number of distinct even numbers in the subarray is equal to the number of distinct odd numbers.
+
+Return the length of the longest balanced subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,4,3]
+
+Output: 4
+
+Explanation:
+
+The longest balanced subarray is [2, 5, 4, 3].
+It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [5, 3]. Thus, the answer is 4.
+Example 2:
+
+Input: nums = [3,2,2,5,4]
+
+Output: 5
+
+Explanation:
+
+The longest balanced subarray is [3, 2, 2, 5, 4].
+It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [3, 5]. Thus, the answer is 5.
+Example 3:
+
+Input: nums = [1,2,3,2]
+
+Output: 3
+
+Explanation:
+
+The longest balanced subarray is [2, 3, 2].
+It has 1 distinct even number [2] and 1 distinct odd number [3]. Thus, the answer is 3.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1500
+1 <= nums[i] <= 105
+'''
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        max_len = 0
+        n = len(nums)
+        for i in range(n):
+            even, odd = Counter(), Counter()
+            for j in range(i, n):
+                if nums[j] & 1:
+                    odd[nums[j]] += 1
+                else:
+                    even[nums[j]] += 1
+                if len(odd) == len(even):
+                    max_len = max(max_len, j-i+1)
+        
+        return max_len
